3e182d0b7bfb11201bd20d4ec3705b740984fffa
vikasmadan09 <vikasmadan09@gmail.com>
2022-03-08 11:50:52 +0530

    set "defaultChecked" to true for first item in a expansion panel list

--- packages/frontend/src/components/mbc/pipeline/createNewPipeline/CreateNewPipeline.tsx
+++ packages/frontend/src/components/mbc/pipeline/createNewPipeline/CreateNewPipeline.tsx
@@ -702,7 +702,7 @@ const CreateNewPipeline = () => {
                         >
                           <div className={classNames('expansion-panel', index === 0 ? 'open' : '')}>
                             <span className="animation-wrapper"></span>
-                            <input type="checkbox" className="ff-only" id={index + '1'} />
+                            <input type="checkbox" className="ff-only" id={index + '1'} defaultChecked={index === 0} />
                             <label
                               className={'expansion-panel-label' + ' ' + Styles.dagContentCaption}
                               htmlFor={index + '1'}
@@ -809,7 +809,12 @@ const CreateNewPipeline = () => {
                                                     >
                                                       <label className={'checkbox ' + Styles.checkBoxDisable}>
                                                         <span className="wrapper">
-                                                          <input type="checkbox" className="ff-only" value="can_dag_read" checked={true} />
+                                                          <input
+                                                            type="checkbox"
+                                                            className="ff-only"
+                                                            value="can_dag_read"
+                                                            checked={true}
+                                                          />
                                                         </span>
                                                         <span className="label">Read</span>
                                                       </label>
