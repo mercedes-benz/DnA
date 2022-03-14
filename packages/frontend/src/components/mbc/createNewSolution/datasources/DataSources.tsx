609c6545b369c167ca25f31e728c88fd0c6a6b83
vikasmadan09 <vikasmadan09@gmail.com>
2022-03-14 16:38:21 +0530

    replace acessing label with textContent as textContent wont trim trailling white spaces

--- packages/frontend/src/components/mbc/createNewSolution/datasources/DataSources.tsx
+++ packages/frontend/src/components/mbc/createNewSolution/datasources/DataSources.tsx
@@ -141,7 +141,7 @@ export default class DataSources extends React.Component<IDataSourcesProps, IDat
     if (selectedOptions.length) {
       Array.from(selectedOptions).forEach((option) => {
         dataVolume.id = option.value;
-        dataVolume.name = option.label;
+        dataVolume.name = option.textContent;
       });
     }
     this.setState({ dataVolumeValue: dataVolume.id ? dataVolume : {} });
