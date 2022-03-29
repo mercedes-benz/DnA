11b1b6e4840cac4bce84c6ce4492596a8bbf7ca2
Sathishkumar <ibm.i.palani@daimler.com>
2022-03-29 14:44:50 +0530

    adding rule to  find the file

--- packages/frontend/docker-start.sh
+++ packages/frontend/docker-start.sh
@@ -12,8 +12,7 @@ echo "DNS Resolver set to: $PROJECTSMO_DNS_RESOLVER_ADDRESS"
 echo "Changing to directory and setting environment variables"
 cd /usr/share/nginx/html
 env > env.txt
-export JS_FILE=$(find . -name '*.js')
-export JS_MAP_FILE=$(find . -name '*.js.map')
+export JS_FILE=$(find . -type f -print0 | xargs -0 grep -l "PROJECTSMO")
 env > env_new.txt
 
 # echo "Processing app.js ($APP_FILE). Execute ENV replacement ..."
@@ -25,13 +24,6 @@ then
     perl -p -e 's/\$\{([^}]+)\}/defined $ENV{$1} ? $ENV{$1} : $&/eg; s/\$\{PROJECTSMO_([^}])+\}//eg' -i $JS_FILE
 else
     echo "No .js files found $JS_FILE"
-fi
-if [ "$JS_MAP_FILE" != "" ]
-then
-    echo "Processing .js.map files $JS_MAP_FILE. Execute ENV replacement ..."
-    perl -p -e 's/\$\{([^}]+)\}/defined $ENV{$1} ? $ENV{$1} : $&/eg; s/\$\{PROJECTSMO_([^}])+\}//eg' -i $JS_MAP_FILE
-else
-    echo "No .js.map files found $JS_MAP_FILE"
 fi
 echo "Starting NGINX"
 nginx -c /etc/nginx/nginx.conf -g 'daemon off;'
