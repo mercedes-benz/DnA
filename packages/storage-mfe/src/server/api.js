31473cb18d7c36f640b26d4f81cf0d396d6c03ae
vikasmadan09 <vikasmadan09@gmail.com>
2022-03-31 22:14:52 +0530

    API integration 1. include libraries 'moment' 'rc-upload' and react-hooks plugin (for eslint) 2. create a folder to maintain api's 3. rename ProtectedRoutes.js and Routes.js 4. include session expired page for microfront-end only development (won't route to this page from dna frontend) 5. ensure Pagination functionality 6. api integration for connection modal 7. include necessary redux actions and reducers 8. handle chonky actions create folder, open folder, upload and download by integrating with api 9. add necessary css changes

--- packages/storage-mfe/src/server/api.js
+++ packages/storage-mfe/src/server/api.js
@@ -1,13 +1,31 @@
 import axios from 'axios';
 const jwt = sessionStorage.length ? sessionStorage.getItem('jwt') : null;
 
+export const baseURL = process.env.API_BASEURL
+  ? `persistence/${process.env.API_BASEURL}`
+  : `http://${window.location.hostname}:7175/persistence/api`;
+
 const server = axios.create({
-  baseURL: process.env.API_BASEURL ? process.env.API_BASEURL : `http://${window.location.hostname}:7171/api/`,
+  baseURL,
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json',
     Authorization: jwt,
   },
+});
+
+server.interceptors.request.use((config) => {
+  if (['/login', '/verifyLogin'].includes(config.url)) {
+    const appUrl = process.env.API_BASEURL ? process.env.API_BASEURL : `http://${window.location.hostname}:7171/api`;
+    config.baseURL = appUrl;
+    config.data = {};
+    config.headers = {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      Authorization: jwt,
+    };
+  }
+  return config;
 });
 
 export default server;
