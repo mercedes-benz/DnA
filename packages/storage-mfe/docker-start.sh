00fbb243b30d72966fa0048c117ca4c7829aea46
Sathishkumar <sathishkumarkevin@gmail.com>
2022-04-08 13:55:52 +0530

    adding context path and docker start to find onlyprojectsmo

--- packages/storage-mfe/docker-start.sh
+++ packages/storage-mfe/docker-start.sh
@@ -12,17 +12,18 @@ echo "DNS Resolver set to: $PROJECTSMO_DNS_RESOLVER_ADDRESS"
 echo "Changing to directory and setting environment variables"
 cd /usr/share/nginx/html
 env > env.txt
-# Replace envs in compiled frontend app
-export JS_FILE=$(find . -name '*.js')
-export JS_MAP_FILE=$(find . -name '*.js.map')
+export JS_FILE=$(find . -type f -print0 | xargs -0 grep -l "PROJECTSMO")
 env > env_new.txt
 
 # echo "Processing app.js ($APP_FILE). Execute ENV replacement ..."
 # cp $APP_FILE app.bak
 # cp $LEGACY_APP_FILE legacy.bak
-echo "Processing ($JS_FILE). Execute ENV replacement ..."
-perl -p -e 's/\$\{([^}]+)\}/defined $ENV{$1} ? $ENV{$1} : $&/eg; s/\$\{([^}]+)\}//eg' -i $JS_FILE
-echo "Processing ($JS_MAP_FILE). Execute ENV replacement ..."
-perl -p -e 's/\$\{([^}]+)\}/defined $ENV{$1} ? $ENV{$1} : $&/eg; s/\$\{([^}]+)\}//eg' -i $JS_MAP_FILE
+if [ "$JS_FILE" != "" ]
+then
+    echo "Processing .js files $JS_FILE. Execute ENV replacement ..."
+    perl -p -e 's/\$\{([^}]+)\}/defined $ENV{$1} ? $ENV{$1} : $&/eg; s/\$\{PROJECTSMO_([^}])+\}//eg' -i $JS_FILE
+else
+    echo "No .js files found $JS_FILE"
+fi
 echo "Starting NGINX"
 nginx -c /etc/nginx/nginx.conf -g 'daemon off;'
