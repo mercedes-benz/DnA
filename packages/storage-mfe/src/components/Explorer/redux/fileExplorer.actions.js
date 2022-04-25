5d8ca21c56fef28604586d5a29b51822f53f4c03
vikasmadan09 <vikasmadan09@gmail.com>
2022-04-25 23:12:45 +0530

    1. remove unnecessary validation to filter falsy values 2. handle edge cases for in draft stage

--- packages/storage-mfe/src/components/Explorer/redux/fileExplorer.actions.js
+++ packages/storage-mfe/src/components/Explorer/redux/fileExplorer.actions.js
@@ -83,6 +83,7 @@ export const getFiles = (files, bucketName, fileToOpen) => {
       payload: true,
     });
     ProgressIndicator.show();
+    console.log(fileToOpen.objectName);
     try {
       const prefix = !fileToOpen.objectName ? '/' : fileToOpen.objectName;
       const res = await bucketsObjectApi.getObjects(bucketName, prefix);
@@ -169,7 +170,7 @@ export const deleteFiles = (bucketName, filesPath, files) => {
         });
         ProgressIndicator.hide();
         const pathList = filesPath?.split(',');
-        const path = pathList?.map((item) => item?.split('/')?.filter((x) => !!x));
+        const path = pathList?.map((item) => item?.split('/')?.filter((x) => x));
 
         const fileList = path?.map((item) => {
           return item[item.length - 1];
