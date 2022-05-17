17130dda9cc1ab6a1079c59c8576d41620b99737
Kumar <kurahu@apac.corpdir.net>
2022-05-17 07:45:47 +0530

    Added LOV api for bucket classification type

---
+++ packages/storage-backend/storage-lib/src/main/resources/db/migration/V1__create_tables.sql
@@ -0,0 +1,19 @@
+--
+-- DDL Script start
+--
+
+CREATE TABLE IF NOT EXISTS classification_nsql
+(
+    id               TEXT             PRIMARY KEY,
+    data             jsonb            not null       
+);
+
+CREATE TABLE IF NOT EXISTS storage_nsql
+(
+    id               TEXT             PRIMARY KEY,
+    data             jsonb            not null       
+);
+
+--
+-- DDL Script end
+--
