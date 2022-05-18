a58b021294431bc3c8586d16495445b4a18b12b1
Aluri <bealuri@apac.corpdir.net>
2022-05-18 20:08:34 +0530

    Added minio config to get models

--- packages/model-registry/model-registry-lib/src/main/java/com/mb/dna/kube/client/main/Application.java
+++ packages/model-registry/model-registry-lib/src/main/java/com/mb/dna/kube/client/main/Application.java
@@ -3,8 +3,12 @@
  */
 package com.mb.dna.kube.client.main;
 
+import java.util.Map;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import io.kubernetes.client.openapi.ApiClient;
 import io.kubernetes.client.openapi.Configuration;
@@ -13,7 +17,13 @@ import io.kubernetes.client.openapi.models.V1Container;
 import io.kubernetes.client.openapi.models.V1Pod;
 import io.kubernetes.client.openapi.models.V1PodList;
 import io.kubernetes.client.openapi.models.V1PodSpec;
+import io.kubernetes.client.openapi.models.V1Secret;
 import io.kubernetes.client.util.Config;
+import io.minio.BucketExistsArgs;
+import io.minio.ListObjectsArgs;
+import io.minio.MinioClient;
+import io.minio.Result;
+import io.minio.messages.Item;
 
 @SpringBootApplication
 public class Application {
@@ -30,12 +40,35 @@ public class Application {
         V1Pod minioPod = items.getItems().stream().filter(pod -> pod.getMetadata().getName().contains("minio")).findFirst().get();
         V1PodSpec minioPodSpec = minioPod.getSpec();
         V1Container minioContainer = minioPodSpec.getContainers().stream().filter(container -> container.getName().contains("minio")).findFirst().get();
-//        minioContainer.get
-//        String accessKey = ;
-//        String secretKey = ;
         minioContainer.getEnv().forEach(x -> System.out.println("Environment name: " + x.getName() + " , value: " + x.getValue()));
         
         
+        String minioBaseUri = "http://localhost";
+        String minioAdminAccessKeySample = "minio";
+    	String minioAdminSecretKeySample = "minio123";
+    	MinioClient minioClient =
+    		    MinioClient.builder()
+    		        .endpoint(minioBaseUri, 9000, true)
+    		        .credentials(minioAdminAccessKeySample, minioAdminSecretKeySample)
+    		        .build();
+    	boolean found =
+    			  minioClient.bucketExists(BucketExistsArgs.builder().bucket("models").build());
+    			if (found) {
+    				Iterable<Result<Item>> results = minioClient.listObjects(
+    					    ListObjectsArgs.builder().bucket("models").recursive(true).build());
+    				ObjectMapper mapper = new ObjectMapper();
+    				System.out.println(mapper.writeValueAsString(results));
+    			} else {
+    			  System.out.println("my-bucketname does not exist");
+    			}
+    			
+		V1Secret result = api.readNamespacedSecret("mlpipeline-minio-artifact", KUBEFLOW_NAMESPACE, "true" );
+        System.out.println("Got results successfully");
+        Map<String, byte[]> secretsMap = result.getData();
+        for (String key: secretsMap.keySet()) {
+            System.out.println(key + ": " + secretsMap.get(key));
+        }
+    	        
 	}catch(Exception e) {
 		e.printStackTrace();
 	}
