aaa4ea2657ac304a4e272d3e965cb82194259452
Aluri <bealuri@apac.corpdir.net>
2022-05-20 13:29:30 +0530

    Added collabs email for notifying

--- packages/storage-backend/storage-lib/src/main/java/com/daimler/data/service/storage/BaseStorageService.java
+++ packages/storage-backend/storage-lib/src/main/java/com/daimler/data/service/storage/BaseStorageService.java
@@ -173,7 +173,7 @@ public class BaseStorageService implements StorageService {
 					Map<String, String> bucketConnectionUri = dnaMinioClient.getUri(currentUser,
 							bucketVo.getBucketName(), null);
 					ownerUserVO.setUri(bucketConnectionUri.get(ConstantsUtility.URI));
-					String bucketUri = "/#/storage/explorer/" + bucketVo.getBucketName();
+					String bucketUri = bucketVo.getBucketName();
 					ownerUserVO.setHostName(bucketConnectionUri.get(ConstantsUtility.HOSTNAME));
 
 					// Setting bucket access info for owner
@@ -203,7 +203,7 @@ public class BaseStorageService implements StorageService {
 								if (onboardUserResponse != null
 										&& onboardUserResponse.getStatus().equals(ConstantsUtility.SUCCESS)) {
 									subscribedUsers.add(userVO.getAccesskey());
-									subscribedUsersEmails.add("");
+									subscribedUsersEmails.add(userVO.getEmail());
 									LOGGER.info("Collaborator:{} onboarding successfull", userVO.getAccesskey());
 								} else {
 									LOGGER.info("Collaborator:{} onboarding failed", userVO.getAccesskey());
