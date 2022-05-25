21c31b254c8750a0baee016e2f67aafb6e083739
Maria Rathinam <amariar@capelit77946372.inedc.corpintra.net>
2022-05-25 16:52:52 +0530

    changes related to env values

--- packages/storage-mfe/public/config.js
+++ packages/storage-mfe/public/config.js
@@ -1,4 +1,7 @@
 window["INJECTED_ENVIRONMENT"]={
-    CONTAINER_APP_URL:"http://localhost:9090",
-    STORAGE_API_BASEURL: "http://localhost:7175/storage/api"
-}    
+  CONTAINER_APP_URL: "http://localhost:9090",
+  API_BASEURL: "http://localhost:7171/api",
+  STORAGE_API_BASEURL: "http://localhost:7175/storage/api",
+  TOU_HTML: "<div>I agree to <a href=\"#\" target=\"_blank\" rel=\"noopener noreferrer\">terms of use</a></div>", 
+  ENABLE_DATA_CLASSIFICATION_SECRET: false
+}
