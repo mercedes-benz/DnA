d13b5e8be0b6435f3c419b3bbbd9cc0e7cf59461
Maria Rathinam <amariar@capelit77946372.inedc.corpintra.net>
2022-05-26 15:46:29 +0530

    Storage fixes and cleanup

--- packages/frontend/public/config.js
+++ packages/frontend/public/config.js
@@ -35,7 +35,13 @@ window["INJECTED_ENVIRONMENT"]={
     ENABLE_REPORTS: true,
     ENABLE_ML_PIPELINE_SERVICE: true,
     ENABLE_NOTIFICATION: true,
-    ML_PIPELINE_URL: "YOUR_ML_PIPELINE_URL",
-    TOU_HTML: "<div>I agree to <a href=\"#\" target=\"_blank\" rel=\"noopener noreferrer\">terms of use</a></div>", 
-    ENABLE_DATA_CLASSIFICATION_SECRET: false,
+    ML_PIPELINE_URL: "YOUR_ML_PIPELINE_URL"
   };
+
+  window["STORAGE_INJECTED_ENVIRONMENT"]={
+    CONTAINER_APP_URL: "http://localhost:9090",
+    API_BASEURL: "http://localhost:7171/api",
+    STORAGE_API_BASEURL: "http://localhost:7175/storage/api",
+    TOU_HTML: "<div>I agree to <a href=\"#\" target=\"_blank\" rel=\"noopener noreferrer\">terms of use</a></div>", 
+    ENABLE_DATA_CLASSIFICATION_SECRET: false
+  }
