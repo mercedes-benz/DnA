37df2e843ad2047e972460333ac5575ad5879a34
Aluri <bealuri@apac.corpdir.net>
2022-05-26 10:35:16 +0530

    added component scan

--- packages/model-registry/model-registry-lib/src/main/java/com/daimler/data/registry/main/Application.java
+++ packages/model-registry/model-registry-lib/src/main/java/com/daimler/data/registry/main/Application.java
@@ -32,15 +32,27 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.web.WebApplicationInitializer;
 
 @SpringBootApplication
-@ComponentScan({ "com.daimler.data"}) 
-public class Application {
+@EnableAspectJAutoProxy(proxyTargetClass = true)
+@ComponentScan({ "com.daimler.data.application.filter", "com.daimler.data.application.logging", "com.daimler.data.auth.client",
+	"com.daimler.data.controller", "com.daimler.data.registry.config", "com.daimler.data.registry.models.service" }) 
+public class Application extends SpringBootServletInitializer implements WebApplicationInitializer {
 
 	private static final Logger LOG = LoggerFactory.getLogger(Application.class);
 
 	public static void main(String[] args) {
+		LOG.info("Starting up the model-registry {} application");
 		SpringApplication.run(Application.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(Application.class);
 	}
 }
