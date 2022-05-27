b4b26796a9c60a85986ab644402cbb7736d77f41
Kumar <princku@apac.corpdir.net>
2022-05-27 13:52:38 +0530

    model registry adding JsonIgnoreProperties

--- packages/model-registry/model-registry-lib/src/main/java/com/daimler/data/dto/MinioKeyDetails.java
+++ packages/model-registry/model-registry-lib/src/main/java/com/daimler/data/dto/MinioKeyDetails.java
@@ -27,6 +27,8 @@
 
 package com.daimler.data.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -34,9 +36,10 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class MinioKeyDetails {
 
 	private String accessKey;
 	private String secretKey;
-	
+
 }
