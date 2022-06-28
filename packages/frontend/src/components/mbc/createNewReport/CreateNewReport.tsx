b2a7f8e9402a5d5b60db840e74dcc9fa8523aaa9
Shubham <sbm_verma31@rediffmail.com>
2022-06-28 12:05:16 +0530

    changed logic syntax

--- packages/frontend/src/components/mbc/createNewReport/CreateNewReport.tsx
+++ packages/frontend/src/components/mbc/createNewReport/CreateNewReport.tsx
@@ -322,7 +322,7 @@ export default class CreateNewReport extends React.Component<ICreateNewReportPro
               isProductOwner !== undefined ||
               // user.id === (res.createdBy ? res.createdBy.id : '')
               res.members.admin.find((teamMember) => teamMember.shortId === user.id) !== undefined ||
-              (user?.divisionAdmins ? user?.divisionAdmins.includes(res?.description?.division?.name) : false)
+              (user?.divisionAdmins && user?.divisionAdmins.includes(res?.description?.division?.name))
             ) {
               const response = this.state.response;
               const {
