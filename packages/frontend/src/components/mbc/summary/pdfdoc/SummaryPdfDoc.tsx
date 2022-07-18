3140be5286f9a42e2a6d516a982b0298a25dd3a5
Shubham <sbm_verma31@rediffmail.com>
2022-07-18 14:15:52 +0530

    Added not applicable if empty

--- packages/frontend/src/components/mbc/summary/pdfdoc/SummaryPdfDoc.tsx
+++ packages/frontend/src/components/mbc/summary/pdfdoc/SummaryPdfDoc.tsx
@@ -845,14 +845,14 @@ export const SummaryPdfDoc = (props: SummaryPdfDocProps) => (
             
             <Text style={[styles.subTitle, styles.setMarginTop]}>Rollout Locations</Text>
             <View style={{ display: 'flex', flexDirection: 'row', marginBottom: 25, marginTop: 25 }}>
-            {props.solution.milestones?.rollouts?.details && props.solution.milestones?.rollouts?.details.map((rollout, index) => {
+            {props.solution.milestones?.rollouts?.details && props.solution.milestones?.rollouts?.details.length > 0 ? props.solution.milestones?.rollouts?.details.map((rollout, index) => {
                 return (
                   <Text key={index}>
                     {rollout.location.name}({rollout.month > 0 && rollout.year > 0 ? regionalForMonthAndYear(rollout.month+'/'+'01'+'/'+rollout.year):''})
                     { index <= props.solution.milestones.rollouts.details.length-2 ? ', ' : '' }
                   </Text>
                 );
-              })}
+              }) : <Text>N/A</Text>}
             </View>
             <View style={styles.seperatorLine} />
           </View>
