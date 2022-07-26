ed6fce83f71e67c265860a75c47460567dd90123
Aluri <bealuri@apac.corpdir.net>
2022-07-26 09:26:11 +0530

    Changed cache load from postconstruct of bean to on event of application started

---
+++ packages/naas/naas-lib/src/main/java/com/daimler/dna/notifications/common/event/config/LoadCache.java
@@ -0,0 +1,27 @@
+package com.daimler.dna.notifications.common.event.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+public class LoadCache {
+
+	@Autowired
+	private CacheUpdateEventListener cacheUpdater;
+	
+	@EventListener(ApplicationReadyEvent.class)
+	public void runAfterStartup() {
+		try {
+	    cacheUpdater.init();
+	    log.info("Successfully completed loading messages from topic to users cache based on preferences");
+		}catch(Exception e) {
+			log.error("Failed while loading messages from topic to users cache based on preferences, with exception : {} ",e.getMessage());
+		}
+	}
+	
+}
