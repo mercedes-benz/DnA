82881bfdb6d164f9dcaaf4222ce3b3afc99fc45f
Kumar <princku@apac.corpdir.net>
2022-08-16 10:00:19 +0530

    authentication implementation

--- packages/model-registry/model-registry-lib/src/main/java/com/daimler/data/registry/util/AppIdGenerator.java
+++ packages/model-registry/model-registry-lib/src/main/java/com/daimler/data/registry/util/AppIdGenerator.java
@@ -27,17 +27,7 @@
 
 package com.daimler.data.registry.util;
 
-import java.nio.charset.StandardCharsets;
-import java.security.spec.KeySpec;
-import java.util.Base64;
-
-import javax.crypto.Cipher;
-import javax.crypto.SecretKey;
-import javax.crypto.SecretKeyFactory;
-import javax.crypto.spec.IvParameterSpec;
-import javax.crypto.spec.PBEKeySpec;
-import javax.crypto.spec.SecretKeySpec;
-
+import org.apache.commons.codec.digest.HmacUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -52,55 +42,20 @@ public class AppIdGenerator {
 
 	private static String SECRET_KEY;
 
-	private static String SALT;
-
-	@Value("${aes.secret.key}")
+	@Value("${hmac.secret.key}")
 	public void setSecretKey(String secretKey) {
 		SECRET_KEY = secretKey;
 	}
 
-	@Value("${aes.secret.salt}")
-	public void setSalt(String salt) {
-		SALT = salt;
-	}
-
-	public static String encrypt(String modelName) {
+	public static String encrypt(String dataToEncrypt) {
+		String algorithm = "HmacMD5";
 		try {
-			byte[] iv = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-			IvParameterSpec ivspec = new IvParameterSpec(iv);
-
-			SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
-
-			KeySpec spec = new PBEKeySpec(SECRET_KEY.toCharArray(), SALT.getBytes(), 65536, 256);
-			SecretKey tmp = factory.generateSecret(spec);
-			SecretKeySpec secretKey = new SecretKeySpec(tmp.getEncoded(), "AES");
-			Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding");
-			cipher.init(Cipher.ENCRYPT_MODE, secretKey, ivspec);
-			return Base64.getEncoder().encodeToString(cipher.doFinal(modelName.getBytes(StandardCharsets.UTF_8)));
+			String hmac = new HmacUtils(algorithm, SECRET_KEY).hmacHex(dataToEncrypt);
+			return hmac;
 		} catch (Exception e) {
 			LOGGER.error("Error while encrypting: {}", e.getMessage());
 			return null;
 		}
-	}
 
-	public static String decrypt(String appId) {
-		try {
-			byte[] iv = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-
-			IvParameterSpec ivspec = new IvParameterSpec(iv);
-
-			SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
-
-			KeySpec spec = new PBEKeySpec(SECRET_KEY.toCharArray(), SALT.getBytes(), 65536, 256);
-			SecretKey tmp = factory.generateSecret(spec);
-			SecretKeySpec secretKey = new SecretKeySpec(tmp.getEncoded(), "AES");
-
-			Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding");
-			cipher.init(Cipher.DECRYPT_MODE, secretKey, ivspec);
-			return new String(cipher.doFinal(Base64.getDecoder().decode(appId)));
-		} catch (Exception e) {
-			LOGGER.error("Error while decrypting: {}", e.getMessage());
-			return null;
-		}
 	}
 }
