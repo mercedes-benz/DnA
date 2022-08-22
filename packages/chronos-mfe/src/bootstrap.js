2b84855210983f0e3f071cfa3c59d8454cb9cfc6
vikasmadan09 <vikasmadan09@gmail.com>
2022-08-22 20:50:58 +0530

    1. Add Chronos Micro Front End package 'chronos-mfe' 2. include all necessary changes needed to setup mfe 3. include create and edit functionality with mock data

---
+++ packages/chronos-mfe/src/bootstrap.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// import css
+import './common/modules/uilab/bundle/css/uilab.min.css';
+import './common/globals/css/main.scss';
+
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
