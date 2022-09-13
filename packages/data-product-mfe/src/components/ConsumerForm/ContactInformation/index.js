0d57c6d766af3e3fe3533edb9ade0dec26e23cc1
vikasmadan09 <vikasmadan09@gmail.com>
2022-09-13 11:30:16 +0530

    1. enable delete and edit of provider form only for the creator 2. In consumer form, providersummary tab should show only the filled in details 3. If provider form is in Draft state, disable CTA items 4. set notifyUsers values in each of the tabs 5. if consumer form product id is invalid route to NotFoundPage 6. include "type" of form whether provider or consumer while serializing and deserialzing the form data

--- packages/data-product-mfe/src/components/ConsumerForm/ContactInformation/index.js
+++ packages/data-product-mfe/src/components/ConsumerForm/ContactInformation/index.js
@@ -12,19 +12,22 @@ import { hostServer } from '../../../server/api';
 import { dataProductsApi } from '../../../apis/dataproducts.api';
 
 import ProgressIndicator from '../../../common/modules/uilab/js/src/progress-indicator';
+import { useSelector } from 'react-redux';
 
-const ContactInformation = ({ onSave, divisions, setSubDivisions, subDivisions }) => {
+const ContactInformation = ({ onSave, divisions, setSubDivisions, subDivisions, isFormMounted }) => {
   const {
     register,
     formState: { errors, isSubmitting, dirtyFields },
     watch,
-    resetField,
     handleSubmit,
     trigger,
     reset,
     control,
+    setValue,
   } = useFormContext();
   const [showInfoModal, setShowInfoModal] = useState(false);
+
+  const { division, complianceOfficer: selectedcomplianceOfficer } = watch();
 
   const [complianceOfficerList, setComplianceOfficerList] = useState({
     records: [],
@@ -32,50 +35,67 @@ const ContactInformation = ({ onSave, divisions, setSubDivisions, subDivisions }
   });
   const [complianceOfficer, setComplianceOfficer] = useState([]);
 
+  const provideDataProducts = useSelector((state) => state.provideDataProducts);
+
   useEffect(() => {
     const id = watch('division');
-    if (id > 0) {
+    if (id > '0') {
       ProgressIndicator.show();
       hostServer.get('/subdivisions/' + id).then((res) => {
-        setSubDivisions(res.data);
-        (dirtyFields.division || dirtyFields.subDivision) && resetField('subDivision', { defaultValue: '0' });
+        setSubDivisions(res?.data || []);
+        if (!dirtyFields.division && !dirtyFields.subDivision) {
+          setValue('subDivision', provideDataProducts.selectedDataProduct.consumer.subDivision);
+        } else {
+          setValue('subDivision', '0');
+        }
         SelectBox.defaultSetup();
         ProgressIndicator.hide();
       });
     } else {
-      resetField('subDivision', { defaultValue: '0' });
-      (dirtyFields.division || dirtyFields.subDivision) && SelectBox.defaultSetup();
+      setSubDivisions([]);
     }
     //eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [watch('division')]);
+  }, [division]);
 
   useEffect(() => {
-    SelectBox.defaultSetup();
     reset(watch());
     //eslint-disable-next-line
   }, []);
 
   useEffect(() => {
-    ProgressIndicator.show();
-    dataProductsApi
-      .getDataComplianceList(0, 0, 'entityId', 'asc')
-      .then((res) => {
-        res.data?.records?.map((item) => {
-          item['name'] = item.localComplianceOfficer.toString();
-          return item;
+    if (isFormMounted) {
+      ProgressIndicator.show();
+      dataProductsApi
+        .getDataComplianceList(0, 0, 'entityId', 'asc')
+        .then((res) => {
+          res.data?.records?.map((item) => {
+            item['name'] = item.localComplianceOfficer.toString();
+            return item;
+          });
+          setComplianceOfficerList(res.data);
+          ProgressIndicator.hide();
+        })
+        .catch((e) => {
+          ProgressIndicator.hide();
+          Notification.show(
+            e.response?.data?.errors?.[0]?.message || 'Error while fethcing data compliance officers list.',
+            'alert',
+          );
         });
-        setComplianceOfficerList(res.data);
-        ProgressIndicator.hide();
-      })
-      .catch((e) => {
-        ProgressIndicator.hide();
-        Notification.show(
-          e.response?.data?.errors?.[0]?.message || 'Error while fethcing data compliance officers list.',
-          'alert',
-        );
-      });
+    }
     //eslint-disable-next-line
-  }, []);
+  }, [isFormMounted]);
+
+  useEffect(() => {
+    // if division is set to "Choose", reset sub divison dropdown to empty list
+    division === '0' && SelectBox.defaultSetup();
+  }, [division, subDivisions.length]);
+
+  useEffect(() => {
+    if (selectedcomplianceOfficer?.length) {
+      setComplianceOfficer(selectedcomplianceOfficer);
+    }
+  }, [selectedcomplianceOfficer]);
 
   return (
     <>
@@ -89,12 +109,12 @@ const ContactInformation = ({ onSave, divisions, setSubDivisions, subDivisions }
           </div>
           <div className={Styles.formWrapper}>
             <div className={Styles.flexLayout}>
-              <div className={classNames('input-field-group include-error', errors.businessOwner ? 'error' : '')}>
+              <div className={classNames('input-field-group include-error', errors.businessOwnerName ? 'error' : '')}>
                 <label id="businessOwnerLabel" htmlFor="businessOwnerInput" className="input-label">
                   Business and/or Information Owner <sup>*</sup>
                 </label>
                 <input
-                  {...register('businessOwner', { required: '*Missing entry' })}
+                  {...register('businessOwnerName', { required: '*Missing entry' })}
                   type="text"
                   className="input-field"
                   id="businessOwnerInput"
@@ -102,7 +122,7 @@ const ContactInformation = ({ onSave, divisions, setSubDivisions, subDivisions }
                   placeholder="Type here"
                   autoComplete="off"
                 />
-                <span className={classNames('error-message')}>{errors.businessOwner?.message}</span>
+                <span className={classNames('error-message')}>{errors.businessOwnerName?.message}</span>
               </div>
               <div className={classNames(Styles.flexLayout)}>
                 <div className={Styles.divisionContainer}>
@@ -121,7 +141,6 @@ const ContactInformation = ({ onSave, divisions, setSubDivisions, subDivisions }
                         required-error={'*Missing entry'}
                         {...register('division', {
                           validate: (value) => value !== '0' || '*Missing entry',
-                          // onChange: () => resetField('subDivision'),
                         })}
                       >
                         <option id="divisionOption" value={0}>
@@ -184,6 +203,45 @@ const ContactInformation = ({ onSave, divisions, setSubDivisions, subDivisions }
               </div>
             </div>
             <div className={Styles.flexLayout}>
+              <div
+                className={classNames(`input-field-group include-error ${errors?.lcoNeeded ? 'error' : ''}`)}
+                style={{ minHeight: '50px' }}
+              >
+                <label className={classNames(Styles.inputLabel, 'input-label')}>
+                  Is LCO/LCR needed to be involved / has to check legal basis of usage of personal data? <sup>*</sup>
+                </label>
+                <div className={Styles.radioBtns}>
+                  <label className={'radio'}>
+                    <span className="wrapper">
+                      <input
+                        {...register('lcoNeeded', {
+                          required: '*Missing entry',
+                        })}
+                        type="radio"
+                        className="ff-only"
+                        name="lcoNeeded"
+                        value="No"
+                      />
+                    </span>
+                    <span className="label">No</span>
+                  </label>
+                  <label className={'radio'}>
+                    <span className="wrapper">
+                      <input
+                        {...register('lcoNeeded', { required: '*Missing entry' })}
+                        type="radio"
+                        className="ff-only"
+                        name="lcoNeeded"
+                        value="Yes"
+                      />
+                    </span>
+                    <span className="label">Yes</span>
+                  </label>
+                </div>
+                <span className={classNames('error-message')}>{errors?.lcoNeeded?.message}</span>
+              </div>
+            </div>
+            <div className={Styles.flexLayout}>
               <div className={classNames('input-field-group include-error', errors.complianceOfficer ? 'error' : '')}>
                 <Controller
                   control={control}
@@ -238,10 +296,10 @@ const ContactInformation = ({ onSave, divisions, setSubDivisions, subDivisions }
           className="btn btn-primary"
           type="submit"
           disabled={isSubmitting}
-          onClick={handleSubmit((data) => {
-            console.log(data);
-            onSave(data);
-            reset(data, {
+          onClick={handleSubmit(() => {
+            setValue('notifyUsers', false);
+            onSave(watch());
+            reset(watch(), {
               keepDirty: false,
             });
           })}
