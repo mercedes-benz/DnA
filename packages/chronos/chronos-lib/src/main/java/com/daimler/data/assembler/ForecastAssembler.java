ac8e94c61ecdd140b33b6ca3ef50276569398ab0
Aluri <bealuri@apac.corpdir.net>
2022-09-20 09:49:01 +0530

    Chronos init checkin

---
+++ packages/chronos/chronos-lib/src/main/java/com/daimler/data/assembler/ForecastAssembler.java
@@ -0,0 +1,21 @@
+package com.daimler.data.assembler;
+
+import com.daimler.data.db.entities.ForecastNsql;
+import com.daimler.data.dto.forecast.ForecastVO;
+
+public class ForecastAssembler implements GenericAssembler<ForecastVO, ForecastNsql>{
+
+	@Override
+	public ForecastVO toVo(ForecastNsql e) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ForecastNsql toEntity(ForecastVO vo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+}
