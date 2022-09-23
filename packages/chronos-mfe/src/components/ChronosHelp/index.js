29055bd42ea2afc571f9161d1431d2b233c4fb6c
Khartade <khartaa@apac.corpdir.net>
2022-09-23 16:10:29 +0530

    chronos ui changes

--- packages/chronos-mfe/src/components/ChronosHelp/index.js
+++ packages/chronos-mfe/src/components/ChronosHelp/index.js
@@ -4,19 +4,16 @@ import { Link } from 'react-router-dom';
 import Styles from './styles.scss';
 
 import help from '../../assets/help.png';
+import Breadcrumb from '../shared/breadcrumb/Breadcrumb';
 
 const ChronosHelp = () => {
   return (
     <>
       <div className={classNames(Styles.mainPanel)}>
-        <div className={classNames(Styles.breadcrumb)}>
-          <ol>
-            <li><a href='#/'>Start</a></li>
-            <li><a href='#/services'>My Services</a></li>
-            <li><Link to='/'>Chronos Forecasting</Link></li>
-            <li>Help</li>
-          </ol>
-        </div>
+        <Breadcrumb>
+          <li><Link to='/'>Chronos Forecasting</Link></li>
+          <li>Help</li>
+        </Breadcrumb>
         <h3 className={classNames(Styles.title)}>Chronos Help</h3>
         <div className={classNames(Styles.wrapper)}>
             <div className={Styles.container}>
