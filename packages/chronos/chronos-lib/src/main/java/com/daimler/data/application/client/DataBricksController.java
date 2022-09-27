823e1442232881fc9aafc700539b5009df4b01aa
Aluri <bealuri@apac.corpdir.net>
2022-09-27 10:05:47 +0530

    updated new databricks api changes after discussion

--- packages/chronos/chronos-lib/src/main/java/com/daimler/data/application/client/DataBricksController.java
+++ packages/chronos/chronos-lib/src/main/java/com/daimler/data/application/client/DataBricksController.java
@@ -20,24 +20,24 @@ import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @Api(value = "Forecast Run update API")
-@RequestMapping("/client/databricks/forecasts/runs")
+@RequestMapping("/client/databricks}")
 @Slf4j
 public class DataBricksController {
 
-//	@ApiOperation(value = "api for databricks to update the run details upon finishing the run", nickname = "updateRun", notes = "", response = GenericMessage.class, tags={ "forecast-runs", })
-//    @ApiResponses(value = { 
-//        @ApiResponse(code = 201, message = "Returns message of success or failure ", response = GenericMessage.class),
-//        @ApiResponse(code = 400, message = "Bad Request", response = GenericMessage.class),
-//        @ApiResponse(code = 401, message = "Request does not have sufficient credentials."),
-//        @ApiResponse(code = 403, message = "Request is not authorized."),
-//        @ApiResponse(code = 405, message = "Method not allowed"),
-//        @ApiResponse(code = 500, message = "Internal error") })
-//    @RequestMapping(value = "/forecasts/{id}/runs/{rid}",
-//        produces = { "application/json" }, 
-//        consumes = { "application/json" },
-//        method = RequestMethod.PUT)
-//    public ResponseEntity<GenericMessage> updateRun(@ApiParam(value = "Request Body that contains updated details of run" ,required=true )  @Valid @RequestBody RunUpdateRequestWrapperVO forecastRunUpdateRequestVO){
-//		return null;
-//	}
+	@ApiOperation(value = "api for databricks to update the run details upon finishing the run", nickname = "updateRun", notes = "", response = GenericMessage.class, tags={ "forecast-runs", })
+    @ApiResponses(value = { 
+        @ApiResponse(code = 201, message = "Returns message of success or failure ", response = GenericMessage.class),
+        @ApiResponse(code = 400, message = "Bad Request", response = GenericMessage.class),
+        @ApiResponse(code = 401, message = "Request does not have sufficient credentials."),
+        @ApiResponse(code = 403, message = "Request is not authorized."),
+        @ApiResponse(code = 405, message = "Method not allowed"),
+        @ApiResponse(code = 500, message = "Internal error") })
+    @RequestMapping(value = "/forecasts/runs/{correlationId",
+        produces = { "application/json" }, 
+        consumes = { "application/json" },
+        method = RequestMethod.PUT)
+    public ResponseEntity<GenericMessage> updateRun(@ApiParam(value = "Request Body that contains updated details of run" ,required=true )  @Valid @RequestBody RunUpdateRequestWrapperVO forecastRunUpdateRequestVO){
+		return null;
+	}
     
 }
