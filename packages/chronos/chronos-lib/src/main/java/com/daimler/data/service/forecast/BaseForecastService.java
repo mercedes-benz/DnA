fa8a62f62e1ae2a4c0ff6f92dc4e4b7adb2a62e9
Aluri <bealuri@apac.corpdir.net>
2022-10-03 13:51:33 +0530

    delete changes

--- packages/chronos/chronos-lib/src/main/java/com/daimler/data/service/forecast/BaseForecastService.java
+++ packages/chronos/chronos-lib/src/main/java/com/daimler/data/service/forecast/BaseForecastService.java
@@ -4,9 +4,11 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -25,10 +27,13 @@ import com.daimler.data.db.json.RunState;
 import com.daimler.data.db.repo.forecast.ForecastCustomRepository;
 import com.daimler.data.db.repo.forecast.ForecastRepository;
 import com.daimler.data.dto.databricks.RunNowNotebookParamsDto;
+import com.daimler.data.dto.forecast.DataBricksErrorResponseVO;
 import com.daimler.data.dto.forecast.ForecastRunResponseVO;
 import com.daimler.data.dto.forecast.ForecastVO;
 import com.daimler.data.dto.forecast.InputFileVO;
+import com.daimler.data.dto.forecast.RunDetailsVO;
 import com.daimler.data.dto.forecast.RunNowResponseVO;
+import com.daimler.data.dto.forecast.RunStateVO;
 import com.daimler.data.dto.forecast.RunVO;
 import com.daimler.data.dto.storage.CreateBucketResponseWrapperDto;
 import com.daimler.data.dto.storage.FileUploadResponseDto;
@@ -56,6 +61,7 @@ public class BaseForecastService extends BaseCommonService<ForecastVO, ForecastN
 	private ForecastCustomRepository customRepo;
 	@Autowired
 	private ForecastRepository jpaRepo;
+	
 	@Autowired
 	private ForecastAssembler assembler;
 
@@ -99,12 +105,14 @@ public class BaseForecastService extends BaseCommonService<ForecastVO, ForecastN
 	public ForecastRunResponseVO createJobRun(String savedInputPath, Boolean saveRequestPart, String runName,
 			String configurationFile, String frequency, BigDecimal forecastHorizon, String comment,
 			ForecastVO existingForecast,String triggeredBy, Date triggeredOn) {
+		this.jpaRepo.findAll();
 		ForecastRunResponseVO responseWrapper = new ForecastRunResponseVO();
 		RunNowResponseVO runNowResponseVO = new RunNowResponseVO();
 		GenericMessage responseMessage = new GenericMessage();
 		RunNowNotebookParamsDto noteboookParams = new RunNowNotebookParamsDto();
 		String correlationId = UUID.randomUUID().toString();
 		String bucketName = existingForecast.getBucketName();
+		noteboookParams.setConfig(dataBricksJobDefaultConfigYml);
 		noteboookParams.setCorrelationId(correlationId);
 		noteboookParams.setExcel("");
 		noteboookParams.setFh(forecastHorizon.toString());
@@ -138,6 +146,7 @@ public class BaseForecastService extends BaseCommonService<ForecastVO, ForecastN
 				currentRun.setRunName(runName);
 				currentRun.setTriggeredBy(triggeredBy);
 				currentRun.setTriggeredOn(triggeredOn);
+				currentRun.setIsDelete(false);
 				RunState newRunState = new RunState();
 				newRunState.setLife_cycle_state("PENDING");
 				newRunState.setUser_cancelled_or_timedout(false);
@@ -179,14 +188,112 @@ public class BaseForecastService extends BaseCommonService<ForecastVO, ForecastN
 
 	@Override
 	public Long getRunsCount(String id) {
-		// TODO Auto-generated method stub
-		return null;
+		return customRepo.getTotalRunsCount(id);
 	}
 
 	@Override
-	public List<RunVO> getAllRunsForProject(Integer limit, Integer offset, ForecastVO existingForecast) {
-		// TODO Auto-generated method stub
-		return null;
+	@Transactional
+	public List<RunVO> getAllRunsForProject(int limit, int offset, ForecastVO existingForecast) {
+		
+		List<RunDetails> updatedRuns = new ArrayList<>();
+		List<RunVO> updatedRunVOList = new ArrayList<>();
+		
+		Optional<ForecastNsql> entityOptional = jpaRepo.findById(existingForecast.getId());
+		if(entityOptional!=null) {
+			ForecastNsql entity = entityOptional.get();
+			if(entity!=null && entity.getData()!=null && 
+					entity.getData().getRuns()!=null && !entity.getData().getRuns().isEmpty()) {
+				List<RunDetails> existingRuns = entity.getData().getRuns();
+				for(RunDetails run: existingRuns) {
+					RunState state = run.getRunState();
+					String runId = run.getRunId();
+					if(runId!=null && (run.getIsDelete() == null || !run.getIsDelete()) && 
+							(state==null || state.getResult_state()==null || state.getLife_cycle_state()==null ||
+							"PENDING".equalsIgnoreCase(state.getLife_cycle_state()) || 
+							"RUNNING".equalsIgnoreCase(state.getLife_cycle_state()))) {
+						RunDetailsVO updatedRunResponse = this.dataBricksClient.getSingleRun(runId);
+						if(updatedRunResponse!=null && runId.equals(updatedRunResponse.getRunId())) {
+							RunDetails updatedRunDetail = new RunDetails();
+							BeanUtils.copyProperties(run, updatedRunDetail);
+							updatedRunDetail.setCreatorUserName(updatedRunResponse.getCreatorUserName());
+							if(updatedRunResponse.getEndTime()!=null) 
+								updatedRunDetail.setEndTime(updatedRunResponse.getEndTime().longValue());
+							if(updatedRunResponse.getExecutionDuration()!=null) 
+								updatedRunDetail.setExecutionDuration(updatedRunResponse.getExecutionDuration().longValue());
+							if(updatedRunResponse.getSetupDuration()!=null) 
+								updatedRunDetail.setSetupDuration(updatedRunResponse.getSetupDuration().longValue());
+							if(updatedRunResponse.getStartTime()!=null)
+								updatedRunDetail.setStartTime(updatedRunResponse.getStartTime().longValue());
+							if(updatedRunResponse.getState()!=null) {
+								RunStateVO updatedState = updatedRunResponse.getState();
+								RunState newState = new RunState();
+								if(updatedState.getLifeCycleState()!=null)
+									newState.setLife_cycle_state(updatedState.getLifeCycleState().name());
+								if(updatedState.getResultState()!=null)
+									newState.setResult_state(updatedState.getResultState().name());
+								String updatedStateMsg = "";
+								if(updatedRunResponse.getState().getStateMessage()!=null) {
+									updatedStateMsg = updatedRunResponse.getState().getStateMessage() + ". " + updatedState.getStateMessage();
+								}
+								newState.setState_message(updatedStateMsg);
+								newState.setUser_cancelled_or_timedout(updatedState.isUserCancelledOrTimedout());
+								updatedRunDetail.setRunState(newState);
+							}
+							updatedRuns.add(updatedRunDetail);
+						}else {
+							updatedRuns.add(run);
+						}
+					}else {
+						updatedRuns.add(run);
+					}
+						
+				}
+				entity.getData().setRuns(updatedRuns);
+				this.jpaRepo.save(entity);
+				updatedRunVOList = this.assembler.toRunsVO(updatedRuns);
+			}
+					
+		}
+		return updatedRunVOList;
+	}
+
+	@Override
+	public GenericMessage deletRunByUUID(String id, String rid) {
+		GenericMessage responseMessage = new GenericMessage();
+		List<MessageDescription> errors = new ArrayList<>();
+		List<MessageDescription> warnings = new ArrayList<>();
+		Optional<ForecastNsql> entityOptional = jpaRepo.findById(id);
+		if(entityOptional!=null) {
+			ForecastNsql entity = entityOptional.get();
+			List<RunDetails> existingRuns = entity.getData().getRuns();
+			List<RunDetails> updatedRuns = new ArrayList<>();
+			for(RunDetails run : existingRuns) {
+				if(rid.equalsIgnoreCase(run.getId())) {
+					DataBricksErrorResponseVO errResponse = this.dataBricksClient.deleteRun(run.getRunId());
+					if(errResponse!=null && (errResponse.getErrorCode()!=null || errResponse.getMessage()!=null)) {
+						String msg = "Failed to delete Run." ;
+						if(errResponse.getErrorCode()!=null) {
+							msg+= errResponse.getErrorCode();
+						}
+						if(errResponse.getMessage()!=null) {
+							msg+= errResponse.getMessage();
+						}
+						MessageDescription errMsg = new MessageDescription(msg);
+						errors.add(errMsg);
+						responseMessage.setSuccess("FAILED");
+						responseMessage.setErrors(errors);
+						return responseMessage;
+					}else {
+						run.setIsDelete(true);
+					}
+				}
+				updatedRuns.add(run);
+			}
+			entity.getData().setRuns(updatedRuns);
+			jpaRepo.save(entity);
+		}
+		responseMessage.setSuccess("SUCCESS");
+		return responseMessage;
 	}
 
 	
