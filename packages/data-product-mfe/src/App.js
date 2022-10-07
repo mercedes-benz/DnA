71e7e965fc8eab35326da6a2e5fe9af25998d63e
vikasmadan09 <vikasmadan09@gmail.com>
2022-10-07 14:37:29 +0530

    1. update tiles in the Home page 2. include insiderInformation field under Provider form 3. update insiderInformation field in the summary pages 4. sort data products by dataProductId 5. modify Notfoundpage to display proper link text

--- packages/data-product-mfe/src/App.js
+++ packages/data-product-mfe/src/App.js
@@ -3,11 +3,18 @@ import React, { useEffect } from 'react';
 import { Provider } from 'react-redux';
 import store, { history } from './store';
 
-import Routes from './components/DataProductRoutes';
+import Routes, { protectedRoutes } from './components/DataProductRoutes';
 
 const App = ({ user, ...rest }) => {
   useEffect(() => {
-    user?.roles?.length > 0 && rest.history?.location?.pathname === '/dataproduct' && history.replace('/');
+    const pathName = rest.history?.location?.pathname?.replace('/dataproduct', '') || '/';
+    const isValidRoute = protectedRoutes.find((route) => pathName === route.path)?.path;
+
+    if (user?.roles?.length > 0 && rest.history?.location?.pathname.includes('/dataproduct')) {
+      if (isValidRoute) {
+        history.replace(isValidRoute);
+      }
+    }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   return (
