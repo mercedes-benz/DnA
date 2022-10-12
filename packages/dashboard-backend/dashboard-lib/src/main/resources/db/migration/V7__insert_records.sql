9e3bf55766b16d37d8e04e4ffb5b33c60b4dc78d
Kumar <princku@apac.corpdir.net>
2022-10-12 12:18:27 +0530

    report new changes

---
+++ packages/dashboard-backend/dashboard-lib/src/main/resources/db/migration/V7__insert_records.sql
@@ -0,0 +1,5 @@
+INSERT INTO data_classification_sql(id,data)
+VALUES('1','{"name": "Public"}'),
+      ('2','{"name": "Confidential"}'),
+	  ('3','{"name": "Internal"}'),
+	  ('4','{"name": "Secret"}');
