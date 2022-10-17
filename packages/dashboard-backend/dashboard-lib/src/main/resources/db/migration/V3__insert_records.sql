48abb1175610d02ea85b7fdd9f2b76a36541b3b2
Kumar <princku@apac.corpdir.net>
2022-10-17 14:16:01 +0530

    report data clean up

---
+++ packages/dashboard-backend/dashboard-lib/src/main/resources/db/migration/V3__insert_records.sql
@@ -0,0 +1,15 @@
+INSERT INTO agile_releasetrain_sql (id, name) VALUES
+(DEFAULT, 'ART 1'),
+(DEFAULT,'ART 2');
+
+INSERT INTO common_function_sql (id, name) VALUES
+(DEFAULT, 'CommonFunction 1'),
+(DEFAULT,'CommonFunction 2');
+
+INSERT INTO datawarehouse_sql (id, name) VALUES
+(DEFAULT, 'DataWarehouse 1'),
+(DEFAULT,'DataWarehouse 2');
+
+INSERT INTO legal_entity_sql (id, name) VALUES
+(DEFAULT, 'LegalEntity 1'),
+(DEFAULT,'LegalEntity 2');
