5f3e485818ac0236ca5b48bf995c5c12d1ea1b8c
Shubham <sbm_verma31@rediffmail.com>
2022-10-18 08:53:39 +0530

    changed comment to description

--- packages/frontend/src/components/mbc/reportSummary/kpi/KpiSummary.tsx
+++ packages/frontend/src/components/mbc/reportSummary/kpi/KpiSummary.tsx
@@ -72,7 +72,7 @@ export default class KpiSummary extends React.Component<IKpiProps> {
                       <div className={Styles.commentSection}>
                         <label className="input-label summary">Comment</label>
                         <p>
-                          <pre className={Styles.commentPre}>{kpi.comment}</pre>
+                          <pre className={Styles.commentPre}>{kpi.description}</pre>
                         </p>
                       </div>
                       {this.props.kpis?.length > 1 && <hr className="divider1" />}
