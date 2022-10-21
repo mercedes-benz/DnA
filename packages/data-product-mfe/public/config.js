a92f56da45760b788752c5fd13bb479f8fd77bb4
vikasmadan09 <vikasmadan09@gmail.com>
2022-10-21 18:58:03 +0530

    update sample value for information policy link make link to open in new tab

--- packages/data-product-mfe/public/config.js
+++ packages/data-product-mfe/public/config.js
@@ -5,5 +5,5 @@ window['DATA_PRODUCT_INJECTED_ENVIRONMENT'] = {
     '<p>If yes, and due to the complexity of corresponding Data Governance requirements, we recommend you reach out to us at the <a href="#" target="_blank" rel="noopener noreferrer"> Data Governance Office FM </a> directly.</p>',
   DATA_PRODUCT_TOU_HTML:
     '<div>On behalf of my above mentioned Business Owner, I confirm the corresponding minimum information to be correct and complete to the best of my knowledge. I accept the conditions.</div>',
-  INFORMATION_POLICY_LINK: '#/dataproduct',
+  INFORMATION_POLICY_LINK: 'YOUR_INFORMATION_POLICY_LINK',
 };
