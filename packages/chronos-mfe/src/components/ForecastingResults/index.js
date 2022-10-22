6baf6c352f2a381272aebe26f485e92646419fa7
Khartade <khartaa@apac.corpdir.net>
2022-10-22 10:30:56 +0530

    Integrate visualization api

--- packages/chronos-mfe/src/components/ForecastingResults/index.js
+++ packages/chronos-mfe/src/components/ForecastingResults/index.js
@@ -1,124 +1,29 @@
 import classNames from 'classnames';
-import React, { useState } from 'react';
-import { useHistory } from 'react-router-dom';
-import { Line, LineChart, CartesianGrid, XAxis, YAxis, Tooltip, ResponsiveContainer, RadarChart, PolarGrid, PolarAngleAxis, PolarRadiusAxis, Radar, Legend } from 'recharts';
+import React, { useState, useEffect } from 'react';
+import { useHistory, useParams } from 'react-router-dom';
+import { Line, LineChart, CartesianGrid, XAxis, YAxis, Tooltip, ResponsiveContainer, Legend, ReferenceArea } from 'recharts';
 import ContextMenu from '../shared/contextMenu/ContextMenu';
 import Styles from './styles.scss';
+import ProgressIndicator from '../../common/modules/uilab/js/src/progress-indicator';
+import { chronosApi } from '../../apis/chronos.api';
+import Spinner from '../shared/spinner/Spinner';
 
-const data = [
-	{ name: "Jan 2017", kpi: 2000, trend: 3700, movingAverage: 6000 },
-	{ name: "Feb 2017", kpi: 4200, trend: 3700, movingAverage: 6000 },
-	{ name: "Mar 2017", kpi: 8200, trend: 3700, movingAverage: 6000 }, 
-	{ name: "Apr 2017", kpi: 13000, trend: 1100, movingAverage: 2300 },
-	{ name: "May 2017", kpi: 8200, trend: 3700, movingAverage: 6000 },
-	{ name: "Jun 2017", kpi: 3200, trend: 3700, movingAverage: 6000 },
-	{ name: "Jul 2017", kpi: 1200, trend: 3700, movingAverage: 6000 },
-	{ name: "Aug 2017", kpi: 3200, trend: 3700, movingAverage: 6000 },
-	{ name: "Sep 2017", kpi: 3200, trend: 3700, movingAverage: 6000 },
-	{ name: "Oct 2017", kpi: 3200, trend: 3700, movingAverage: 6000 },
-	{ name: "Nov 2017", kpi: 3200, trend: 3700, movingAverage: 6000 },
-	{ name: "Dec 2017", kpi: 3200, trend: 3700, movingAverage: 6000 },
-	{ name: "Jan 2018", kpi: 18000, trend: 3700, movingAverage: 6000 },
-	{ name: "Feb 2018", kpi: 3200, trend: 3700, movingAverage: 6000 },
-	{ name: "Mar 2018", kpi: 3200, trend: 3700, movingAverage: 6000 },
-	{ name: "Apr 2018", kpi: 3200, trend: 1100, movingAverage: 2300 },
-	{ name: "May 2018", kpi: 1500, trend: 3700, movingAverage: 6000 },
-	{ name: "Jun 2018", kpi: 3200, trend: 3700, movingAverage: 6000 },
-	{ name: "Jul 2018", kpi: 10000, trend: 3700, movingAverage: 6000 },
-	{ name: "Aug 2018", kpi: 3200, trend: 3700, movingAverage: 6000 },
-	{ name: "Sep 2018", kpi: 3200, trend: 3700, movingAverage: 6000 },
-	{ name: "Oct 2018", kpi: 3200, trend: 3700, movingAverage: 6000 },
-	{ name: "Nov 2018", kpi: 3200, trend: 3700, movingAverage: 6000 },
-	{ name: "Dec 2018", kpi: 3200, trend: 3700, movingAverage: 6000 },
-  { name: "Jan 2019", kpi: 20000, trend: 3700, movingAverage: 6000 },
-];
-
-const data2 = [
-  {
-    "month": "January",
-    "A": -150,
-    "B": 110,
-    "fullMark": 150
-  },
-  {
-    "month": "February",
-    "A": 98,
-    "B": 130,
-    "fullMark": 150
-  },
-  {
-    "month": "March",
-    "A": 86,
-    "B": 130,
-    "fullMark": 150
-  },
-  {
-    "month": "April",
-    "A": 99,
-    "B": 100,
-    "fullMark": 150
-  },
-  {
-    "month": "May",
-    "A": 85,
-    "B": 90,
-    "fullMark": 150
-  },
-  {
-    "month": "June",
-    "A": 65,
-    "B": 85,
-    "fullMark": 150
-  },
-  {
-    "month": "July",
-    "A": 120,
-    "B": 110,
-    "fullMark": 150
-  },
-  {
-    "month": "August",
-    "A": 98,
-    "B": 130,
-    "fullMark": 150
-  },
-  {
-    "month": "September",
-    "A": 86,
-    "B": 130,
-    "fullMark": 150
-  },
-  {
-    "month": "October",
-    "A": 99,
-    "B": 100,
-    "fullMark": 150
-  },
-  {
-    "month": "November",
-    "A": 85,
-    "B": 90,
-    "fullMark": 150
-  },
-  {
-    "month": "December",
-    "A": 65,
-    "B": 85,
-    "fullMark": 150
-  },
-];
+const COLORS = ['#00ADF0', '#33ADAC', '#E0144C', '#EED180', '#AA8B56', '#A8E890', '#6F38C5'];
 
 const ForecastingResults = () => {
   const history = useHistory();
   const goback = () => {
     history.goBack();
   }
+  const { projectid: projectId, runid: runId } = useParams();
 
   const [nerd, setNerd] = useState(false);
 
   /* chart */
   const axisTextStyle = { fill: '#99A5B3', fontSize: 'var(--font-size-smallest)', fontFamily: 'Roboto-Medium' };
-  const tooltipCursorBackground = { fill: 'white', opacity: 0.1 };
+  // const tooltipCursorBackground = { fill: '#1f2124', opacity: 1 };
+
+  const [columns, setColumns] = useState([]);
 
   const onDummyClick = () => {
     console.log('dummy click');
@@ -134,6 +39,34 @@ const ForecastingResults = () => {
     }
   ];
 
+  const [loading, setLoading] = useState(true);
+  const [forecastRun, setForecastRun] = useState([]);
+  useEffect(() => {
+    getForecastRun();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const getForecastRun = () => {
+    ProgressIndicator.show();
+    chronosApi.getForecastRun(projectId, runId).then((res) => {
+      if(res.status === 204) {
+        setForecastRun([]);
+      } else {
+        console.log(res);
+        setForecastRun(res.data);
+        const cols = Object.keys(res.data.forecast.data[0]);
+        const newCols = cols.filter(item => item !== 'name');
+        setColumns(newCols);
+      }
+      setLoading(false);
+      ProgressIndicator.hide();
+    }).catch(error => {
+      console.log(error.message);
+      setLoading(false);
+      ProgressIndicator.hide();
+    });
+  };
+
   return (
     <div className={classNames(Styles.mainPanel)}>
       <div className={Styles.backButtonWapper}>
@@ -141,7 +74,7 @@ const ForecastingResults = () => {
           Result Overview
         </button>
         <div className={Styles.summeryBannerTitle}>
-          <h2>Forecasting Results &quot;2022-07-29_Test-Run&quot;</h2>
+          <h2>Forecasting Results &quot;{!loading && forecastRun?.runName}&quot;</h2>
           <div className={Styles.switch}>
             <label className={classNames('switch', nerd && 'on')}>
               <span className="label" style={{ marginRight: '5px' }}>
@@ -162,85 +95,41 @@ const ForecastingResults = () => {
       </div>
       <div className={Styles.content}>
         <div className={Styles.header}>
-          <h3>Trend</h3>
-          <p>Trend component, determined based on different approaches</p>
+          <h3>Forecast</h3>
+          {/* <p></p> */}
           <div className={Styles.actionMenu}>
             <ContextMenu id={'trend'} items={contextMenuItems} />
           </div>
         </div>
         <div className={Styles.firstPanel}>
-          <ResponsiveContainer width="100%" height={450}>
-            <LineChart
-              data={data}
-              margin={{
-                top: 20,
-                right: 5,
-                left: -20,
-                bottom: 15,
-              }}
-              style={{ cursor: 'pointer' }}
-            >
-              <Line type="linear" dataKey="kpi" stroke="#00adf0" strokeWidth={2} dot={false} />
-              <Line type="linear" dataKey="trend" stroke="#33adac" strokeWidth={2} dot={false} />
-              <Line type="linear" dataKey="movingAverage" stroke="#979797" strokeDasharray="5 5" dot={false} />
-              <CartesianGrid stroke="#383F49" />
-              <XAxis dataKey="name" axisLine={false} tickLine={false} tick={axisTextStyle} />
-              <YAxis axisLine={false} tickLine={false} tick={axisTextStyle} />
-              <Tooltip cursor={tooltipCursorBackground} />
-              <Legend />
-            </LineChart>
-          </ResponsiveContainer>
-        </div>
-      </div>
-      <div className={Styles.content}>
-        <div className={Styles.header}>
-          <h3>Decomposition</h3>
-          <p>Decomposing the target series into a trend, seasonal<br />and residual component</p>
-          <div className={Styles.actionMenu}>
-            <ContextMenu id={'decomposition'} items={contextMenuItems} />
-          </div>
-        </div>
-        <div className={Styles.firstPanel}>
-          <ResponsiveContainer width="100%" height={450}>
-            <LineChart
-              data={data}
-              margin={{
-                top: 20,
-                right: 5,
-                left: -20,
-                bottom: 15,
-              }}
-              style={{ cursor: 'pointer' }}
-            >
-              <Line type="linear" dataKey="kpi" stroke="#00adf0" strokeWidth={2} dot={false} />
-              <Line type="linear" dataKey="trend" stroke="#33adac" strokeWidth={2} dot={false} />
-              <Line type="linear" dataKey="movingAverage" stroke="#979797" strokeDasharray="5 5" dot={false} />
-              <CartesianGrid stroke="#383F49" />
-              <XAxis dataKey="name" axisLine={false} tickLine={false} tick={axisTextStyle} />
-              <YAxis axisLine={false} tickLine={false} tick={axisTextStyle} />
-              <Tooltip cursor={tooltipCursorBackground} />
-              <Legend />
-            </LineChart>
-          </ResponsiveContainer>
-        </div>
-      </div>
-      <div className={Styles.content}>
-        <div className={Styles.header}>
-          <h3>Monthly Seasonality</h3>
-          <p>Allows for conclusions regarding the strength of<br/>the seasonal component</p>
-          <div className={Styles.actionMenu}>
-            <ContextMenu id={'seasonality'} items={contextMenuItems} />
-          </div>
-        </div>
-        <div className={Styles.firstPanel}>
-          <RadarChart outerRadius={300} width={1080} height={750} data={data2} startAngle={150} endAngle={-210}>
-            <PolarGrid stroke="#383F49" />
-            <PolarAngleAxis dataKey="month" tick={axisTextStyle} stroke="#383F49" />
-            <PolarRadiusAxis angle={0} domain={[0, 150]} tick={axisTextStyle} stroke="#383F49" />
-            <Radar name="Mike" dataKey="A" stroke="#00adf0" fill="#8884d8" fillOpacity={0} />
-            <Radar name="Lily" dataKey="B" stroke="#33adac" fill="#82ca9d" fillOpacity={0} />
-            <Tooltip />
-          </RadarChart>
+          { loading && <Spinner /> }
+          { !loading && 
+              <ResponsiveContainer width="100%" height={450}>
+                <LineChart
+                  data={forecastRun.forecast.data}
+                  margin={{
+                    top: 20,
+                    right: 5,
+                    left: -20,
+                    bottom: 15,
+                  }}
+                  style={{ cursor: 'pointer' }}
+                >
+                  {
+                    columns.length > 0 && columns.map((column, index) => {
+                      return <Line type="linear" key={column} dataKey={column} stroke={COLORS[index]} strokeWidth={2} dot={false} />
+                    })
+                  }
+                  <CartesianGrid stroke="#383F49" />
+                  <XAxis dataKey="name" axisLine={false} tickLine={false} tick={axisTextStyle} />
+                  <YAxis axisLine={false} tickLine={false} tick={axisTextStyle} />
+                  <ReferenceArea x1={forecastRun.forecast.data[forecastRun.forecast.data.length - parseInt(forecastRun.forecastHorizon)].name} x2={forecastRun.forecast.data[forecastRun.forecast.data.length - 1].name} stroke="gray" strokeOpacity={0.3} />
+                  {/* <Tooltip cursor={tooltipCursorBackground} /> */}
+                  <Tooltip contentStyle={{backgroundColor: '#252a33'}} />
+                  <Legend />
+                </LineChart>
+              </ResponsiveContainer>
+          }
         </div>
       </div>
     </div>
