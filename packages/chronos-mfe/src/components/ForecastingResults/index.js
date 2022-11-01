59f02bb1c238c98d71436eec8ae90466d05a0158
Khartade <khartaa@apac.corpdir.net>
2022-11-01 21:07:36 +0530

    New chart implementation

--- packages/chronos-mfe/src/components/ForecastingResults/index.js
+++ packages/chronos-mfe/src/components/ForecastingResults/index.js
@@ -1,7 +1,7 @@
 import classNames from 'classnames';
 import React, { useState, useEffect } from 'react';
 import { useHistory, useParams } from 'react-router-dom';
-import { Line, LineChart, CartesianGrid, XAxis, YAxis, Tooltip, ResponsiveContainer, Legend, ReferenceArea, Brush } from 'recharts';
+// import { Line, LineChart, CartesianGrid, XAxis, YAxis, Tooltip, ResponsiveContainer, Legend, ReferenceArea, Brush } from 'recharts';
 import html2canvas from 'html2canvas';
 import { jsPDF } from 'jspdf';
 import ContextMenu from '../shared/contextMenu/ContextMenu';
@@ -9,8 +9,9 @@ import Styles from './styles.scss';
 import ProgressIndicator from '../../common/modules/uilab/js/src/progress-indicator';
 import { chronosApi } from '../../apis/chronos.api';
 import Spinner from '../shared/spinner/Spinner';
+import Plot from 'react-plotly.js';
 
-const COLORS = ['#00ADF0', '#33ADAC', '#E0144C', '#EED180', '#AA8B56', '#A8E890', '#6F38C5', '#9E7676', '#D0B8A8', '#7895B2'];
+// const COLORS = ['#00ADF0', '#33ADAC', '#E0144C', '#EED180', '#AA8B56', '#A8E890', '#6F38C5', '#9E7676', '#D0B8A8', '#7895B2'];
 
 const ForecastingResults = () => {
   const printRef = React.useRef();
@@ -24,7 +25,7 @@ const ForecastingResults = () => {
   const [nerd, setNerd] = useState(false);
 
   /* chart */
-  const axisTextStyle = { fill: '#99A5B3', fontSize: 'var(--font-size-smallest)', fontFamily: 'Roboto-Medium' };
+  // const axisTextStyle = { fill: '#99A5B3', fontSize: 'var(--font-size-smallest)', fontFamily: 'Roboto-Medium' };
   // const tooltipCursorBackground = { fill: '#1f2124', opacity: 1 };
 
   const [columns, setColumns] = useState([]);
@@ -59,14 +60,14 @@ const ForecastingResults = () => {
         setForecastRun([]);
       } else {
         setForecastRun(res.data);
-        const y = 'name' + res.data.y;
+        const y = 'date' + res.data.y;
         const yObj = csvToJSON(y);
-        const yPred = 'name' + res.data.yPred;
+        const yPred = 'date' + res.data.yPred;
         const yPredObj = csvToJSON(yPred);
-        const forecastObj = [...yObj, ...yPredObj].filter(obj => obj.name !== '');
+        const forecastObj = [...yObj, ...yPredObj].filter(obj => obj.date !== '');
         setForecastData(forecastObj);
         const cols = Object.keys(forecastObj[0]);
-        const newCols = cols.filter(item => item !== 'name');
+        const newCols = cols.filter(item => item !== 'date');
         setColumns(newCols.slice(0, 10));
       }
       setLoading(false);
@@ -121,6 +122,93 @@ const ForecastingResults = () => {
     }
   ];
 
+  const layout = {  
+    colorway: ['#00ADF0', '#33ADAC', '#E0144C', '#EED180', '#AA8B56', '#A8E890', '#6F38C5', '#9E7676', '#D0B8A8', '#7895B2'],
+    margin: {b: 0, r: 20, t: 25},
+    autosize: true, 
+    title: '', 
+    paper_bgcolor: 'transparent',
+    plot_bgcolor: '#252a33',
+    yaxis: {
+      gridcolor: '#383F49',
+      automargin: true,
+    },
+    xaxis: {
+      gridcolor: '#383F49',
+      automargin: true,
+    },
+    font: {
+      color: '#99A5B3', 
+      family: 'Roboto-Medium', 
+      size: 'var(--font-size-smallest)'
+    },
+    showlegend: true,
+    legend: {
+      orientation: 'h', 
+      font: {
+        color: '#d9dfe4', 
+        family: 'Roboto-Medium', 
+        size: 'var(--font-size-smallest)'
+      }
+    },
+    hoverlabel: {
+      bgcolor: '#252a33',
+      bordercolor: '#99A5B3'
+    },
+    modebar: {
+      bgcolor: 'transparent',
+      remove: ["lasso", "lasso2d"]
+    },
+    hovermode: "x unified",
+    dragmode: "pan",
+    shapes: [
+      {
+        type: 'rect',
+        xref: 'x',
+        yref: 'paper',
+        x0: (!loading && forecastData.length > 0) && forecastData[forecastData.length - parseInt(forecastRun.forecastHorizon)].date || '1959-01',
+        y0: 0,
+        x1: (!loading && forecastData.length > 0) && forecastData[forecastData.length - 1].date || '1960-01',
+        y1: 1,
+        fillcolor: '#383F49',
+        opacity: 0.5,
+        line: {
+            width: 0
+        }
+      },
+    ],
+  };
+
+  const addTraces = (data) => {
+    let traces = [];
+    
+    let dates = [];
+    let lines = {};
+
+    columns.forEach(column => {
+      lines[column] = {'y': []};
+    });
+    
+    data.map(each => {
+      dates.push(each.date);
+      columns.forEach(column => {
+        lines[column].y.push(each[column]);
+      });
+    });
+    
+    for(const [key, value] of Object.entries(lines)) {
+      traces.push({
+        type: 'scatter',
+        mode: 'lines',
+        x: dates,
+        y: value.y,
+        name: key
+      });
+    }
+    
+    return traces;
+  }
+
   return (
     <div className={classNames(Styles.mainPanel)}>
       <div className={Styles.backButtonWapper}>
@@ -160,7 +248,16 @@ const ForecastingResults = () => {
           { !loading && forecastData.length > 0 &&
               <>
                 <p className={Styles.chartLabel}>Forecast</p>
-                <ResponsiveContainer width="100%" height={450}>
+                <div className={Styles.chartContainer}>
+                  <Plot
+                    data={addTraces(forecastData)}
+                    layout={layout}
+                    useResizeHandler
+                    config={{ scrollZoom: true, displaylogo: false }}
+                    style={{width: '100%', height: '450px'}}
+                  />
+                </div>
+                {/* <ResponsiveContainer width="100%" height={450}>
                   <LineChart
                     data={forecastData}
                     margin={{
@@ -178,14 +275,14 @@ const ForecastingResults = () => {
                       })
                     }
                     <CartesianGrid stroke="#383F49" />
-                    <XAxis dataKey="name" axisLine={false} tickLine={false} tick={axisTextStyle} />
+                    <XAxis dataKey="date" axisLine={false} tickLine={false} tick={axisTextStyle} />
                     <YAxis axisLine={false} tickLine={false} tick={axisTextStyle} />
                     <ReferenceArea x1={forecastData[forecastData.length - parseInt(forecastRun.forecastHorizon)].name} x2={forecastData[forecastData.length - 1].name} stroke="gray" strokeOpacity={0.3} />
                     <Tooltip contentStyle={{backgroundColor: '#252a33'}} />
                     <Legend />
-                    <Brush dataKey="name" height={30} />
+                    <Brush dataKey="date" height={30} />
                   </LineChart>
-                </ResponsiveContainer>
+                </ResponsiveContainer> */}
               </>
           }
         </div>
