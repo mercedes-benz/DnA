f405c16c49008644a49ad36e2130aeafe76202ba
Shubham <sbm_verma31@rediffmail.com>
2022-11-07 10:55:59 +0530

    updated summary screen

--- packages/frontend/src/components/mbc/reportSummary/customer/CustomerSummary.tsx
+++ packages/frontend/src/components/mbc/reportSummary/customer/CustomerSummary.tsx
@@ -24,9 +24,6 @@ export default class CustomerSummary extends React.Component<ICustomerSummaryPro
   }
 
   public render() {
-    // const teamMembersList = this.props.customers.processOwners?.map((member: ITeams, index: number) => {
-    //   return <TeamMemberListItem key={index} itemIndex={index} teamMember={member} />;
-    // });
     
     return (
       <React.Fragment>
@@ -60,12 +57,12 @@ export default class CustomerSummary extends React.Component<ICustomerSummaryPro
                             <div id="hierarchy">
                               <label className="input-label summary">Level</label>
                               <br />
-                              <div>{customer.level}</div>
+                              <div>{customer.level ? customer.level : 'NA'}</div>
                             </div>
                             <div id="department">
                               <label className="input-label summary">E2-Department</label>
                               <br />
-                              <div>{customer.department}</div>
+                              <div>{customer.department ? customer.department : 'NA'}</div>
                             </div>
                             <div id="ressort">
                               <label className="input-label summary">US-Risk (US-Access to sensible data)</label>
@@ -77,17 +74,17 @@ export default class CustomerSummary extends React.Component<ICustomerSummaryPro
                             <div id="ressort">
                               <label className="input-label summary">MB Legal Entity</label>
                               <br />
-                              <div>{customer.legalEntity}</div>
+                              <div>{customer.legalEntity ? customer.legalEntity : 'NA'}</div>
                             </div>
                             <div id="customerRelation">
                               <label className="input-label summary">Customer Relation</label>
                               <br />
-                              <div>{customer.customerRelation}</div>
+                              <div>{customer.customerRelation ? customer.customerRelation : 'NA'}</div>
                             </div>
                             <div id="customerDivision">
                               <label className="input-label summary">Customer Division</label>
                               <div className={Styles.customerDivision}>
-                                {customer?.division?.name}
+                                {customer?.division?.name && customer?.division?.name !== 'Choose' ? customer?.division?.name : 'NA'}
                               </div>
                             </div>
                             
@@ -97,7 +94,7 @@ export default class CustomerSummary extends React.Component<ICustomerSummaryPro
                             <div id="comment">
                               <label className="input-label summary">Comment</label>
                               <p>
-                                <pre className={Styles.commentPre}>{customer.comment}</pre>
+                                <pre className={Styles.commentPre}>{customer.comment ? customer.comment : 'NA'}</pre>
                               </p>
                             </div>
                             
@@ -120,23 +117,23 @@ export default class CustomerSummary extends React.Component<ICustomerSummaryPro
                             <div id="nameExternal">
                               <label className="input-label summary">Name</label>
                               <br />
-                              <div>{customer.name?.firstName}{' '}{customer.name?.lastName}</div>
+                              <div>{customer.name?.firstName || customer.name?.lastName ? customer.name?.firstName+' '+customer.name?.lastName : 'NA'}</div>
                             </div>
                             <div id="companyName">
                               <label className="input-label summary">Company Name</label>
                               <br />
-                              <div>{customer.companyName}</div>
+                              <div>{customer.companyName ? customer.companyName : 'NA'}</div>
                             </div>
                             <div id="customerRelationExternal">
                               <label className="input-label summary">Customer Relation</label>
                               <br />
-                              <div>{customer.customerRelation}</div>
+                              <div>{customer.customerRelation ? customer.customerRelation : 'NA'}</div>
                             </div>
                           </div>
                           <div className={Styles.commentSection}>
                             <label className="input-label summary">Comment</label>
                             <p>
-                              <pre className={Styles.commentPre}>{customer.comment}</pre>
+                              <pre className={Styles.commentPre}>{customer.comment ? customer.comment : 'NA'}</pre>
                             </p>
                           </div>
                           {(this.props.customers.externalCustomers?.length > 1 ||
@@ -146,15 +143,6 @@ export default class CustomerSummary extends React.Component<ICustomerSummaryPro
                     );
                   })
                 : null}  
-              {/* {this.props.customers.processOwners?.length ? (
-                <div className={Styles.customerListView}>
-                  <div className={Styles.processOwnerCardSection}>
-                    <label className="input-label summary">Process Owner</label>
-                    <br />
-                    <div>{teamMembersList}</div>
-                  </div>
-                </div>
-              ) : null} */}
             </div>
           </div>
         </div>
