526a0c17dd6b06cbd36c3a9b1a94b48e7fb3c5e8
Shubham <sbm_verma31@rediffmail.com>
2022-11-25 10:00:29 +0530

    Added transparency

--- packages/frontend/src/components/mbc/workspaces/Workspaces.tsx
+++ packages/frontend/src/components/mbc/workspaces/Workspaces.tsx
@@ -22,7 +22,7 @@ const Workspaces = () => {
 
   return (
     <MainPanel title={'Data'} subTitle={'Lorem ipsum dolor sit amet'}>
-      <div className={Styles.Workspaces}>
+      <div className={Styles.workspacesWrapper}>
         <DNACard
           title={'Data Products'}
           description={'Data is one of the most valuable assets in our company,&nbsp;therefore we treat our data as a product!&nbsp;We offer you a&nbsp;growing selection of intuitive to use and well documented&nbsp;data products - check it out!'}
