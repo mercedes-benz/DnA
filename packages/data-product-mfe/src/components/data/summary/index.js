45f3151ea155f2925d562245eaf3b4e87774c782
vikasmadan09 <vikasmadan09@gmail.com>
2022-11-29 17:41:53 +0530

    1. update data Transfer Card Layout component to honour isDataProduct prop 2. update mock data 3. update data product summary page and necessary css

--- packages/data-product-mfe/src/components/data/summary/index.js
+++ packages/data-product-mfe/src/components/data/summary/index.js
@@ -1,26 +1,23 @@
 import classNames from 'classnames';
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useParams, withRouter } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 import { dataProductApi } from '../../../apis/data.api';
 import ProgressIndicator from '../../../common/modules/uilab/js/src/progress-indicator';
 import Tabs from '../../../common/modules/uilab/js/src/tabs';
 import { hostServer } from '../../../server/api';
+import DataTranferCardLayout from '../../dataTransfer/Layout/CardView/DataProductCardItem';
 
 import { setSelectedData, setDivisionList } from '../redux/dataSlice';
 
 import Styles from './styles.scss';
 
 import { regionalDateFormat } from '../../../Utility/utils';
+import mockData from '../data.json';
 
-const Summary = ({ history }) => {
-  const { id: dataProductId } = useParams();
+const Summary = ({ history, user }) => {
   const { selectedData: data, data: dataList } = useSelector((state) => state.data);
-
-  // const division = serializeDivisionSubDivision(divisionList, {
-  //   division: data.division,
-  //   subDivision: data.subDivision,
-  // });
 
   const dispatch = useDispatch();
 
@@ -44,7 +41,7 @@ const Summary = ({ history }) => {
   }, [dataList]);
 
   const getDataProductById = () => {
-    dataProductApi.getDataById(dataList, dataProductId).then((res) => {
+    dataProductApi.getDataById(dataList, '0059').then((res) => {
       // const data = deserializeFormData(res.data);
       if (res) {
         dispatch(setSelectedData(res));
@@ -113,47 +110,254 @@ const Summary = ({ history }) => {
                   <div className={Styles.firstPanel}>
                     <div className={Styles.flexLayout}>
                       <div>
-                        <h5>Form 1</h5>
+                        <h5>Description</h5>
+                      </div>
+                    </div>
+                    <div className={classNames(Styles.flexLayout, Styles.threeColumn)}>
+                      <div>
+                        <label className="input-label summary">Data Product Name</label>
+                        <br />
+                        {data.productName}
+                      </div>
+                      <div>
+                        <label className="input-label summary">Data Product Description</label>
+                        <br />
+                        {data.productDescription}
                       </div>
                     </div>
                     <div className={classNames(Styles.flexLayout, Styles.fourColumn)}>
                       <div>
-                        <label className="input-label summary">Data Product Name</label>
+                        <label className="input-label summary">ART</label>
                         <br />
-                        {data.dataProductName}
+                        {data.ART}
                       </div>
                       <div>
-                        <label className="input-label summary">Date of Data Transfer</label>
+                        <label className="input-label summary">carLA</label>
                         <br />
-                        {regionalDateFormat(data.createdDate)}
+                        {data.carLA}
+                      </div>
+                      <div>
+                        <label className="input-label summary">Corporate Data Catalog</label>
+                        <br />
+                        {data.dataCatalog}
+                      </div>
+                      <div>
+                        <label className="input-label summary">How to access data catalog</label>
+                        <br />
+                        <span dangerouslySetInnerHTML={{ __html: data.howToAccess }}></span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className={Styles.sectionWrapper}>
+                  <div className={Styles.firstPanel}>
+                    <div className={Styles.flexLayout}>
+                      <div>
+                        <h5>Contact Information</h5>
+                      </div>
+                    </div>
+                    <div className={classNames(Styles.flexLayout, Styles.fourColumn)}>
+                      <div>
+                        <label className="input-label summary">Information Owner</label>
+                        <br />
+                        {data.informationOwner?.firstName} {data.informationOwner?.lastName}
+                      </div>
+                      <div>
+                        <label className="input-label summary">Publish Date of Data Product</label>
+                        <br />
+                        {regionalDateFormat(data.dateOfDataProduct)}
                       </div>
                       <div>
                         <label className="input-label summary">Name</label>
                         <br />
-                        {data.createdBy?.firstName} {data.createdBy?.lastName}
+                        {data?.providerInformation?.contactInformation?.name?.firstName}{' '}
+                        {data?.providerInformation?.contactInformation?.name?.lastName}
                       </div>
                       <div>
                         <label className="input-label summary">Division</label>
                         <br />
-                        {data.division?.name}
+                        {data?.division?.name}
                       </div>
                     </div>
                     <div className={classNames(Styles.flexLayout, Styles.fourColumn)}>
                       <div>
                         <label className="input-label summary">Sub Division</label>
                         <br />
-                        {data.division?.subdivision?.name || '-'}
+                        {data?.division?.subdivision?.name || '-'}
                       </div>
                       <div>
                         <label className="input-label summary">Department</label>
                         <br />
                         {data.department}
                       </div>
+                      <div>
+                        <label className="input-label summary">PlanningIT App-ID</label>
+                        <br />
+                        {data.planningIT || '-'}
+                      </div>
+                      <div>
+                        <label className="input-label summary">Compliance Officer / Responsible (LCO/LCR) </label>
+                        <br />
+                        {data.complianceOfficer}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className={Styles.sectionWrapper}>
+                  <div className={Styles.firstPanel}>
+                    <div className={Styles.flexLayout}>
+                      <div>
+                        <h5>Data Description & Classification</h5>
+                      </div>
+                    </div>
+                    <div className={classNames(Styles.flexLayout, Styles.threeColumn)}>
+                      <div>
+                        <label className="input-label summary">Description & Classification of transfered data</label>
+                        <br />
+                        {data.classificationOfTransferedData}
+                      </div>
+                      <div>
+                        <label className="input-label summary">Confidentiality</label>
+                        <br />
+                        {data?.providerInformation?.classificationConfidentiality?.confidentiality}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className={Styles.sectionWrapper}>
+                  <div className={Styles.firstPanel}>
+                    <div className={Styles.flexLayout}>
+                      <div>
+                        <h5>Personal Related Data</h5>
+                      </div>
+                    </div>
+                    <div className={Styles.flexLayout}>
+                      <div>
+                        <label className="input-label summary">Is data personal related</label>
+                        <br />
+                        {data.personalRelatedData}
+                      </div>
+                    </div>
+                    {data.personalRelatedData === 'Yes' ? (
+                      <div className={classNames(Styles.flexLayout, Styles.fourColumn)}>
+                        <div>
+                          <label className="input-label summary">Description</label>
+                          <br />
+                          {data.personalRelatedDataDescription}
+                        </div>
+                        <div>
+                          <label className="input-label summary">
+                            Original (business) purpose of processing this personal related data
+                          </label>
+                          <br />
+                          {data.personalRelatedDataPurpose}
+                        </div>
+                        <div>
+                          <label className="input-label summary">
+                            Original legal basis for processing this personal related data
+                          </label>
+                          <br />
+                          {data.personalRelatedDataLegalBasis}
+                        </div>
+                        <div></div>
+                      </div>
+                    ) : null}
+                  </div>
+                </div>
+                <div className={Styles.sectionWrapper}>
+                  <div className={Styles.firstPanel}>
+                    <div className={Styles.flexLayout}>
+                      <div>
+                        <h5>Trans-national Data</h5>
+                      </div>
+                    </div>
+                    <div className={classNames(Styles.flexLayout, Styles.threeColumn)}>
+                      <div>
+                        <label className="input-label summary">
+                          Is data being transferred from one country to another?
+                        </label>
+                        <br />
+                        {data.transnationalDataTransfer}
+                      </div>
+                      {data.transnationalDataTransfer == 'Yes' ? (
+                        <div>
+                          <label className="input-label summary">Is one of these countries not within the EU?</label>
+                          <br />
+                          {data.transnationalDataTransferNotWithinEU || 'No'}
+                        </div>
+                      ) : null}
+                      {data.transnationalDataTransfer == 'Yes' && data.transnationalDataTransferNotWithinEU == 'Yes' ? (
+                        <div>
+                          <label className="input-label summary">Has LCO/LCR approved this data transfer?</label>
+                          <br />
+                          {data.LCOApprovedDataTransfer}
+                        </div>
+                      ) : null}
+                      <div>
+                        <label className="input-label summary">Does product contain insider information?</label>
+                        <br />
+                        {data.insiderInformation}
+                      </div>
+                    </div>
+                    <div className={Styles.flexLayout}>
+                      <div>
+                        <label className="input-label summary">Is data from China included?</label>
+                        <br />
+                        {data.dataOriginatedFromChina}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className={Styles.sectionWrapper}>
+                  <div className={Styles.firstPanel}>
+                    <div className={Styles.flexLayout}>
+                      <div>
+                        <h5>Deletion Requirements & Other</h5>
+                      </div>
+                    </div>
+                    <div className={classNames(Styles.flexLayout, Styles.threeColumn)}>
+                      <div>
+                        <label className="input-label summary">
+                          Are there specific deletion requirements for this data?
+                        </label>
+                        <br />
+                        {data.deletionRequirement}
+                      </div>
+                      {data.deletionRequirement === 'Yes' ? (
+                        <div>
+                          <label className="input-label summary">Describe deletion requirements</label>
+                          <br />
+                          {data.deletionRequirementDescription}
+                        </div>
+                      ) : null}
+                      <div></div>
+                    </div>
+                    <div className={Styles.flexLayout}>
+                      <div>
+                        <label className="input-label summary">Other relevant information </label>
+                        <br />
+                        {data.otherRelevantInfo || '-'}
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
+          </div>
+          <hr className={Styles.line} />
+          <div className={Styles.dataTransferSection}>
+            <h3>{`Data Transfer ${mockData?.result?.length ? `(${mockData?.result?.length})` : null}`}</h3>
+            <div>
+              <Link to={'/datasharing'} target="_blank" rel="noreferrer noopener">
+                Show in Data Sharing
+                <i tooltip-data="Open in New Tab" className={'icon mbc-icon new-tab'} />
+              </Link>
+            </div>
+          </div>
+          <div className={classNames(Styles.allDataproductCardviewContent)}>
+            {mockData.result?.map((product, index) => {
+              return <DataTranferCardLayout key={index} product={product} user={user} isDataProduct={true} />;
+            })}
           </div>
         </div>
       </div>
