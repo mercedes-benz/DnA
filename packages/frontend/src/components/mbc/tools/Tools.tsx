0b1d2f608ef055b9e3a430335b48b98e585daebe
Shubham <sbm_verma31@rediffmail.com>
2022-12-09 16:42:43 +0530

    Added file to use data for landing pages

--- packages/frontend/src/components/mbc/tools/Tools.tsx
+++ packages/frontend/src/components/mbc/tools/Tools.tsx
@@ -1,30 +1,18 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import Styles from './Tools.scss';
-import { Envs } from 'globals/Envs';
-// import MainPanel from '../shared/mainPanel/MainPanel';
 import DNACard from 'components/card/Card';
 import LandingSummary from '../shared/landingSummary/LandingSummary';
 import headerImageURL from '../../../assets/images/Tools-Landing.png';
 
+import { ToolsLandingPageElements } from 'globals/landingPageElements';
+
 const Tools = () => {
   
-  const [enableJupiyterNoteWorkspace, setEnableJupiyterNoteWorkspace] = useState(true);
-  const [enableDataikuWorkspace, setEnableDataikuWorkspace] = useState(true);
-  const enableMalwareScanService = Envs.ENABLE_MALWARE_SCAN_SERVICE;
-  const enableDataPipelineService = Envs.ENABLE_DATA_PIPELINE_SERVICE;
-  const enableMyModelRegistryService = Envs.ENABLE_MY_MODEL_REGISTRY_SERVICE;
-  const enableMLPipelineService = Envs.ENABLE_ML_PIPELINE_SERVICE;
-  const enableStorageService = Envs.ENABLE_STORAGE_SERVICE;
-  const mLPipelineUrl = enableMLPipelineService ? Envs.ML_PIPELINE_URL : '#/comingsoon';
-  const enableChronosForecastingService = Envs.ENABLE_CHRONOS_FORECASTING_SERVICE;
-  const enableSapAnalyticsCloud = Envs.ENABLE_SAP_ANALYTICS_CLOUD;
-  const sapAnalyticsUrl = Envs.SAP_ANALYTICS_CLOUD_URL;
-  const enableCodeSpace = Envs.ENABLE_CODE_SPACE;
 
   useEffect(() => {
-    setEnableJupiyterNoteWorkspace(Envs.ENABLE_JUPYTER_WORKSPACE);
-    setEnableDataikuWorkspace(Envs.ENABLE_DATAIKU_WORKSPACE);
   });
+
+  const cards = ToolsLandingPageElements;
 
   return (
     <LandingSummary
@@ -37,94 +25,18 @@ const Tools = () => {
     isBackButton={false}
     >
       <div className={Styles.toolsWrapper}>
-        <DNACard
-          title={'Malware Scan'}
-          description={'Data is one of the most valuable assets in our company, therefore we treat our data as a product! We offer you a growing selection of intuitive to use and well documented data products - check it out!'}
-          url={'/malwarescanservice'}
-          isTextAlignLeft={false}
-          isDisabled={!enableMalwareScanService}
-          isSmallCard={false}
-          isMediumCard={true} />
-        <DNACard
-          title={'Data Pipeline'}
-          description={'Data is one of the most valuable assets in our company, therefore we treat our data as a product! We offer you a growing selection of intuitive to use and well documented data products - check it out!'}
-          url={'/pipeline'}
-          isTextAlignLeft={false}
-          isDisabled={!enableDataPipelineService}
-          isSmallCard={false}
-          isMediumCard={true} />
-        <DNACard
-          title={'ML Pipeline'}
-          description={'Data is one of the most valuable assets in our company, therefore we treat our data as a product! We offer you a growing selection of intuitive to use and well documented data products - check it out!'}
-          url={mLPipelineUrl}
-          isTextAlignLeft={false}
-          isDisabled={!enableMLPipelineService}
-          isSmallCard={false}
-          isMediumCard={true}
-          isExternalLink={true}/>
-        <DNACard
-          title={'My Storage'}
-          description={'Data is one of the most valuable assets in our company, therefore we treat our data as a product! We offer you a growing selection of intuitive to use and well documented data products - check it out!'}
-          url={'/storage'}
-          isTextAlignLeft={false}
-          isDisabled={!enableStorageService}
-          isSmallCard={false}
-          isMediumCard={true} />  
-        <DNACard
-          title={'My Model Registry'}
-          description={'Data is one of the most valuable assets in our company, therefore we treat our data as a product! We offer you a growing selection of intuitive to use and well documented data products - check it out!'}
-          url={'/modelregistry'}
-          isTextAlignLeft={false}
-          isDisabled={!enableMyModelRegistryService}
-          isSmallCard={false}
-          isMediumCard={true} />  
-        <DNACard
-          title={'Chronos Forecasting'}
-          description={'Data is one of the most valuable assets in our company, therefore we treat our data as a product! We offer you a growing selection of intuitive to use and well documented data products - check it out!'}
-          url={'/chronos'}
-          isTextAlignLeft={false}
-          isDisabled={!enableChronosForecastingService}
-          isSmallCard={false}
-          isMediumCard={true} /> 
-
-        <DNACard
-          title={'Jupyter Notebook'}
-          description={'Data is one of the most valuable assets in our company, therefore we treat our data as a product! We offer you a growing selection of intuitive to use and well documented data products - check it out!'}
-          url={'/notebook'}
-          isTextAlignLeft={false}
-          isDisabled={!enableJupiyterNoteWorkspace}
-          isSmallCard={false}
-          isMediumCard={true} /> 
-
-        <DNACard
-          title={'Dataiku'}
-          description={'Data is one of the most valuable assets in our company, therefore we treat our data as a product! We offer you a growing selection of intuitive to use and well documented data products - check it out!'}
-          url={'/mydataiku'}
-          isTextAlignLeft={false}
-          isDisabled={!enableDataikuWorkspace}
-          isSmallCard={false}
-          isMediumCard={true} /> 
-
-        {enableSapAnalyticsCloud && (
-          <DNACard
-            title={'SAP Analytics Cloud'}
-            description={'Data is one of the most valuable assets in our company, therefore we treat our data as a product! We offer you a growing selection of intuitive to use and well documented data products - check it out!'}
-            url={sapAnalyticsUrl}
-            isTextAlignLeft={false}
-            isDisabled={!enableSapAnalyticsCloud}
-            isSmallCard={false}
-            isMediumCard={true}
-            isExternalLink={true} /> 
-        )}        
-
-        <DNACard
-          title={'My Code Space (Beta)'}
-          description={'Data is one of the most valuable assets in our company, therefore we treat our data as a product! We offer you a growing selection of intuitive to use and well documented data products - check it out!'}
-          url={'/codespaces'}
-          isTextAlignLeft={false}
-          isDisabled={!enableCodeSpace}
-          isSmallCard={false}
-          isMediumCard={true} />                
+      {cards.map((card, index)=> {
+          return <DNACard
+          key={index}
+          title={card.name}
+          description={card.description}
+          url={card.url}
+          isExternalLink={card.isExternalLink}
+          isTextAlignLeft={card.isTextAlignLeft}
+          isDisabled={card.isDisabled}
+          isSmallCard={card.isSmallCard}
+          isMediumCard={card.isMediumCard} />
+        })}
 
       </div>
     </LandingSummary>
