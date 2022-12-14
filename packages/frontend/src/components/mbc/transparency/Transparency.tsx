3b1665613799e7c6d65119f295846f5f7a9f85c7
Maria Rathinam <amariar@capelit77946372.inedc.corpintra.net>
2022-12-14 13:21:18 +0530

    Updated images to latest and corrected data and transparency landing

--- packages/frontend/src/components/mbc/transparency/Transparency.tsx
+++ packages/frontend/src/components/mbc/transparency/Transparency.tsx
@@ -1,31 +1,41 @@
 import React from 'react';
 import Styles from './Transparency.scss';
-import MainPanel from '../shared/mainPanel/MainPanel';
 import DNACard from 'components/card/Card';
-
+import headerImageURL from '../../../assets/images/Transparency-Landing.png';
 import { TranparencyLandingPageElements } from 'globals/landingPageElements';
+import LandingSummary from '../shared/landingSummary/LandingSummary';
 
 const Transparency = () => {
-
   const cards = TranparencyLandingPageElements;
 
   return (
-    <MainPanel title={'Transparency'} subTitle={'Lorem ipsum dolor sit amet'}>
+    <LandingSummary
+      title={'Transparency'}
+      subTitle={
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+      }
+      tags={['Lorem Ipsum', 'ABC', 'XYZ']}
+      headerImage={headerImageURL}
+      isBackButton={true}
+    >
       <div className={Styles.transparencyWrapper}>
-        {cards.map((card, index)=> {
-          return <DNACard
-          key={index}
-          title={card.name}
-          description={card.description}
-          url={card.url}
-          isExternalLink={card.isExternalLink}
-          isTextAlignLeft={card.isTextAlignLeft}
-          isDisabled={card.isDisabled}
-          isSmallCard={card.isSmallCard}
-          isMediumCard={card.isMediumCard} />
+        {cards.map((card, index) => {
+          return (
+            <DNACard
+              key={index}
+              title={card.name}
+              description={card.description}
+              url={card.url}
+              isExternalLink={card.isExternalLink}
+              isTextAlignLeft={card.isTextAlignLeft}
+              isDisabled={card.isDisabled}
+              isSmallCard={card.isSmallCard}
+              isMediumCard={card.isMediumCard}
+            />
+          );
         })}
       </div>
-    </MainPanel>
+    </LandingSummary>
   );
 };
 
