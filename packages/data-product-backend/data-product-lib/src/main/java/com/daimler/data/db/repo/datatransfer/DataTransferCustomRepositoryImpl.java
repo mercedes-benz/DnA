7f4ad473316d2b69ca0111329008e5c9a2afdabc
AJAY SHIVANAGOL <ajay.shivanagol@mercedes-benz.com>
2023-01-04 10:53:17 +0530

    Comment resolved

--- packages/data-product-backend/data-product-lib/src/main/java/com/daimler/data/db/repo/datatransfer/DataTransferCustomRepositoryImpl.java
+++ packages/data-product-backend/data-product-lib/src/main/java/com/daimler/data/db/repo/datatransfer/DataTransferCustomRepositoryImpl.java
@@ -37,7 +37,6 @@ import javax.persistence.Query;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.StringUtils;
 
