770c7a20654c759e1ad3925bf4beb0ea00bcb642
Maria Rathinam <amariar@capelit77946372.inedc.corpintra.net>
2023-01-09 19:26:57 +0530

    Icon and tools page updates as per design

--- packages/frontend/src/components/mbc/shared/landingSummary/LandingSummary.tsx
+++ packages/frontend/src/components/mbc/shared/landingSummary/LandingSummary.tsx
@@ -1,11 +1,7 @@
-import React, { useEffect, 
-    // useState 
-} from 'react';
+import React, { useEffect, useState } from 'react';
 import Styles from './LandingSummary.scss';
-// import TagSection from './tagSection/TagSection';
+import TagSection from './tagSection/TagSection';
 import HeadingSection from './headingSection/HeadingSection';
-
-
 
 export interface ILandingSummaryProps {
   title: string;
@@ -18,26 +14,30 @@ export interface ILandingSummaryProps {
 }
 
 const LandingSummary = (props: ILandingSummaryProps) => {
+  const [selectedTags, setSelectedTags] = useState([]);
 
-    // const [selectedTags, setSelectedTags] = useState([]);
+  useEffect(() => {}, []);
 
-    useEffect(() => {
-    },[])
-    // const setSelectedFilter = (values: string[]) => {
-    //     setSelectedTags(values);
-    // }
+  const setSelectedFilter = (values: string[]) => {
+    setSelectedTags(values);
+  };
 
-    return (
-        <div className={Styles.landingSummary}>
-            <HeadingSection title={props.title} subTitle={props.subTitle} headerImage={props.headerImage} isBackButton={props.isBackButton}></HeadingSection>
-            {/* {props.isTagsFilter ? 
-                <TagSection tags={props.tags} selectedTags={selectedTags} setSeletedTags={setSelectedFilter}></TagSection>
-            : ''} */}
-            <div className={Styles.content}>
-                {props.children}
-            </div>
+  return (
+    <div className={Styles.landingSummary}>
+      <HeadingSection
+        title={props.title}
+        subTitle={props.subTitle}
+        headerImage={props.headerImage}
+        isBackButton={props.isBackButton}
+      />
+      {props.isTagsFilter && (
+        <div className={Styles.tagsSectionWrapper}>
+          <TagSection tags={props.tags} selectedTags={selectedTags} setSeletedTags={setSelectedFilter} />
         </div>
-    );
+      )}
+      <div className={Styles.content}>{props.children}</div>
+    </div>
+  );
 };
 
 export default LandingSummary;
