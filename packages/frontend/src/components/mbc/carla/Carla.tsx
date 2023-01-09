5af1ebcbf93a78f7ebc8cc14f462ed998b90a55e
Maria Rathinam <amariar@capelit77946372.inedc.corpintra.net>
2023-01-09 20:53:51 +0530

    Text updates and some ui fixes

--- packages/frontend/src/components/mbc/carla/Carla.tsx
+++ packages/frontend/src/components/mbc/carla/Carla.tsx
@@ -4,19 +4,19 @@ import DNACard from 'components/card/Card';
 import LandingSummary from '../shared/landingSummary/LandingSummary';
 import headerImageURL from '../../../assets/images/CarLA-Landing.png';
 
-import { CarlaLandingPageElements } from 'globals/landingPageElements';
+import { CarLALandingPageElements } from 'globals/landingPageElements';
 
 const Carla = () => {
-  const cards = CarlaLandingPageElements;
+  const cards = CarLALandingPageElements;
 
   return (
     <LandingSummary
       title={'CarLA'}
       subTitle={
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+        'CarLA (Cars analysis and reporting landscape) is the core element of the BI & Analytics landscape in Finance Controlling at MBC. It bundles all data & functions, which are used for analysis and reporting of business performance and planning.'
       }
       headerImage={headerImageURL}
-      isBackButton={false}
+      isBackButton={true}
     >
       <div className={Styles.carlaWrapper}>
         {cards.map((card, index) => {
