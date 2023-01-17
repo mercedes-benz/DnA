87995c2699351a7434dce3dce8adcac3de5c1713
Khartade <khartaa@apac.corpdir.net>
2023-01-17 17:44:32 +0530

    All solutions reloading fix

--- packages/frontend/src/components/mbc/filters/SolutionsFilter.tsx
+++ packages/frontend/src/components/mbc/filters/SolutionsFilter.tsx
@@ -119,7 +119,7 @@ const SolutionsFilter = ({
   const [dataFilterApplied, setFilterApplied] = useState(false);
 
   const isPortfolioPage = pathname === '/portfolio';
-  const isAllSolutionsPage = pathname === '/allsolutions';
+  const isAllSolutionsPage = pathname === '/allsolutions?page=1';
 
   useEffect(()=>{
     onsetTags(setSelectedTags);
