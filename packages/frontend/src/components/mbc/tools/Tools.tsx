2f6630a7d15b0f6906e865ed460f1eebcfcb4684
Shubham <sbm_verma31@rediffmail.com>
2023-01-19 13:40:34 +0530

    Updated card layout for tools

--- packages/frontend/src/components/mbc/tools/Tools.tsx
+++ packages/frontend/src/components/mbc/tools/Tools.tsx
@@ -42,6 +42,7 @@ const Tools = () => {
               isSmallCard={card.isSmallCard}
               isMediumCard={card.isMediumCard}
               svgIcon={card.svgIcon}
+              animation={card.animation}
               className="tools"
             />
           );
