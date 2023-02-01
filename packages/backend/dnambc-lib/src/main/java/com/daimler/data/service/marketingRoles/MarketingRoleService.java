cb7f1e41943b7bfeb542742cafeafbb4371dcffb
Pavithra <ibm.venkata_jaini@mercedes-benz.com>
2023-02-01 20:22:17 +0530

    changes for marketing roles

--- packages/backend/dnambc-lib/src/main/java/com/daimler/data/service/marketingRoles/MarketingRoleService.java
+++ packages/backend/dnambc-lib/src/main/java/com/daimler/data/service/marketingRoles/MarketingRoleService.java
@@ -4,6 +4,8 @@ import com.daimler.data.db.entities.MarketingRoleNsql;
 import com.daimler.data.dto.marketingRole.MarketingRoleVO;
 import com.daimler.data.service.common.CommonService;
 
-public interface MarketingRoleService extends CommonService<MarketingRoleVO, MarketingRoleNsql, String>{
+public interface MarketingRoleService extends CommonService<MarketingRoleVO, MarketingRoleNsql, String>{	
+
+	boolean deleteRole(String id);
 
 }
