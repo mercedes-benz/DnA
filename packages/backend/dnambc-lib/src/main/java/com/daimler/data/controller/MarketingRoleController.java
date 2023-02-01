3c05cd519c6ffe6258e142bd6b03ff346d540e60
Pavithra <ibm.venkata_jaini@mercedes-benz.com>
2023-02-01 20:22:17 +0530

    changes for marketing roles

--- packages/backend/dnambc-lib/src/main/java/com/daimler/data/controller/MarketingRoleController.java
+++ packages/backend/dnambc-lib/src/main/java/com/daimler/data/controller/MarketingRoleController.java
@@ -3,6 +3,7 @@ package com.daimler.data.controller;
 import java.util.Comparator;
 import java.util.List;
 
+import javax.persistence.EntityNotFoundException;
 import javax.validation.Valid;
 
 import org.springframework.http.HttpStatus;
@@ -11,17 +12,24 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.daimler.data.api.marketingRole.MarketingRolesApi;
+import com.daimler.data.application.auth.UserStore;
 import com.daimler.data.controller.exceptions.GenericMessage;
+import com.daimler.data.controller.exceptions.MessageDescription;
 import com.daimler.data.dto.marketingRole.MarketingRoleCollection;
 import com.daimler.data.dto.marketingRole.MarketingRoleRequestVO;
 import com.daimler.data.dto.marketingRole.MarketingRoleVO;
+import com.daimler.data.dto.solution.CreatedByVO;
+import com.daimler.data.dto.userinfo.UserInfoVO;
+import com.daimler.data.dto.userinfo.UserRoleVO;
 import com.daimler.data.service.marketingRoles.MarketingRoleService;
+import com.daimler.data.service.userinfo.UserInfoService;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -39,6 +47,13 @@ public class MarketingRoleController implements MarketingRolesApi {
 
 	@Autowired
 	private MarketingRoleService marketingRoleService;
+	
+	@Autowired
+	private UserStore userStore;
+
+	@Autowired
+	private UserInfoService userInfoService;
+
 
 	@Override
 	@ApiOperation(value = "Adds a new marketingRole.", nickname = "create", notes = "Adds a new non existing marketingRoles which is used in providing solution.", response = MarketingRoleVO.class, tags = {
@@ -108,6 +123,62 @@ public class MarketingRoleController implements MarketingRolesApi {
 			log.debug("No marketingRoles found");
 			return new ResponseEntity<>(marketingRoleCollection, HttpStatus.NO_CONTENT);
 		}
+	}
+
+	@Override
+	 @ApiOperation(value = "Deletes the marketingRole identified by given ID.", nickname = "delete", notes = "Deletes the marketingRole identified by given ID", response = GenericMessage.class, tags={ "marketingRoles", })
+    @ApiResponses(value = { 
+        @ApiResponse(code = 200, message = "Successfully deleted.", response = GenericMessage.class),
+        @ApiResponse(code = 400, message = "Bad request"),
+        @ApiResponse(code = 401, message = "Request does not have sufficient credentials."),
+        @ApiResponse(code = 403, message = "Request is not authorized."),
+        @ApiResponse(code = 404, message = "Invalid id, record not found."),
+        @ApiResponse(code = 500, message = "Internal error") })
+    @RequestMapping(value = "/marketingRoles/{id}",
+        produces = { "application/json" }, 
+        consumes = { "application/json" },
+        method = RequestMethod.DELETE)
+	public ResponseEntity<GenericMessage> delete(
+			@ApiParam(value = "Id of the marketingRole",required=true) @PathVariable("id") String id) {
+
+		try {			
+			CreatedByVO currentUser = this.userStore.getVO();
+			String userId = currentUser != null ? currentUser.getId() : "";
+			if (userId != null && !"".equalsIgnoreCase(userId)) {
+				UserInfoVO userInfoVO = userInfoService.getById(userId);
+				if (userInfoVO != null) {
+					List<UserRoleVO> userRoleVOs = userInfoVO.getRoles();
+					if (userRoleVOs != null && !userRoleVOs.isEmpty()) {
+						boolean isAdmin = userRoleVOs.stream().anyMatch(n -> "Admin".equalsIgnoreCase(n.getName()));
+						if (userId == null || !isAdmin) {
+							MessageDescription notAuthorizedMsg = new MessageDescription();
+							notAuthorizedMsg.setMessage(
+									"Not authorized to delete roles. User does not have admin privileges.");
+							GenericMessage errorMessage = new GenericMessage();
+							errorMessage.addErrors(notAuthorizedMsg);
+							return new ResponseEntity<>(errorMessage, HttpStatus.FORBIDDEN);
+						}
+					}
+				}
+			}
+			marketingRoleService.deleteRole(id);
+			GenericMessage successMsg = new GenericMessage();
+			successMsg.setSuccess("success");
+			return new ResponseEntity<>(successMsg, HttpStatus.OK);
+		} catch (EntityNotFoundException e) {
+			LOGGER.error("No Role found with the given id: {}", e.getMessage());
+			MessageDescription invalidMsg = new MessageDescription("No Role found with the given id.");
+			GenericMessage errorMessage = new GenericMessage();
+			errorMessage.addErrors(invalidMsg);
+			return new ResponseEntity<>(errorMessage, HttpStatus.NOT_FOUND);
+		} catch (Exception e) {
+			LOGGER.error("Failed to delete due to internal error: {}", e.getMessage());
+			MessageDescription exceptionMsg = new MessageDescription("Failed to delete due to internal error.");
+			GenericMessage errorMessage = new GenericMessage();
+			errorMessage.addErrors(exceptionMsg);
+			return new ResponseEntity<>(errorMessage, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	
 	}
 
 }
