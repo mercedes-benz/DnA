e22003fdbd904078eae61a34760d72d9462b126e
vikasmadan09 <vikasmadan09@gmail.com>
2023-02-06 20:12:52 +0530

    optional chaining for the file path name

--- packages/storage-mfe/src/components/Explorer/FileExplorer.js
+++ packages/storage-mfe/src/components/Explorer/FileExplorer.js
@@ -493,7 +493,7 @@ const FileExplorer = () => {
         data,
       });
     } else if (data.id === CustomActions.CopyPath.id) {
-      const filePath = bucketName + '/' + data.state.selectedFiles[0].objectName;
+      const filePath = bucketName + '/' + data.state?.selectedFiles?.[0]?.objectName;
       navigator.clipboard.writeText(filePath).then(() => Notification.show('File path copied to clipboard'));
     }
   };
