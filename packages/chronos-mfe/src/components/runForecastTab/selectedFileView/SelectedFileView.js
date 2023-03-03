5b8e2bf78238780281c2c90a0b9171c383e682e5
Khartade <khartaa@apac.corpdir.net>
2023-03-03 12:07:21 +0530

    change styles to Styles

--- packages/chronos-mfe/src/components/runForecastTab/selectedFileView/SelectedFileView.js
+++ packages/chronos-mfe/src/components/runForecastTab/selectedFileView/SelectedFileView.js
@@ -1,29 +1,29 @@
 import classNames from 'classnames';
 import React from 'react';
-import styles from './selected-file-view.scss';
+import Styles from './selected-file-view.scss';
 
 const SelectedFile = ({ selectedFile, setSelected, setIsExistingInputFile }) => {
   return (
     <>
-      <div className={styles.selectedFile}>
+      <div className={Styles.selectedFile}>
         <div>
           <span>Input File</span>
           <span>{selectedFile?.name}</span>
         </div>
-        <div className={styles.msgContainer}>
-          <i className={classNames('icon mbc-icon check circle', styles.checkCircle)} />
+        <div className={Styles.msgContainer}>
+          <i className={classNames('icon mbc-icon check circle', Styles.checkCircle)} />
           <span>File is ready to use.</span>
         </div>
-        {/* <div className={styles.msgContainer}>
-          <i className={classNames('icon mbc-icon close circle', styles.closeCircle)} />
+        {/* <div className={Styles.msgContainer}>
+          <i className={classNames('icon mbc-icon close circle', Styles.closeCircle)} />
           <span>Something&#8217;s wrong with the Input File. Please select correct data set. <a href="#">Here</a> you can find further information on how to set up data correctly.</span>
         </div>
-        <div className={styles.msgContainer}>
-          <i className={classNames('icon mbc-icon alert circle', styles.alertCircle)} />
+        <div className={Styles.msgContainer}>
+          <i className={classNames('icon mbc-icon alert circle', Styles.alertCircle)} />
           <span>Index not sorted. Either delete Input File and select another one or try to run anyway.</span>
         </div> */}
         <div>
-          <i onClick={() => { setSelected(); setIsExistingInputFile(false); }} className={classNames('icon delete', styles.deleteIcon)} />
+          <i onClick={() => { setSelected(); setIsExistingInputFile(false); }} className={classNames('icon delete', Styles.deleteIcon)} />
         </div>
       </div>
     </>
