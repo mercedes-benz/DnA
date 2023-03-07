a6f4466cd612cb4bb38604d50f862489c3624419
BHEEMEN <bheemesha6666@gmail.com>
2023-03-07 19:56:03 +0530

    Introduced new teams api.

---
+++ packages/backend/dnambc-lib/src/main/java/com/daimler/data/client/teamsApi/TeamsApiResponseWrapperDto.java
@@ -0,0 +1,16 @@
+package com.daimler.data.client.teamsApi;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class TeamsApiResponseWrapperDto {
+    private List<TeamsApiResponseDto> entries;
+}
