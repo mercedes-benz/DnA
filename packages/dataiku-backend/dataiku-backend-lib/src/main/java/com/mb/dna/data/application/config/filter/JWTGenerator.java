d775bdce85cd2fe17dc43219985ac76a7ac6902c
Aluri <bealuri@apac.corpdir.net>
2023-03-11 13:17:36 +0530

    refactored backend packages

---
+++ packages/dataiku-backend/dataiku-backend-lib/src/main/java/com/mb/dna/data/application/config/filter/JWTGenerator.java
@@ -0,0 +1,37 @@
+package com.mb.dna.data.application.config.filter;
+
+import javax.xml.bind.DatatypeConverter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.SignatureException;
+import io.jsonwebtoken.UnsupportedJwtException;
+import io.micronaut.context.annotation.Value;
+import jakarta.inject.Singleton;
+
+@Singleton
+public class JWTGenerator {
+
+	private static Logger log = LoggerFactory.getLogger(JWTGenerator.class);
+	
+	@Value("${jwt.secret.key}")
+	private static String secret_key;
+
+	public static Claims decodeJWT(String jwt) {
+		try {
+			Claims claims = Jwts.parser().setSigningKey(DatatypeConverter.parseBase64Binary(secret_key))
+					.parseClaimsJws(jwt).getBody();
+			return claims;
+		} catch (ExpiredJwtException | MalformedJwtException | SignatureException | UnsupportedJwtException
+				| IllegalArgumentException e) {
+			log.error("Error parsing JWT:{}", e.getMessage());
+			return null;
+		}
+	}
+	
+}
