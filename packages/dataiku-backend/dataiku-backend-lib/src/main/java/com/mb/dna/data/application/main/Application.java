7eb1fd8d3875b5d6927654183e56cb8b7efd9d28
Aluri <bealuri@apac.corpdir.net>
2023-03-13 04:39:42 +0530

    userprivileges

--- packages/dataiku-backend/dataiku-backend-lib/src/main/java/com/mb/dna/data/application/main/Application.java
+++ packages/dataiku-backend/dataiku-backend-lib/src/main/java/com/mb/dna/data/application/main/Application.java
@@ -1,7 +1,16 @@
 package com.mb.dna.data.application.main;
 
 import io.micronaut.runtime.Micronaut;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 
+@OpenAPIDefinition(
+        info = @Info(
+                title = "dataiku-service",
+                version = "1.0",
+                description = "Dataiku APIs",
+                contact = @Contact(url = "", name = "BEALURI")))
 public class Application {
 
     public static void main(String[] args) {
