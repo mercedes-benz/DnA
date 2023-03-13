f0efef2e931c4c97cc2bcf2f447642c3797499b2
Aluri <bealuri@apac.corpdir.net>
2023-03-13 16:56:48 +0530

    User privileges api

--- packages/dataiku-backend/dataiku-backend-lib/src/main/java/com/mb/dna/data/controller/dataiku/DataikuProjectDto.java
+++ packages/dataiku-backend/dataiku-backend-lib/src/main/java/com/mb/dna/data/controller/dataiku/DataikuProjectDto.java
@@ -1,26 +1,23 @@
-package com.mb.dna.data.application.config.filter;
+package com.mb.dna.data.controller.dataiku;
 
 import java.io.Serializable;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-//import io.micronaut.serde.annotation;
-import io.micronaut.context.annotation.Bean;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
-@ToString
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class UserRole implements Serializable{
-	
+public class DataikuProjectDto implements Serializable {
+
 	private String id;
-	private String name;
+	private String projectName;
+	private String description;
+	private String cloudProfile;
+	private List<CollaboratorDetailsDto> collaborators;
 }
