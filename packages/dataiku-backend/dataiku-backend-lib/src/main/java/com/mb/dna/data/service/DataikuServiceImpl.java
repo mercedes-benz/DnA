83491a3c53d363b1eb272de8ffc6c5509cc4f8f4
Aluri <bealuri@apac.corpdir.net>
2023-03-13 16:56:48 +0530

    User privileges api

--- packages/dataiku-backend/dataiku-backend-lib/src/main/java/com/mb/dna/data/service/DataikuServiceImpl.java
+++ packages/dataiku-backend/dataiku-backend-lib/src/main/java/com/mb/dna/data/service/DataikuServiceImpl.java
@@ -1,9 +1,30 @@
 package com.mb.dna.data.service;
 
+import com.mb.dna.data.db.repo.DataikuRepository;
+import com.mb.dna.data.utility.DataikuAssembler;
+
+import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
 
 @Singleton
 public class DataikuServiceImpl implements DataikuService	{
 
+	@Inject
+	private DataikuRepository dataikuRepo;
+	
+	@Inject
+	private DataikuAssembler assembler;
+	
+	public DataikuServiceImpl() {
+		super();
+	}
+	
+	public DataikuServiceImpl(DataikuRepository dataikuRepo, DataikuAssembler assembler) {
+		super();
+		this.dataikuRepo = dataikuRepo;
+		this.assembler = assembler;
+	}
+	
+	
 	
 }
---
+++ packages/dataiku-backend/dataiku-backend-lib/src/main/java/com/mb/dna/data/utility/DataikuAssembler.java
@@ -0,0 +1,8 @@
+package com.mb.dna.data.utility;
+
+import jakarta.inject.Singleton;
+
+@Singleton
+public class DataikuAssembler {
+
+}
