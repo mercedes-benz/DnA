acc13ddd2326be7eebcb165d4485493a9fdf78a5
Aluri <bealuri@apac.corpdir.net>
2023-03-15 22:04:51 +0530

    cors change

--- packages/dataiku-backend/dataiku-backend-lib/src/main/java/com/mb/dna/data/application/main/CorsAdvice.java
+++ packages/dataiku-backend/dataiku-backend-lib/src/main/java/com/mb/dna/data/application/main/CorsAdvice.java
@@ -15,7 +15,7 @@ public class CorsAdvice  implements HttpServerFilter {
 		return Publishers.then(chain.proceed(request), resp -> {
 			resp.getHeaders().add("Access-Control-Allow-Credentials","true");
 			resp.getHeaders().add("Access-Control-Allow-Methods","*");
-			resp.getHeaders().add("Access-Control-Allow-Origin","*");
+			//resp.getHeaders().add("Access-Control-Allow-Origin","*");
 			resp.getHeaders().add("Access-Control-Allow-Headers","*");
       });
 	}
