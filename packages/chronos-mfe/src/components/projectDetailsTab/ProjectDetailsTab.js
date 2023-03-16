f22d6e412ccdd6d9e6511392b8f68851e61aa862
Khartade <khartaa@apac.corpdir.net>
2023-03-16 16:14:02 +0530

    Project details fixes

--- packages/chronos-mfe/src/components/projectDetailsTab/ProjectDetailsTab.js
+++ packages/chronos-mfe/src/components/projectDetailsTab/ProjectDetailsTab.js
@@ -1,5 +1,6 @@
 import classNames from 'classnames';
 import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import Styles from './project-details-tab.scss';
 // import from DNA Container
 import TeamMemberListItem from 'dna-container/TeamMemberListItem';
@@ -9,17 +10,41 @@ import ChronosProjectForm from '../chronosProjectForm/ChronosProjectForm';
 import ChronosAccessDetails from '../chronosAccessDetails/ChronosAccessDetails';
 // utils
 import { regionalDateAndTimeConversionSolution } from '../../utilities/utils';
+import ProgressIndicator from '../../common/modules/uilab/js/src/progress-indicator';
+import { chronosApi } from '../../apis/chronos.api';
+import Spinner from '../spinner/Spinner';
 
-const ProjectDetailsTab = ({project, onRefresh}) => {
+const ProjectDetailsTab = () => {
+  const { id: projectId } = useParams();
+  const [project, setProject] = useState();
+  const [loading, setLoading] = useState(true);
   const [editProject, setEditProject] = useState(false);
   const [teamMembers, setTeamMembers] = useState([]);
 
   useEffect(() => {
-    if(project.collaborators !== null) {
-      const members = project.collaborators.map(member => ({...member, shortId: member.id, userType: 'internal'}));
+    getProjectById();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const getProjectById = () => {
+    ProgressIndicator.show();
+    chronosApi.getForecastProjectById(projectId).then((res) => {
+      setProject(res.data);
+      ProgressIndicator.hide();
+      setLoading(false);
+    }).catch(error => {
+      Notification.show(error.message, 'alert');
+      ProgressIndicator.hide();
+      setLoading(false);
+    });
+  };
+  
+  useEffect(() => {
+    if(!loading && project?.collaborators !== null) {
+      const members = project?.collaborators.map(member => ({...member, shortId: member.id, userType: 'internal'}));
       setTeamMembers(members);
     }
-  }, [project]);
+  }, [loading, project]);
 
   const teamMembersList = teamMembers?.map((member, index) => {
     return (
@@ -30,8 +55,8 @@ const ProjectDetailsTab = ({project, onRefresh}) => {
         hidePosition={true}
         hideContextMenu={!editProject}
         showInfoStacked={true}
-        showMoveUp={index !== 0}
-        showMoveDown={index + 1 !== teamMembers?.length}
+        showMoveUp={false}
+        showMoveDown={false}
       />
     );
   });
@@ -47,39 +72,45 @@ const ProjectDetailsTab = ({project, onRefresh}) => {
         <h3 id="productName">Project Details</h3>
         <div className={Styles.firstPanel}>
           <div className={Styles.formWrapper}>
-            <div className={classNames(Styles.flexLayout, Styles.threeColumn)}>
-              <div id="productDescription">
-                <label className="input-label summary">Project Name</label>
-                <br />                    
-                {project?.name}
+            { loading && <Spinner /> }
+            { !loading && 
+              <div className={classNames(Styles.flexLayout, Styles.threeColumn)}>
+                <div id="productDescription">
+                  <label className="input-label summary">Project Name</label>
+                  <br />                    
+                  {project?.name}
+                </div>
+                <div id="tags">
+                  <label className="input-label summary">Created on</label>
+                  <br />
+                  {project?.createdOn !== undefined && regionalDateAndTimeConversionSolution(project?.createdOn)}
+                </div>
+                <div id="isExistingSolution">
+                  <label className="input-label summary">Created by</label>
+                  <br />
+                  {project?.createdBy?.firstName} {project?.createdBy?.lastName}
+                </div>
               </div>
-              <div id="tags">
-                <label className="input-label summary">Created on</label>
-                <br />
-                {project?.createdOn !== undefined && regionalDateAndTimeConversionSolution(project?.createdOn)}
-              </div>
-              <div id="isExistingSolution">
-                <label className="input-label summary">Created by</label>
-                <br />
-                {project?.createdBy?.firstName} {project?.createdBy?.lastName}
-              </div>
-            </div>
+            }
           </div>
         </div>
       </div>
       <div className={Styles.content}>
         <h3 id="productName">Collaborators</h3>
         <div className={Styles.firstPanel}>
-        <div className={Styles.collabAvatar}>
-          <div className={Styles.teamListWrapper}>
-            {teamMembers.length === 0 ? <p className={Styles.noCollaborator}>No Collaborators</p> : null}
-            {teamMembers.length !== 0 ?
-              <div className={Styles.membersList}>
-                {teamMembersList}
-              </div> : null
-            }
+        { loading && <Spinner /> }
+        { !loading && 
+          <div className={Styles.collabAvatar}>
+            <div className={Styles.teamListWrapper}>
+              {teamMembers.length === 0 ? <p className={Styles.noCollaborator}>No Collaborators</p> : null}
+              {teamMembers.length !== 0 ?
+                <div className={Styles.membersList}>
+                  {teamMembersList}
+                </div> : null
+              }
+            </div>
           </div>
-        </div>
+        }
         </div>
       </div>
       <div className={Styles.content}>
@@ -94,7 +125,7 @@ const ProjectDetailsTab = ({project, onRefresh}) => {
           modalWidth={'60%'}
           buttonAlignment="right"
           show={editProject}
-          content={<ChronosProjectForm edit={true} project={project} onSave={() => {setEditProject(false); onRefresh()}} />}
+          content={<ChronosProjectForm edit={true} project={project} onSave={() => {setEditProject(false); getProjectById()}} />}
           scrollableContent={false}
           onCancel={() => setEditProject(false)}
           modalStyle={{
