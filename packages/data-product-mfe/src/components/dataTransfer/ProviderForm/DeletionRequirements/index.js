b262fe016f0589e8996a184eeec8e71151233715
Maria Rathinam <amariar@capelit77946372.inedc.corpintra.net>
2023-03-17 15:48:00 +0530

    Label updates

--- packages/data-product-mfe/src/components/dataTransfer/ProviderForm/DeletionRequirements/index.js
+++ packages/data-product-mfe/src/components/dataTransfer/ProviderForm/DeletionRequirements/index.js
@@ -125,9 +125,7 @@ const DeletionRequirements = (
             <div className={Styles.formWrapper}>
               <div id="otherRelevantInfoDescription" className={classNames('input-field-group area')}>
                 <label className="input-label" htmlFor="otherRelevantInfo">
-                  Please provide any other relevant & app specific restrictions that might apply to the corresponding
-                  data, examples being individual deletion requirements, antitrust regulations, contractual restrictions
-                  etc.
+                  Please provide any other relevant & app specific restrictions that might apply to the corresponding data, examples being antitrust regulations, contractual restrictions etc.
                 </label>
                 <textarea
                   className="input-field-area"
