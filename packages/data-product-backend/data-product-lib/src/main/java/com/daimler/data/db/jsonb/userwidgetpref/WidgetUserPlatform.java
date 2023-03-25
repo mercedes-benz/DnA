1fcad21e59917e042cfbe8040f6f9e908335322b
System Administrator <root@capelit76658713.inedc.corpintra.net>
2023-03-25 11:57:01 +0530

    Dataproduct widget and list owner filter

---
+++ packages/data-product-backend/data-product-lib/src/main/java/com/daimler/data/db/jsonb/userwidgetpref/WidgetUserPlatform.java
@@ -0,0 +1,17 @@
+package com.daimler.data.db.jsonb.userwidgetpref;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class WidgetUserPlatform implements Serializable {
+    private static final long serialVersionUID = -6062902528095302918L;
+
+    private String id;
+    private String name;
+}
