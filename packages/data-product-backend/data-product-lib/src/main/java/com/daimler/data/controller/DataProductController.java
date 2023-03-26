05fa1f792eb399324a288f6c50e04107caf83429
Pavithra <ibm.venkata_jaini@mercedes-benz.com>
2023-03-26 13:44:40 +0530

    Moving insider information from transnational transfer to deletion requirements

--- packages/data-product-backend/data-product-lib/src/main/java/com/daimler/data/controller/DataProductController.java
+++ packages/data-product-backend/data-product-lib/src/main/java/com/daimler/data/controller/DataProductController.java
@@ -78,7 +78,7 @@ public class DataProductController implements DataproductsApi{
 				   Objects.isNull(requestVO.getContactInformation()) ||
 				   Objects.isNull(requestVO.getPersonalRelatedData()) ||
 				   Objects.isNull(requestVO.getTransnationalDataTransfer())||
-				   Objects.isNull(requestVO.getDeletionRequirement()) || Objects.isNull(requestVO.getTransnationalDataTransfer().getInsiderInformation())) {
+				   Objects.isNull(requestVO.getDeletionRequirement()) || Objects.isNull(requestVO.getDeletionRequirement().getInsiderInformation())) {
 					List<MessageDescription> messages = new ArrayList<>();
 					MessageDescription message = new MessageDescription();
 					message.setMessage("DataProduct cannot be created as user is not providing the values for one of the tabs.");
@@ -527,7 +527,7 @@ public class DataProductController implements DataproductsApi{
 								   Objects.isNull(requestVO.getContactInformation()) ||
 								   Objects.isNull(requestVO.getPersonalRelatedData()) ||
 								   Objects.isNull(requestVO.getTransnationalDataTransfer())||
-								   Objects.isNull(requestVO.getDeletionRequirement()) || Objects.isNull(requestVO.getTransnationalDataTransfer().getInsiderInformation())) {
+								   Objects.isNull(requestVO.getDeletionRequirement()) || Objects.isNull(requestVO.getDeletionRequirement().getInsiderInformation())) {
 							List<MessageDescription> messages = new ArrayList<>();
 							MessageDescription message = new MessageDescription();
 							message.setMessage("DataProduct cannot be created as user is not providing the values for one of the tabs.");
@@ -542,7 +542,7 @@ public class DataProductController implements DataproductsApi{
 						   Objects.isNull(requestVO.getContactInformation()) ||
 						   Objects.isNull(requestVO.getPersonalRelatedData()) ||
 						   Objects.isNull(requestVO.getTransnationalDataTransfer())||
-						   Objects.isNull(requestVO.getDeletionRequirement()) || Objects.isNull(requestVO.getTransnationalDataTransfer().getInsiderInformation())) {
+						   Objects.isNull(requestVO.getDeletionRequirement()) || Objects.isNull(requestVO.getDeletionRequirement().getInsiderInformation())) {
 							List<MessageDescription> messages = new ArrayList<>();
 							MessageDescription message = new MessageDescription();
 							message.setMessage("DataProduct cannot be created as user is not providing the values for one of the tabs.");
