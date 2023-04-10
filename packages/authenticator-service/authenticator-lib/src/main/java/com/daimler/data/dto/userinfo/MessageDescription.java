9d429a99694297c81e373a9cb7f2f0c12a7f1356
Aluri <bealuri@apac.corpdir.net>
2023-04-10 17:12:49 +0530

    new authenticator-service init commit

---
+++ packages/authenticator-service/authenticator-lib/src/main/java/com/daimler/data/dto/userinfo/MessageDescription.java
@@ -0,0 +1,81 @@
+package com.daimler.data.dto.userinfo;
+
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import org.springframework.validation.annotation.Validated;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+
+/**
+ * MessageDescription
+ */
+@Validated
+
+
+public class MessageDescription   {
+  @JsonProperty("message")
+  private String message = null;
+
+  public MessageDescription message(String message) {
+    this.message = message;
+    return this;
+  }
+
+  /**
+   * message
+   * @return message
+  **/
+  @ApiModelProperty(value = "message")
+
+
+  public String getMessage() {
+    return message;
+  }
+
+  public void setMessage(String message) {
+    this.message = message;
+  }
+
+
+  @Override
+  public boolean equals(java.lang.Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    MessageDescription messageDescription = (MessageDescription) o;
+    return Objects.equals(this.message, messageDescription.message);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(message);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class MessageDescription {\n");
+    
+    sb.append("    message: ").append(toIndentedString(message)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(java.lang.Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
+
---
+++ packages/authenticator-service/authenticator-lib/src/main/java/com/daimler/data/dto/userrole/UserRoleCollection.java
@@ -0,0 +1,57 @@
+package com.daimler.data.dto.userrole;
+
+import java.util.Objects;
+import com.daimler.data.dto.userrole.UserRoleVO;
+import io.swagger.annotations.ApiModel;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.validation.annotation.Validated;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+
+/**
+ * Collection of Userroles.
+ */
+@ApiModel(description = "Collection of Userroles.")
+@Validated
+
+
+public class UserRoleCollection extends ArrayList<UserRoleVO>  {
+
+  @Override
+  public boolean equals(java.lang.Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    return true;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(super.hashCode());
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class UserRoleCollection {\n");
+    sb.append("    ").append(toIndentedString(super.toString())).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(java.lang.Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
+
