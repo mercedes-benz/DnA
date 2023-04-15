a07aeb64e82288d633d2f3a023f889d8c5ea1257
System Administrator <root@capelit76658713.inedc.corpintra.net>
2023-04-15 12:12:38 +0530

    Dataiku on premise list changes

--- packages/backend/dnambc-lib/src/main/java/com/daimler/data/controller/DataikuController.java
+++ packages/backend/dnambc-lib/src/main/java/com/daimler/data/controller/DataikuController.java
@@ -1,19 +1,19 @@
 /* LICENSE START
- * 
+ *
  * MIT License
- * 
+ *
  * Copyright (c) 2019 Daimler TSS GmbH
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -21,12 +21,13 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
- * 
- * LICENSE END 
+ *
+ * LICENSE END
  */
 
 package com.daimler.data.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.validation.Valid;
@@ -98,11 +99,15 @@ public class DataikuController implements DataikuApi {
 		DataikuProjectVOCollection col = null;
 		CreatedByVO currentUser = this.userStore.getVO();
 		String userId = currentUser != null ? currentUser.getId() : null;
-		List<DataikuProjectVO> projects = vService.getAllDataikuProjects(userId, live);
-		if (!ObjectUtils.isEmpty(projects)) {
+		List<DataikuProjectVO> extolloprojects = vService.getAllDataikuProjects(userId, live, "extollo");
+		List<DataikuProjectVO> onPremprojects = vService.getAllDataikuProjects(userId, live, "onprem");
+		List<DataikuProjectVO> consolidateList = new ArrayList<>();
+		if(extolloprojects != null && !extolloprojects.isEmpty()) consolidateList.addAll(extolloprojects);
+		if(onPremprojects != null && !onPremprojects.isEmpty()) consolidateList.addAll(onPremprojects);
+		if (!ObjectUtils.isEmpty(consolidateList)) {
 			col = new DataikuProjectVOCollection();
-			col.setData(projects);
-			col.setTotalCount(projects.size());
+			col.setData(consolidateList);
+			col.setTotalCount(consolidateList.size());
 		}
 		if (col != null) {
 			log.debug("Returning projects found for user {} ", userId);
@@ -130,7 +135,24 @@ public class DataikuController implements DataikuApi {
 			@ApiParam(value = "ProjectKey for which project to be fetched", required = true) @PathVariable("projectKey") String projectKey,
 			@NotNull @ApiParam(value = "If requested data from live(Production) or training environment", required = true, defaultValue = "true") @Valid @RequestParam(value = "live", required = true, defaultValue = "true") Boolean live) {
 		LOGGER.trace("Entering getByProjectKey");
-		DataikuProjectVO projectVO = vService.getByProjectKey(projectKey, live);
+		DataikuProjectVO extolloVo = null;
+		DataikuProjectVO onpremVo = null;
+		try {
+			 extolloVo = vService.getByProjectKey(projectKey, live, "extollo");
+		} catch (Exception e) {
+			log.error("Failed to fetch extollo by project key with an exception {} ", e.getMessage());
+		}
+		try {
+			onpremVo = vService.getByProjectKey(projectKey, live, "onprem");
+		} catch (Exception e) {
+			log.error("Failed to fetch onprem by project key with an exception {} ", e.getMessage());
+		}
+		DataikuProjectVO projectVO = null;
+		if (extolloVo != null) {
+			projectVO = extolloVo;
+		} else if (onpremVo != null) {
+			projectVO = onpremVo;
+		}
 		if (projectVO != null) {
 			log.debug("Returning project found {} ", projectKey);
 			return new ResponseEntity<>(projectVO, HttpStatus.OK);
