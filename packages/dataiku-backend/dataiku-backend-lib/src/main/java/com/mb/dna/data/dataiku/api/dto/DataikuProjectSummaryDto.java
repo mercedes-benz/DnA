3513100dc4d05f4e5778ceab8f09349a268ae203
boonbaluri <boonbaluri@gmail.com>
2023-05-24 21:57:41 +0530

    Feature/dataikugetcollection (#1647)
    * added chronos run comparison api implementation
    
    * upload actual file to storage
    
    * init checkin for chronos comparisons
    
    * comparisons validation for create
    
    * added delete and getall api impl for chronos comparisons
    
    * added chronos comparison api impl changes
    
    * added chronos comparison api impl changes
    
    * added chronos comparison api implementation
    
    * remove deleted runs before returning from getById forecast details
    
    * added changes for get comparison.html
    
    * Dataiku get summary changes
    
    * assembler changes
    
    * assembler changes
    
    * include all in json
    
    * iso date changes
    
    ---------
    
    Co-authored-by: Ramnath <nramnat@apac.corpdir.net>
    Co-authored-by: Aluri <bealuri@apac.corpdir.net>

--- packages/dataiku-backend/dataiku-backend-lib/src/main/java/com/mb/dna/data/dataiku/api/dto/DataikuProjectSummaryDto.java
+++ packages/dataiku-backend/dataiku-backend-lib/src/main/java/com/mb/dna/data/dataiku/api/dto/DataikuProjectSummaryDto.java
@@ -1,7 +1,6 @@
 package com.mb.dna.data.dataiku.api.dto;
 
 import java.io.Serializable;
-import java.util.Date;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
@@ -17,17 +16,22 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @JsonInclude(Include.ALWAYS)
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class DataikuProjectUpdateDto implements Serializable{
-
-	private String description;
+public class DataikuProjectSummaryDto implements Serializable{
+    
+	private static final long serialVersionUID = 1L;
+	
+	private String id;
+	private String name;
+	private String projectKey;
+	private String shortDesc;
+	private Boolean isProjectAdmin;
+	private DataikuProjectCheckListDto checklists;
+	private String role;
+	private String cloudProfile;
 	private List<CollaboratorDetailsDto> collaborators;
 	private String status;
 	private String classificationType;
-	private Boolean hasPii;
-	private String divisionId;
-	private String divisionName;
-	private String subdivisionId;
-	private String subdivisionName;
-	private String department;
-	
+	private String solutionId;
+	private List<String> tags;
+	private DataikuProjectTimeStampDetailsDto creationTag;
 }
