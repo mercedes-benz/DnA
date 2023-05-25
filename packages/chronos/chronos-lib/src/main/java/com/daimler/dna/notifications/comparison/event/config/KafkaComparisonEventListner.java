97d285c8d0163ac823e53545ebe6a98f1365ce84
Namitha-01dev <125283526+Namitha-01dev@users.noreply.github.com>
2023-05-25 15:54:21 +0530

    Chronos runs comparisons implementation (#1653)
    * added chronos run comparison api implementation
    
    * upload actual file to storage
    
    * init checkin for chronos comparisons
    
    * comparisons validation for create
    
    * added delete and getall api impl for chronos comparisons
    
    * added chronos comparison api impl changes
    
    * added chronos comparison api impl changes
    
    * added chronos comparison api implementation
    
    * remove deleted runs before returning from getById forecast details
    
    * added changes for get comparison.html
    
    * partial checkin comparison implementation
    
    * added changes for chronos comparison api impl
    
    * changes
    
    * added chronos comparison impl changes
    
    * version update for backend
    
    ---------
    
    Co-authored-by: Ramnath <nramnat@apac.corpdir.net>
    Co-authored-by: Aluri <bealuri@apac.corpdir.net>

---
+++ packages/chronos/chronos-lib/src/main/java/com/daimler/dna/notifications/comparison/event/config/KafkaComparisonEventListner.java
@@ -0,0 +1,64 @@
+/* LICENSE START
+ * 
+ * MIT License
+ * 
+ * Copyright (c) 2019 Daimler TSS GmbH
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * 
+ * LICENSE END 
+ */
+
+package com.daimler.dna.notifications.comparison.event.config;
+
+import com.daimler.data.service.forecast.ForecastService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import com.daimler.dna.notifications.common.event.config.GenericEventRecord;
+import com.daimler.dna.notifications.common.producer.KafkaDynamicProducerService;
+
+@Service
+public class KafkaComparisonEventListner {
+
+	private static Logger LOGGER = LoggerFactory.getLogger(KafkaComparisonEventListner.class);
+
+	@Autowired
+	private KafkaDynamicProducerService dynamicProducer;
+
+	@Value("${kafka.comparisonTopic.name}")
+	private String dnaChronosComparisonTopicName;
+
+	@Autowired
+	private ForecastService service;
+
+//	@KafkaListener(topics = "dnaChronosComparisonEventTopic")
+//	public void chronosComparisonTopicListnerToProcess(GenericEventRecord message) {
+//		if (message != null) {
+//			service.processForecastComparision(message.getResourceId(),message.getMessageDetails());
+//			//call forecastservice processcomparison (forecast id is resourceid from GenericEventRecord and comparision id is messageDetails from GenericEventRecord)
+//
+//		}
+//	}
+
+}
