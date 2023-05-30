b95923868352283ebfaa089cf8e33d23b6f6a6eb
pavithrajaini <114487538+pavithrajaini@users.noreply.github.com>
2023-05-30 12:25:58 +0530

    Update ConstantsUtility.java

--- packages/code-server/code-server-lib/src/main/java/com/daimler/data/util/ConstantsUtility.java
+++ packages/code-server/code-server-lib/src/main/java/com/daimler/data/util/ConstantsUtility.java
@@ -34,7 +34,7 @@ public class ConstantsUtility {
 	public static final String PYFASTAPI = "py-fastapi";
 	public static final String REACT = "react";
 	public static final String ANGULAR = "angular";
-	public static final String PUBLIC = "public";
+	public static final String PUBLIC = "generic";
 	public static final String GRADLE = "gradle";
 	public static final String DEFAULT = "default";
 	public static final String COLLABREQUESTEDSTATE = "COLLABORATION_REQUESTED";
