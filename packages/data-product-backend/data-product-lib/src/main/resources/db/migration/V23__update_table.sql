1f9a06e880f348314610e1d9180e42ba3c34f138
Venkata Jaini <venkatp@apac.corpdir.net>
2023-06-01 13:53:16 +0530

    DB scripts for removing chinese data field from dataproduct and datatransfer

---
+++ packages/data-product-backend/data-product-lib/src/main/resources/db/migration/V23__update_table.sql
@@ -0,0 +1,3 @@
+update dataproduct_nsql set data = data #- '{transnationalDataTransfer,dataFromChina}';
+
+update datatransfer_nsql set data = data #- '{providerInformation,transnationalDataTransfer,dataFromChina}';
