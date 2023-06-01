d5ddbed62a8859fe3da0397ed8dd54070e68f869
SHUVERM <SHUVERM>
2023-06-01 07:39:53 +0000

    removed commented code

--- packages/frontend/src/components/mbc/createNewReport/CreateNewReport.tsx
+++ packages/frontend/src/components/mbc/createNewReport/CreateNewReport.tsx
@@ -67,73 +67,7 @@ import { TeamMemberType } from 'globals/Enums';
 import Caption from '../shared/caption/Caption';
 
 const classNames = cn.bind(Styles);
-// const tempKPIClassifications = [
-//   {
-//       "id": "Overhead & Invest > Overhead Controlling",
-//       "name": "Overhead & Invest > Overhead Controlling"
-//   },
-//   {
-//       "id": "Overhead & Invest > Overhead Planning",
-//       "name": "Overhead & Invest > Overhead Planning"
-//   },
-//   {
-//       "id": "Overhead & Invest > Plan Cost Breakdown",
-//       "name": "Overhead & Invest > Plan Cost Breakdown"
-//   },
-//   {
-//       "id": "Overhead & Invest > Funding Planning & Reporting",
-//       "name": "Overhead & Invest > Funding Planning & Reporting"
-//   },
-//   {
-//       "id": "Overhead & Invest > Depreciation calculations",
-//       "name": "Overhead & Invest > Depreciation calculations"
-//   },
-//   {
-//       "id": "Overhead & Invest > Journalization of funding/invest",
-//       "name": "Overhead & Invest > Journalization of funding/invest"
-//   },
-//   {
-//       "id": "Overhead & Invest > Purchasing E2E Process",
-//       "name": "Overhead & Invest > Purchasing E2E Process"
-//   },
-//   {
-//       "id": "Overhead & Invest > Shift Approval",
-//       "name": "Overhead & Invest > Shift Approval"
-//   }
-// ];
 
-// const tempKPINames = [
-//   {
-//       "id": "1039",
-//       "name": "CAV",
-//       "dataType": "Overhead & Invest > Overhead Controlling",
-//   },
-//   {
-//       "id": "1038",
-//       "name": "Contribution Margin per Unit",
-//       "dataType": "Overhead & Invest > Overhead Planning",
-//   },
-//   {
-//       "id": "1041",
-//       "name": "Cost per Acquisition",
-//       "dataType": "Overhead & Invest > Plan Cost Breakdown",
-//   },
-//   {
-//       "id": "1042",
-//       "name": "EBIT",
-//       "dataType": "Overhead & Invest > Funding Planning & Reporting",
-//   },
-//   {
-//       "id": "1040",
-//       "name": "ROI",
-//       "dataType": "Overhead & Invest > Depreciation calculations",
-//   },
-//   {
-//       "id": "1043",
-//       "name": "TestKPI",
-//       "dataType": "Overhead & Invest > Purchasing E2E Process",
-//   }
-// ]
 
 export interface ICreateNewReportState {
   divisions: IDivision[];
@@ -291,7 +225,6 @@ export default class CreateNewReport extends React.Component<ICreateNewReportPro
           item['name'] = item.kpiName;
           return item;
         });
-        // const kpiNames: IKpiNameList[] = tempKPINames;
         const reportingCauses = response[5].data;
         const ressort = response[6].data;
         const statuses = response[7].data;
@@ -303,7 +236,6 @@ export default class CreateNewReport extends React.Component<ICreateNewReportPro
         const departmentTags: IDepartment[] = response[13].data;
         const dataClassifications: IDataClassification[] = response[14].data;
         const kpiClassifications: IKpiClassification[] = response[15].data;
-        // const kpiClassifications: IKpiClassification[] = tempKPIClassifications;
         const creatorInfo = this.props.user;
         const teamMemberObj: ITeams = {
           department: creatorInfo.department,
