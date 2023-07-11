1f4dd7c66896e0c63bce5cbcef7dbaa9580d841b
Shubham <sbm_verma31@rediffmail.com>
2023-07-11 17:57:00 +0530

    linked description fields to respective tabs

--- packages/data-product-mfe/src/components/dataTransfer/ProviderForm/ClassificationAndConfidentiality/index.js
+++ packages/data-product-mfe/src/components/dataTransfer/ProviderForm/ClassificationAndConfidentiality/index.js
@@ -82,7 +82,7 @@ const Classification = (
                             className="ff-only"
                             name="confidentiality"
                             value={item.name}
-                            defaultChecked={item.name === 'Internal' && true}
+                            // defaultChecked={item.name === 'Internal' && true}
                           />
                         </span>
                         <span className="label">{item.name}</span>
