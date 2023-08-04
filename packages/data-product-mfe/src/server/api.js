eff62c97a00f0ee1c96635bfff81998b7327e9c2
Ajay <shivanagolajay@gmail.com>
2023-08-04 11:44:33 +0530

    Kong gateway Changes (#1820)
    * Kong gateway chanegs
    
    * Update JWTGenerator.java
    
    * Kong gateway chanegs
    
    * Remove log
    
    * typo change
    
    * Comments resolved
    
    * comment resolved
    
    ---------
    
    Co-authored-by: Shivanagol <ajshiva@capelit76658713.inedc.corpintra.net>

--- packages/data-product-mfe/src/server/api.js
+++ packages/data-product-mfe/src/server/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { SESSION_STORAGE_KEYS } from '../Utility/constants';
 import { Envs } from '../Utility/envs';
+import { refreshToken } from 'dna-container/RefreshToken';
 
 const jwt = sessionStorage?.length ? sessionStorage.getItem(SESSION_STORAGE_KEYS.JWT) : null;
 
@@ -28,3 +29,37 @@ export const reportsServer = axios.create({
   baseURL: Envs.REPORTS_API_BASEURL ? Envs.REPORTS_API_BASEURL : `http://${window.location.hostname}:7173/api`,
   headers,
 });
+
+function createRefreshInterceptor(instance) {
+  // Interceptor to handle token refresh on 403 error
+  instance.interceptors.response.use(
+    (response) => response,
+    async (error) => {
+      console.log('error', error);
+      if (error?.response && error?.response.status === 403 && error?.response?.data?.error_description?.includes("JWT is expired")) {
+        // Refresh the token and retry the failed request.
+        const jwt = sessionStorage.getItem(SESSION_STORAGE_KEYS.JWT);
+        const newJwt = await refreshToken(jwt);
+
+        // Update the Authorization header in Axios instances.
+        server.defaults.headers.Authorization = newJwt;
+        reportsServer.defaults.headers.Authorization = newJwt;
+        hostServer.defaults.headers.Authorization = newJwt;
+
+        // Retry the original request with the new token.
+        error.config.headers.Authorization = sessionStorage.getItem(SESSION_STORAGE_KEYS.JWT);
+        return await axios.request(error.config);
+      }
+      return Promise.reject(error);
+    }
+  );
+}
+
+// Apply interceptor to server
+createRefreshInterceptor(server);
+
+// Apply interceptor to hostServer
+createRefreshInterceptor(hostServer);
+
+// Apply interceptor to reportsServer
+createRefreshInterceptor(reportsServer);
