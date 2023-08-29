3bc0d42570dbc4412d9630686539f2e858ace7bc
agnelpraveen <anna_agnel.maria_rathinam@mercedes-benz.com>
2023-08-29 13:24:52 +0530

    Removing password field from code space creation (#1902)
    Co-authored-by: Maria Rathinam <amariar@capelit77946372.inedc.corpintra.net>

--- packages/frontend/src/components/mbc/codeSpace/newCodeSpace/NewCodeSpace.tsx
+++ packages/frontend/src/components/mbc/codeSpace/newCodeSpace/NewCodeSpace.tsx
@@ -46,12 +46,7 @@ const NewCodeSpace = (props: ICodeSpaceProps) => {
   const recipes = recipesMaster;
 
   const [recipeError, setRecipeError] = useState('');
-  const [passwordError, setPasswordErr] = useState('');
-  const [confirmPasswordError, setConfirmPasswordError] = useState('');
-  const [passwordInput, setPasswordInput] = useState({
-    password: '',
-    confirmPassword: '',
-  });
+  
   // const [githubUserName, setGithubUserName] = useState('');
   // const [githubUserNameError, setGithubUserNameError] = useState('');
   const [githubToken, setGithubToken] = useState('');
@@ -132,65 +127,6 @@ const NewCodeSpace = (props: ICodeSpaceProps) => {
     setRecipeError(selectedOption !== '0' ? '' : requiredError);
   };
 
-  const handlePasswordChange = (evnt: React.FormEvent<HTMLInputElement>) => {
-    const passwordInputValue = evnt.currentTarget.value.trim();
-    const passwordInputFieldName = evnt.currentTarget.name;
-    const NewPasswordInput = { ...passwordInput, [passwordInputFieldName]: passwordInputValue };
-    setPasswordInput(NewPasswordInput);
-  };
-
-  const handleValidation = (evnt: React.FormEvent<HTMLInputElement>) => {
-    const passwordInputValue = evnt.currentTarget.value.trim();
-    const passwordInputFieldName = evnt.currentTarget.name;
-    //for password
-    if (passwordInputFieldName === 'password') {
-      const oneLetterRegExp = /(?=.*?[a-zA-Z])/;
-      // const uppercaseRegExp = /(?=.*?[A-Z])/;
-      // const lowercaseRegExp = /(?=.*?[a-z])/;
-      const digitsRegExp = /(?=.*?[0-9])/;
-      // const specialCharRegExp = /(?=.*?[#?!@$%^&*-])/;
-      const minLengthRegExp = /.{8,}/;
-      const passwordLength = passwordInputValue.length;
-      const atleastOneLetter = oneLetterRegExp.test(passwordInputValue);
-      // const uppercasePassword = uppercaseRegExp.test(passwordInputValue);
-      // const lowercasePassword = lowercaseRegExp.test(passwordInputValue);
-      const digitsPassword = digitsRegExp.test(passwordInputValue);
-      // const specialCharPassword = specialCharRegExp.test(passwordInputValue);
-      const minLengthPassword = minLengthRegExp.test(passwordInputValue);
-      let errMsg = '';
-      if (passwordLength === 0) {
-        errMsg = 'Password is empty';
-      } else if (!atleastOneLetter) {
-        errMsg = 'At least one letter';
-      } else if (!digitsPassword) {
-        /*else if (!uppercasePassword) {
-        errMsg = 'At least one Uppercase';
-      } else if (!lowercasePassword) {
-        errMsg = 'At least one Lowercase';
-      }*/
-        errMsg = 'At least one digit';
-      } else if (!minLengthPassword) {
-        /*else if (!specialCharPassword) {
-        errMsg = 'At least one Special Characters';
-      }*/
-        errMsg = 'At least minumum 8 characters';
-      } else {
-        errMsg = '';
-      }
-      setPasswordErr(errMsg);
-    }
-    // for confirm password
-    if (
-      passwordInputFieldName === 'confirmPassword' ||
-      (passwordInputFieldName === 'password' && passwordInput.confirmPassword.length > 0)
-    ) {
-      if (passwordInput.confirmPassword !== passwordInput.password) {
-        setConfirmPasswordError('Confirm password is not matched');
-      } else {
-        setConfirmPasswordError('');
-      }
-    }
-  };
   // User Name
   const namePrefix = props.user.firstName;
 
@@ -372,14 +308,6 @@ const NewCodeSpace = (props: ICodeSpaceProps) => {
       setRecipeError(requiredError);
       formValid = false;
     }
-    if (passwordInput.password === '') {
-      setPasswordErr(requiredError);
-      formValid = false;
-    }
-    if (passwordInput.confirmPassword === '') {
-      setConfirmPasswordError(requiredError);
-      formValid = false;
-    }
     // if (isPublicRecipeChoosen && githubUserName === '') {
     //   setGithubUserNameError(requiredError);
     //   formValid = false;
@@ -390,7 +318,7 @@ const NewCodeSpace = (props: ICodeSpaceProps) => {
       setGithubTokenError(requiredError);
       formValid = false;
     }
-    if (projectNameError !== '' || recipeError !== '' || passwordError !== '' || confirmPasswordError !== '' || githubTokenError !== '') {
+    if (projectNameError !== '' || recipeError !== '' || githubTokenError !== '') {
       formValid = false;
     }
     return formValid;
@@ -398,14 +326,6 @@ const NewCodeSpace = (props: ICodeSpaceProps) => {
 
   const validateOnBoardCodeSpaceForm = (isPublicRecipeChoosen: boolean) => {
     let formValid = true;
-    if (passwordInput.password === '') {
-      setPasswordErr(requiredError);
-      formValid = false;
-    }
-    if (passwordInput.confirmPassword === '') {
-      setConfirmPasswordError(requiredError);
-      formValid = false;
-    }
     // if (isPublicRecipeChoosen && githubUserName === '') {
     //   setGithubUserNameError(requiredError);
     //   formValid = false;
@@ -416,7 +336,7 @@ const NewCodeSpace = (props: ICodeSpaceProps) => {
       setGithubTokenError(requiredError);
       formValid = false;
     }
-    if (passwordError !== '' || confirmPasswordError !== '' || githubTokenError !== '') {
+    if (githubTokenError !== '') {
       formValid = false;
     }
     return formValid;
@@ -500,7 +420,6 @@ const NewCodeSpace = (props: ICodeSpaceProps) => {
             }
           }
         },
-        password: passwordInput.password,
         pat: githubToken
       };
 
@@ -573,7 +492,6 @@ const NewCodeSpace = (props: ICodeSpaceProps) => {
     if (validateOnBoardCodeSpaceForm(isPublicRecipeChoosen)) {
 
       const onBoardCodeSpaceRequest = {
-        password: passwordInput.password,
         pat: githubToken
       };
 
@@ -649,7 +567,7 @@ const NewCodeSpace = (props: ICodeSpaceProps) => {
               <div>{projectDetails.recipeDetails.cloudServiceProvider}</div>
             </div>
           </div>
-          <div className={Styles.flexLayout}>
+          {/* <div className={Styles.flexLayout}>
             <div>
               <TextBox
                 type="password"
@@ -682,7 +600,7 @@ const NewCodeSpace = (props: ICodeSpaceProps) => {
                 onKeyUp={handleValidation}
               />
             </div>
-          </div>
+          </div> */}
           {/* {isPublicRecipeChoosen && (
             <div>
               <div>
@@ -815,7 +733,7 @@ const NewCodeSpace = (props: ICodeSpaceProps) => {
                   </div>
                 </div>
               </div>
-              <div className={Styles.flexLayout}>
+              {/* <div className={Styles.flexLayout}>
                 <div>
                   <TextBox
                     type="password"
@@ -848,7 +766,7 @@ const NewCodeSpace = (props: ICodeSpaceProps) => {
                     onKeyUp={handleValidation}
                   />
                 </div>
-              </div>
+              </div> */}
               {/* {isPublicRecipeChoosen && (
                 <div>
                   <div>
