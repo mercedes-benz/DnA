26f2159b566379db50ce60a3df470d5986cbf356
Ramnath <nramnat@apac.corpdir.net>
2023-09-01 11:31:59 +0530

    added matomo api contract

---
+++ packages/matomo-backend/matomo-lib/src/main/java/com/daimler/data/service/common/BaseCommonService.java
@@ -0,0 +1,4 @@
+package com.daimler.data.service.common;
+
+public class BaseCommonService<V, T, ID> implements CommonService<V, T, ID>{
+}
---
+++ packages/matomo-backend/matomo-lib/src/main/java/com/daimler/data/service/common/CommonService.java
@@ -0,0 +1,4 @@
+package com.daimler.data.service.common;
+
+public interface CommonService<V, T, ID> {
+}
