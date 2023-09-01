a5a1bfe80f40098b6f9e5836f8c2abd79ca11828
Ramnath <nramnat@apac.corpdir.net>
2023-09-01 11:31:59 +0530

    added matomo api contract

---
+++ packages/matomo-backend/matomo-lib/src/main/java/com/daimler/data/service/matomo/BaseMatomoService.java
@@ -0,0 +1,9 @@
+package com.daimler.data.service.matomo;
+
+import com.daimler.data.db.entities.MatomoNsql;
+import com.daimler.data.dto.matomo.MatomoVO;
+import com.daimler.data.service.common.BaseCommonService;
+
+public class BaseMatomoService extends BaseCommonService<MatomoVO, MatomoNsql, String> implements MatomoService{
+
+}
