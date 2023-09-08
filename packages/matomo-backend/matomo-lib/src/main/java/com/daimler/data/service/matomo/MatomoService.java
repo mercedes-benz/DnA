c5a7ca6f50fe39ad3f544f060687455eed0ce69a
Ramnath <nramnat@apac.corpdir.net>
2023-09-08 20:01:41 +0530

    added api implementation for creating matomo website

--- packages/matomo-backend/matomo-lib/src/main/java/com/daimler/data/service/matomo/MatomoService.java
+++ packages/matomo-backend/matomo-lib/src/main/java/com/daimler/data/service/matomo/MatomoService.java
@@ -1,8 +1,13 @@
 package com.daimler.data.service.matomo;
 
 import com.daimler.data.db.entities.MatomoNsql;
-import com.daimler.data.dto.matomo.MatomoVO;
+import com.daimler.data.dto.matomo.*;
 import com.daimler.data.service.common.CommonService;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.math.BigDecimal;
+import java.util.Date;
 
 public interface MatomoService extends CommonService<MatomoVO, MatomoNsql, String> {
+    MatomoResponseVO createMatomoSite(String matomoId, String siteId, Date createdOn, Date lastModified, MatomoSiteRequestVO matomoRequestVO, CreatedByVO requestUser);
 }
