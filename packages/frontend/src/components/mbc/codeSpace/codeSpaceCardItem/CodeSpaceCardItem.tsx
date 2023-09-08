7afd2a31bc34b0d4d7c6121238e578d12825859c
Maria Rathinam <amariar@capelit77946372.inedc.corpintra.net>
2023-09-08 17:19:10 +0530

    Adding dummy lock icon ui for demo

--- packages/frontend/src/components/mbc/codeSpace/codeSpaceCardItem/CodeSpaceCardItem.tsx
+++ packages/frontend/src/components/mbc/codeSpace/codeSpaceCardItem/CodeSpaceCardItem.tsx
@@ -223,7 +223,21 @@ const CodeSpaceCardItem = (props: CodeSpaceCardItemProps) => {
                         )}
                         {prodDeployed && (
                           <a href={prodDeployedUrl} target="_blank" rel="noreferrer" className={Styles.deployedLink}>
-                            <i className="icon mbc-icon link" /> Production
+                            <i className="icon mbc-icon link" /> Production{' '}
+                            {projectDetails.projectName === 'ITLSDemoService' && (
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                stroke="#00adef"
+                                fill="#00adef"
+                                strokeWidth="0"
+                                viewBox="0 0 30 30"
+                                width="15px"
+                                height="15px"
+                              >
+                                {' '}
+                                <path d="M 15 2 C 11.145666 2 8 5.1456661 8 9 L 8 11 L 6 11 C 4.895 11 4 11.895 4 13 L 4 25 C 4 26.105 4.895 27 6 27 L 24 27 C 25.105 27 26 26.105 26 25 L 26 13 C 26 11.895 25.105 11 24 11 L 22 11 L 22 9 C 22 5.2715823 19.036581 2.2685653 15.355469 2.0722656 A 1.0001 1.0001 0 0 0 15 2 z M 15 4 C 17.773666 4 20 6.2263339 20 9 L 20 11 L 10 11 L 10 9 C 10 6.2263339 12.226334 4 15 4 z" />
+                              </svg>
+                            )}
                           </a>
                         )}
                       </>
@@ -261,7 +275,7 @@ const CodeSpaceCardItem = (props: CodeSpaceCardItemProps) => {
       <ConfirmModal
         title={''}
         acceptButtonTitle="Yes"
-        cancelButtonTitle={allowDelete ? "No" : "OK"}
+        cancelButtonTitle={allowDelete ? 'No' : 'OK'}
         showAcceptButton={allowDelete}
         showCancelButton={true}
         show={showDeleteModal}
