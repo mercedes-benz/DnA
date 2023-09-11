3929b6ae8569605891540c89166bec45210c65e2
Ramnath <nramnat@apac.corpdir.net>
2023-09-11 17:56:16 +0530

    api implemenation for creating and listing website

--- packages/matomo-backend/matomo-lib/src/main/java/com/daimler/data/dto/MatomoGetSiteResponseDto.java
+++ packages/matomo-backend/matomo-lib/src/main/java/com/daimler/data/dto/MatomoGetSiteResponseDto.java
@@ -1,9 +1,12 @@
 package com.daimler.data.dto;
 
+import com.daimler.data.controller.exceptions.MessageDescription;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Data
 @NoArgsConstructor
@@ -15,4 +18,7 @@ public class MatomoGetSiteResponseDto {
     private String idsite;
     private String name;
     private String main_url;
+    private List<MessageDescription> errors;
+    private String status;
+    private List<MessageDescription> warnings;
 }
--- packages/matomo-backend/matomo-lib/src/main/java/com/daimler/data/service/matomo/BaseMatomoService.java
+++ packages/matomo-backend/matomo-lib/src/main/java/com/daimler/data/service/matomo/BaseMatomoService.java
@@ -1,21 +1,33 @@
 package com.daimler.data.service.matomo;
 
+import com.daimler.data.application.client.MatomoClient;
+import com.daimler.data.assembler.MatomoAssembler;
 import com.daimler.data.controller.exceptions.GenericMessage;
 import com.daimler.data.controller.exceptions.MessageDescription;
 import com.daimler.data.db.entities.MatomoNsql;
+import com.daimler.data.db.repo.matomo.MatomoCustomRepository;
+import com.daimler.data.dto.MatomoGetSiteResponseDto;
 import com.daimler.data.dto.matomo.*;
 import com.daimler.data.service.common.BaseCommonService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 @Slf4j
 public class BaseMatomoService extends BaseCommonService<MatomoVO, MatomoNsql, String> implements MatomoService{
 
+    @Autowired
+    private MatomoCustomRepository customRepo;
+    @Autowired
+    private MatomoAssembler assembler;
+    @Autowired
+    private MatomoClient matomoClient;
     @Override
     public MatomoResponseVO createMatomoSite(String matomoId, String siteId, Date createdOn, Date lastModified, MatomoSiteRequestVO matomoRequestVO, CreatedByVO requestUser) {
         GenericMessage responseMessage = new GenericMessage();
@@ -52,5 +64,54 @@ public class BaseMatomoService extends BaseCommonService<MatomoVO, MatomoNsql, S
         responseWrapperVO.setData(matomoVO);
         responseWrapperVO.setResponse(responseMessage);
         return responseWrapperVO;
+    }
+
+
+    @Override
+    public List<MatomoVO> getAll( int limit,  int offset, String user) {
+        MatomoGetSiteResponseDto getSiteResponse =new MatomoGetSiteResponseDto();
+        Map<String, Object> map = new HashMap<>();
+        List<MatomoVO> matomoVO = new ArrayList<>();
+        List<MatomoVO> matomoVOResponse = new ArrayList<>();
+        List<MatomoNsql> entities = customRepo.getAll(user, offset, limit);
+        if (entities != null && !entities.isEmpty()) {
+            matomoVO = entities.stream().map(n -> assembler.toVo(n)).collect(Collectors.toList());
+            for(MatomoVO matomoRecord: matomoVO) {
+                List<CollaboratorVO> collaborators = new ArrayList<CollaboratorVO>();
+                getSiteResponse=matomoClient.listParticularMatomoSite(matomoRecord.getSiteId());
+                if(getSiteResponse!=null && "SUCCESS".equalsIgnoreCase(getSiteResponse.getStatus())){
+                    matomoRecord.setSiteName(getSiteResponse.getName());
+                    matomoRecord.setSiteUrl(getSiteResponse.getMain_url());
+                }
+
+                map =matomoClient.getUsersAccessFromSite(user, matomoRecord.getSiteId());
+                for (Map.Entry<String, Object> entry : map.entrySet()) {
+
+                    String key = entry.getKey();
+                    if(key.equalsIgnoreCase(user)){
+                        matomoRecord.setPermission(entry.getValue().toString());
+                    }
+                    else{
+                        CollaboratorVO collaborator = new CollaboratorVO();
+                        collaborator.setId(entry.getKey());
+                        collaborator.setPermission(entry.getValue().toString());
+                        collaborators.add(collaborator);
+                    }
+                }
+                matomoRecord.setCollaborators(collaborators);
+                matomoVOResponse.add(matomoRecord);
+            }
+
+            return  matomoVOResponse;
+        }
+        else {
+            return new ArrayList<>();
+        }
+    }
+
+    @Override
+    @Transactional(isolation = Isolation.SERIALIZABLE)
+    public Long getCount(String user) {
+        return customRepo.getTotalCount(user);
     }
 }
