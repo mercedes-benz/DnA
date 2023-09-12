2ae92918556ffb23ded60d59d642a6a684b12891
Aluri <bealuri@apac.corpdir.net>
2023-09-12 17:14:17 +0530

    Added kong changes for securing workspace apis

--- packages/code-server/code-server-lib/src/main/java/com/daimler/data/db/json/CodeServerDeploymentDetails.java
+++ packages/code-server/code-server-lib/src/main/java/com/daimler/data/db/json/CodeServerDeploymentDetails.java
@@ -20,7 +20,7 @@ public class CodeServerDeploymentDetails implements Serializable {
 	private String lastDeployedBranch;
 	private String deploymentUrl;
 	private String lastDeploymentStatus;
-	private boolean isSecureWithIAMRequired;
+	private boolean secureWithIAMRequired;
 	private String technicalUserDetailsForIAMLogin;
 	
 }
