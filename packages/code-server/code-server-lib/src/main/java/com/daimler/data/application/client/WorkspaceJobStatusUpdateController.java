88dc9ab246115157b24897bb7cd610f699ac4e40
pavithrajaini <114487538+pavithrajaini@users.noreply.github.com>
2023-09-13 16:55:13 +0530

    Commenting calling the KONG API's for creating service, route and plugins whenever workspace is created

--- packages/code-server/code-server-lib/src/main/java/com/daimler/data/application/client/WorkspaceJobStatusUpdateController.java
+++ packages/code-server/code-server-lib/src/main/java/com/daimler/data/application/client/WorkspaceJobStatusUpdateController.java
@@ -201,7 +201,7 @@ public class WorkspaceJobStatusUpdateController  {
 			}
 			GenericMessage responseMessage = service.update(userId,name,projectName,existingStatus,latestStatus,targetEnv,branch);
 			log.info("Message details after update action {} and userid is {} and resourceID is {}",message,userId,resourceID);
-			authenticatorClient.callingKongApis(name);
+			//authenticatorClient.callingKongApis(name);
 			kafkaProducer.send(eventType, resourceID, "", userId, message, true, teamMembers, teamMembersEmails, null);
 			return new ResponseEntity<>(responseMessage, HttpStatus.OK);
 		}else {
