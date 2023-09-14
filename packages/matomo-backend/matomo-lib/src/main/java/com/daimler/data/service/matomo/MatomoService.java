77f9b38474b88e59d24c1f2561df31eff1cd2fd4
Ramnath <nramnat@apac.corpdir.net>
2023-09-14 10:48:35 +0530

    added getById and deleteById api implementation

--- packages/matomo-backend/matomo-lib/src/main/java/com/daimler/data/service/matomo/MatomoService.java
+++ packages/matomo-backend/matomo-lib/src/main/java/com/daimler/data/service/matomo/MatomoService.java
@@ -1,5 +1,6 @@
 package com.daimler.data.service.matomo;
 
+import com.daimler.data.controller.exceptions.GenericMessage;
 import com.daimler.data.db.entities.MatomoNsql;
 import com.daimler.data.dto.matomo.*;
 import com.daimler.data.service.common.CommonService;
@@ -12,5 +13,8 @@ import java.util.List;
 public interface MatomoService extends CommonService<MatomoVO, MatomoNsql, String> {
     MatomoResponseVO createMatomoSite(String matomoId, String siteId, Date createdOn, Date lastModified, MatomoSiteRequestVO matomoRequestVO, CreatedByVO requestUser);
     Object[] getAll(int limit, int offset, String user);
+
+    MatomoVO getMatomoById(String matomoId,String user);
+    GenericMessage deleteMatomoByID(String id, String user);
     Long getCount(String user);
 }
