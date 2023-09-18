fc541f2e33d9eb0944d31edf6f45f3e2492f5411
Shubham <sbm_verma31@rediffmail.com>
2023-09-18 11:44:01 +0530

    Migrated to react 18.2.0

--- packages/chronos-mfe/src/bootstrap.js
+++ packages/chronos-mfe/src/bootstrap.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
 // import css
 import './common/modules/uilab/bundle/css/uilab.min.css';
@@ -7,4 +8,8 @@ import './common/globals/css/main.scss';
 
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+const container = document.getElementById('root');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<App />);
