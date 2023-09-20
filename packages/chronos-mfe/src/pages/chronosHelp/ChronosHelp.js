91d08aff88fbe9402503a9777eb552b56a48fb3f
Khartade <khartaa@apac.corpdir.net>
2023-09-20 11:10:33 +0530

    chronos tutorial and help

--- packages/chronos-mfe/src/pages/chronosHelp/ChronosHelp.js
+++ packages/chronos-mfe/src/pages/chronosHelp/ChronosHelp.js
@@ -1,11 +1,16 @@
 import classNames from 'classnames';
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import Styles from './chronos-help.scss';
 import Breadcrumb from '../../components/breadcrumb/Breadcrumb';
-import help from '../../assets/help.png';
+// import help from '../../assets/help.png';
+import { Envs } from '../../utilities/envs';
 
 const ChronosHelp = () => {
+  const history = useHistory();
+  const goback = () => {
+    history.goBack();
+  };
   return (
     <>
       <div className={classNames(Styles.mainPanel)}>
@@ -16,9 +21,13 @@ const ChronosHelp = () => {
         <h3 className={classNames(Styles.title)}>Chronos Help</h3>
         <div className={classNames(Styles.wrapper)}>
             <div className={Styles.container}>
-              <h2>Learn more about how to set up your forecast<br />the right way to achieve better results.</h2>
-              <img src={help} />
-              <button className='btn btn-secondary'>Learn more</button>
+              {/* <img src={help} height={'150px'} /> */}
+              <p>This page is under construction. In case of questions, don&apos;t hesitate to reach out to the team behind Chronos under <a href={`mailto:${Envs.ADS_EMAIL}`}>{Envs.ADS_EMAIL}</a>. Meanwhile, here are some resources that might help you:</p>
+              <ul>
+                <li><a href={`/chronos-templates/Chronos_GUI_tutorial.pptx`} download={true}>Chronos Application tutorial</a></li>
+                <li><a href={Envs.CHRONOS_DOCUMENTATION_URL} target='_blank' rel='noopener noreferrer'>Chronos technical documentation</a></li>
+              </ul>
+              <button className='btn btn-secondary' onClick={goback}>Go Back</button>
             </div>
         </div>
       </div>
