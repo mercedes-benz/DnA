fad65f9e0835a65ca8d968b75e20e291152a0959
Maria Rathinam <amariar@capelit77946372.inedc.corpintra.net>
2023-09-25 14:37:50 +0530

    Solving null issue on new solution flow

--- packages/frontend/src/components/mbc/createNewSolution/digitalvalue/DigitalValue.tsx
+++ packages/frontend/src/components/mbc/createNewSolution/digitalvalue/DigitalValue.tsx
@@ -1109,7 +1109,7 @@ export default class DigitalValue extends React.Component<IDigitalValueProps, ID
   protected onValueCalculationChangeAccept = () => {
     if (this.state.valueCalculationType === DigitalValue.dataValueTypeKeyValue) {
       const {valueDrivers, costDrivers } = this.state;
-      valueDrivers.forEach((valueDriver: IValueFactor) => {
+      valueDrivers?.forEach((valueDriver: IValueFactor) => {
         let value = 0;
         (valueDriver.category !== DigitalValue.dataValueSavingsKeyValue &&
           valueDriver.category !== DigitalValue.dataValueRevenueKeyValue) &&
@@ -1121,7 +1121,7 @@ export default class DigitalValue extends React.Component<IDigitalValueProps, ID
         valueDriver.value = value.toString();
       });
 
-      costDrivers.forEach((costDriver: ICostFactor) => {
+      costDrivers?.forEach((costDriver: ICostFactor) => {
         let value = 0;
         costDriver.rampUp.forEach((rampUp: IValueRampUp) => {
           value += parseFloat(rampUp.value);
