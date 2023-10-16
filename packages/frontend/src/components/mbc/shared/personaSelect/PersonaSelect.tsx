d3af3e51549833b18f6c2d990c692b537a096f28
sushma <98083702+6-Sushma@users.noreply.github.com>
2023-10-16 17:44:18 +0530

    Bugfix-NA-Persona (#2171)

--- packages/frontend/src/components/mbc/shared/personaSelect/PersonaSelect.tsx
+++ packages/frontend/src/components/mbc/shared/personaSelect/PersonaSelect.tsx
@@ -29,7 +29,7 @@ const PersonaSelect = (props: IPersonaSelectProps) => {
   return (
     <div className={Styles.container}>
       <h2 className={classNames(Styles.heading, props.isSummary? Styles.summaryHeading : '')}>Personas</h2>
-      {selectedPersona.length === 0 || props.personas.length === 0 ? <div className={Styles.noPersonaSelected}>NA</div> :
+      {props.personas.length === 0 ? <div className={Styles.noPersonaSelected}>NA</div> :
       <div className={Styles.personaContainer}>
         {
           props.personas.map((persona:any) => {
