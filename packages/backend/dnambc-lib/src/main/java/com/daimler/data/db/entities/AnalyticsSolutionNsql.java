c8cc013e625d7b48e6d3c60c101ec91198c1b736
Namitha-01dev <125283526+Namitha-01dev@users.noreply.github.com>
2023-10-20 14:12:29 +0530

    Feature/add analytics solution gen ai (#2204)
    * added solution field for genai analytics
    
    * added backend implementation for solution analytics
    
    * added solution field for analytics
    
    ---------
    
    Co-authored-by: Ramnath <nramnat@apac.corpdir.net>

---
+++ packages/backend/dnambc-lib/src/main/java/com/daimler/data/db/entities/AnalyticsSolutionNsql.java
@@ -0,0 +1,19 @@
+package com.daimler.data.db.entities;
+
+
+import com.daimler.data.db.jsonb.AnalyticsSolution;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "analyticsSolution_nsql")
+public class AnalyticsSolutionNsql extends BaseEntity<AnalyticsSolution> implements Serializable {
+
+    private static final long serialVersionUID = -7715637669188213350L;
+
+    public AnalyticsSolutionNsql() {
+    }
+
+}
