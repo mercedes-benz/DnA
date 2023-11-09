8d8792e81c17fd55321176324acc314f3621ae62
ARAARUM <ARAARUM>
2023-11-09 05:03:03 +0000

    added airflow changes

--- packages/airflow-backend/airflow-backend-lib/src/main/java/com/daimler/dna/airflow/app/main/filter/JWTAuthenticationFilter.java
+++ packages/airflow-backend/airflow-backend-lib/src/main/java/com/daimler/dna/airflow/app/main/filter/JWTAuthenticationFilter.java
@@ -38,6 +38,9 @@ import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.WebApplicationContextUtils;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import com.daimler.dna.airflow.app.main.auth.UserStore;
 import com.daimler.dna.airflow.controller.LoginController;
@@ -56,8 +59,8 @@ import java.util.List;
 @Component
 public class JWTAuthenticationFilter implements Filter {
 
-//    @Value("${oidc.disabled}")
-//    private boolean oidcDisabled;
+	// @Value("${oidc.disabled}")
+	// private boolean oidcDisabled;
 
 	@Value("#{'${jwt.secret.byPassUrl}'.split(';')}")
 	private List<String> byPassUrl;
@@ -80,49 +83,18 @@ public class JWTAuthenticationFilter implements Filter {
 		injectSpringDependecies(servletRequest);
 		HttpServletRequest httpRequest = (HttpServletRequest) servletRequest;
 		String requestUri = httpRequest.getRequestURI();
-		log.debug("Intercepting Request to validate JWT:" + requestUri);
-		String jwt = httpRequest.getHeader("Authorization");
-		if (!StringUtils.hasText(jwt)) {
-			log.error("Request UnAuthorized,No JWT available");
+		log.debug("Intercepting Request to validate userinfo:" + requestUri);
+		String userinfo = httpRequest.getHeader("dna-request-userdetails");
+		if (!StringUtils.hasText(userinfo)) {
+			log.error("Request UnAuthorized,No userinfo available");
 			forbidResponse(servletResponse);
 			return;
-		} else {
-			log.debug("decoding jwt token...");
-			Claims claims = JWTGenerator.decodeJWT(jwt);
-			String userId = "";
-			if (claims == null) {
-				log.error("Invalid  JWT!");
-				HttpServletResponse response = (HttpServletResponse) servletResponse;
-				response.reset();
-				response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-				return;
-			} else {
-				userId = (String) claims.get("id");
-				if (dnaAuthEnable) {
-					JSONObject res = dnaAuthClient.verifyLogin(jwt);
-					if (res != null) {
-						try {
-							setUserDetailsToStore(res);
-							filterChain.doFilter(servletRequest, servletResponse);
-						} finally {
-							// Otherwise when a previously used container thread is used, it will have the
-							// old user id set and
-							// if for some reason this filter is skipped, userStore will hold an unreliable
-							// value
-							this.userStore.clear();
-						}
-
-					} else {
-						log.error("Request UnAuthorized,No JWT available");
-						forbidResponse(servletResponse);
-						return;
-					}
-
-				} else {
+		} else if (StringUtils.hasText(userinfo)) {
+			if (dnaAuthEnable) {
+				JSONObject res = dnaAuthClient.verifyLogin(userinfo);
+				if (res != null) {
 					try {
-						log.debug(
-								"Request validation successful, set request user details in the store for further access");
-						setUserDetailsToStore(claims);
+						setUserDetailsToStore(res);
 						filterChain.doFilter(servletRequest, servletResponse);
 					} finally {
 						// Otherwise when a previously used container thread is used, it will have the
@@ -131,28 +103,68 @@ public class JWTAuthenticationFilter implements Filter {
 						// value
 						this.userStore.clear();
 					}
+
+				} else {
+					log.error("Request UnAuthorized,No userinfo available");
+					forbidResponse(servletResponse);
+					return;
 				}
 
+			} else {
+				try {
+					log.debug(
+							"Request validation successful, set request user details in the store for further access");
+					setUserDetailsToStore(userinfo);
+					filterChain.doFilter(servletRequest, servletResponse);
+				} catch (Exception e) {
+					log.error("Error while storing userDetails {} ", e.getMessage());
+					forbidResponse(servletResponse);
+					this.userStore.clear();
+					return;
+				} finally {
+					// Otherwise when a previously used container thread is used, it will have the
+					// old user id set and
+					// if for some reason this filter is skipped, userStore will hold an unreliable
+					// value
+					this.userStore.clear();
+				}
 			}
+
+		} else {
+			log.debug("Request is exempted from validation");
+			filterChain.doFilter(servletRequest, servletResponse);
 		}
 
 	}
 
-	private void setUserDetailsToStore(Claims claims) {
+	// private void setUserDetailsToStore(Claims claims) {
 
-		LoginController.UserInfo user = LoginController.UserInfo.builder().id((String) claims.get("id"))
-				.firstName((String) claims.get("firstName")).lastName((String) claims.get("lastName"))
-				.email((String) claims.get("email")).department((String) claims.get("department"))
-				.mobileNumber((String) claims.get("mobileNumber")).build();
-		/*
-		 * List<LinkedHashMap> claimedRoles = (ArrayList) claims.get("digiRole");
-		 * List<LoginController.UserRole> roles = new ArrayList<>();
-		 * claimedRoles.forEach(roleMapEntity -> { roles.add(new
-		 * LoginController.UserRole().builder().id((String) roleMapEntity.get("id"))
-		 * .name((String) roleMapEntity.get("name")).build()); });
-		 * user.setDigiRole(roles);
-		 */
-		this.userStore.setUserInfo(user);
+	// LoginController.UserInfo user =
+	// LoginController.UserInfo.builder().id((String) claims.get("id"))
+	// .firstName((String) claims.get("firstName")).lastName((String)
+	// claims.get("lastName"))
+	// .email((String) claims.get("email")).department((String)
+	// claims.get("department"))
+	// .mobileNumber((String) claims.get("mobileNumber")).build();
+	// /*
+	// * List<LinkedHashMap> claimedRoles = (ArrayList) claims.get("digiRole");
+	// * List<LoginController.UserRole> roles = new ArrayList<>();
+	// * claimedRoles.forEach(roleMapEntity -> { roles.add(new
+	// * LoginController.UserRole().builder().id((String) roleMapEntity.get("id"))
+	// * .name((String) roleMapEntity.get("name")).build()); });
+	// * user.setDigiRole(roles);
+	// */
+	// this.userStore.setUserInfo(user);
+
+	// }
+
+	private void setUserDetailsToStore(String userinfo) throws JsonProcessingException {
+
+		ObjectMapper objectMapper = new ObjectMapper();
+		LoginController.UserInfo userInfo = objectMapper.readValue(userinfo,
+				new TypeReference<LoginController.UserInfo>() {
+				});
+		this.userStore.setUserInfo(userInfo);
 
 	}
 
