ba386ba47a6531bcd543ffd7b810aeff44559f54
Aluri <bealuri@apac.corpdir.net>
2023-11-17 12:07:24 +0530

    init checkin for codespace access management

---
+++ packages/code-server/code-server-lib/src/main/java/com/daimler/data/db/json/CodespaceSecurityUserRoleMap.java
@@ -0,0 +1,22 @@
+package com.daimler.data.db.json;
+
+import java.io.Serializable;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class CodespaceSecurityUserRoleMap implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private String userId;
+	private List<String> roles;
+}
