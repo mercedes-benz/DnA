f5e1b06322c50c7043958d0098f3c798ee60c56e
KHARTAA <KHARTAA>
2023-11-21 12:45:15 +0000

    create project api integration

--- packages/datalake-mfe/src/redux/projects.services.js
+++ packages/datalake-mfe/src/redux/projects.services.js
@@ -1,10 +1,11 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
+import { datalakeApi } from '../apis/datalake.api';
 
-export const getProjects = createAsyncThunk('projects/getProjects', () => {
-  const res = localStorage.getItem('graphs') ? JSON.parse(localStorage.getItem('graphs')) : [];
-  console.log('res');
-  console.log(res);
-  return {
-    data: res,
-  };
+export const getProjects = createAsyncThunk('projects/getProjects', async () => {
+  try {
+    const res = await datalakeApi.getDatalakeProjectsList();
+    return res;
+  } catch (e) {
+    return e.message;
+  }
 });
