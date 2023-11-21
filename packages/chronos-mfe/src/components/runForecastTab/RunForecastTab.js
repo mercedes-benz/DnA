eda5d7e2600d30adb8bc8cd4bdb9d19228ecd8dd
Aniruddha Khartade <aniruddhakhartade@gmail.com>
2023-11-21 12:22:11 +0530

    small fix chronos config (#2307)
    * update chronos forecasting template
    
    * small fix chronos config

--- packages/chronos-mfe/src/components/runForecastTab/RunForecastTab.js
+++ packages/chronos-mfe/src/components/runForecastTab/RunForecastTab.js
@@ -48,8 +48,9 @@ const RunForecastTab = ({ onRunClick }) => {
     formData.append('hierarchy', data.hierarchy === undefined ? '' : data.hierarchy);
     if(data.configurationFile.includes('OPTIMISATION_CONFIG')) {
       formData.append('runOnPowerfulMachines', true);
+    } else {
+      formData.append('runOnPowerfulMachines', data.runOnPowerfulMachines === undefined ? false : data.runOnPowerfulMachines);
     }
-    formData.append('runOnPowerfulMachines', data.runOnPowerfulMachines === undefined ? false : data.runOnPowerfulMachines);
     formData.append('chronosVersion', data.chronosVersion === undefined ? '' : data.chronosVersion);
     formData.append('comment', data.comment);
     formData.append('saveRequestPart', data.saveRequestPart === undefined ? 'false' : data.saveRequestPart + '');
