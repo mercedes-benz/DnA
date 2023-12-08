e7610d246890c52af73fc2c44f388b39719432ca
sushma <98083702+6-Sushma@users.noreply.github.com>
2023-12-08 15:53:22 +0530

    Chronos lean governance committed.... (#2374)
    * Chronos lean governance committed....
    
    * commited some changes in chronos done
    
    * committed review changes
    
    * assembler changes committed
    
    ---------
    
    Co-authored-by: GAISUSH <GAISUSH>

---
+++ packages/chronos/chronos-lib/src/main/java/com/daimler/data/db/json/LeanGovernanceFeilds.java
@@ -0,0 +1,29 @@
+package com.daimler.data.db.json;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class LeanGovernanceFeilds {
+
+    private String typeOfProject;
+    private String decription;
+    private String division;
+    private String subDivision;
+    private String department;
+    private List<String> tags;
+    private String dataClassification;
+    private Boolean piiData;
+    private String archerId;
+    private String procedureId;
+    private Boolean termsOfUse;
+
+}
