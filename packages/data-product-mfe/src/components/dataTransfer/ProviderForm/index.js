f8eef7d42880d7fb181d64adf85620e68ef981a7
SHPEEKA <SHPEEKA>
2023-12-12 05:59:08 +0000

    fixed finalize and forward button

--- packages/data-product-mfe/src/components/dataTransfer/ProviderForm/index.js
+++ packages/data-product-mfe/src/components/dataTransfer/ProviderForm/index.js
@@ -515,16 +515,12 @@ const ProviderForm = ({ user, history }) => {
 
   const onSave = (currentAction, currentTab, values, callbackFn) => {
     setShowAllTabsError(false);
-    if(currentAction === 'publish'){
-      if(currentTab!='contact-info'){
-        setShowContactInformationTabError(true);
-      } else{  
+    if(currentAction === 'publish'){ 
         if(validatePublishRequest(values)){
           proceedToSave(currentTab, values, callbackFn)
         } else {
           setShowAllTabsError(true);
-        }
-      }  
+        } 
     } else {
       if(!values.id && values.id!='' && currentTab!='contact-info'){
         setShowContactInformationTabError(true);
