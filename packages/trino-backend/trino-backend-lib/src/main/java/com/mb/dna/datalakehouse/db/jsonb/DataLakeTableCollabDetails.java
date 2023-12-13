3f22743ac473f44cff465544f57268a778ee3f6b
Aluri <bealuri@apac.corpdir.net>
2023-12-13 13:19:50 +0530

    Added json ignores

--- packages/trino-backend/trino-backend-lib/src/main/java/com/mb/dna/datalakehouse/db/jsonb/DataLakeTableCollabDetails.java
+++ packages/trino-backend/trino-backend-lib/src/main/java/com/mb/dna/datalakehouse/db/jsonb/DataLakeTableCollabDetails.java
@@ -2,6 +2,8 @@ package com.mb.dna.datalakehouse.db.jsonb;
 
 import java.io.Serializable;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,6 +12,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class DataLakeTableCollabDetails implements Serializable {
 
 	private static final long serialVersionUID = 1L;
