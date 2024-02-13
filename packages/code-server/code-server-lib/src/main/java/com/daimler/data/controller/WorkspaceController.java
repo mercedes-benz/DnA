75fc67ac20c00873735dc86b4385b4fb4ffa2a34
aravindh45 <63506505+aravindh45@users.noreply.github.com>
2024-02-13 20:37:11 +0530

    Update WorkspaceController.java

--- packages/code-server/code-server-lib/src/main/java/com/daimler/data/controller/WorkspaceController.java
+++ packages/code-server/code-server-lib/src/main/java/com/daimler/data/controller/WorkspaceController.java
@@ -30,10 +30,8 @@
  import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 import javax.persistence.EntityNotFoundException;
