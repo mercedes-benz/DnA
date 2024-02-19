8bab46537520e6ffa2ee0f47fd8e0a5a2fa7237b
SHPEEKA <SHPEEKA>
2024-02-19 13:07:38 +0000

    fix for view dataProduct in dataLake

--- packages/datalake-mfe/src/pages/graph/Graph.js
+++ packages/datalake-mfe/src/pages/graph/Graph.js
@@ -58,13 +58,7 @@ const Graph = ({user, hostHistory}) => {
     const [loading, setLoading] = useState(true);
     const [connectionInfo, setConnectionInfo] = useState();
     const [hasDataProduct,setHasDataProduct] =useState(false);
-     
     const [hasTable, setHasTable] = useState(project.tables.length > 0 );
-
-    useEffect (()=>{
-      setHasTable(project.tables.length > 0 );
-      setHasDataProduct(sessionStorage.getItem(SESSION_STORAGE_KEYS.DATAPRODUCT_ID)?.split(':')[0] == project.id);
-    },[project])
    
 
     useEffect(() => {
@@ -84,6 +78,11 @@ const Graph = ({user, hostHistory}) => {
             setLoading(false);
         });
     }, [id]);
+    
+    useEffect (()=>{
+      setHasTable(project.tables.length > 0 );
+      setHasDataProduct(sessionStorage.getItem(SESSION_STORAGE_KEYS.DATAPRODUCT_ID)?.split(':')[0] == project.id);
+    },[project])
 
     /* A callback function that is used to update the viewbox of the svg. */
     const resizeHandler = useCallback(() => {
