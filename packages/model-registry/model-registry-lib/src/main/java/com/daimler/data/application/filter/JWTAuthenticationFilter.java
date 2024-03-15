bd8189937ad29a8df0b46e735dedee40fd46bc3b
sushma <98083702+6-Sushma@users.noreply.github.com>
2024-03-15 14:50:38 +0530

    changes for trino.. (#2660)
    * changes for trino..
    
    * added model reg changes
    
    * removal of sysout
    
    ---------
    
    Co-authored-by: GAISUSH <GAISUSH>
    Co-authored-by: ARAARUM <ARAARUM>

--- packages/model-registry/model-registry-lib/src/main/java/com/daimler/data/application/filter/JWTAuthenticationFilter.java
+++ packages/model-registry/model-registry-lib/src/main/java/com/daimler/data/application/filter/JWTAuthenticationFilter.java
@@ -57,6 +57,7 @@ import com.daimler.data.application.auth.UserStore;
 import com.daimler.data.application.auth.UserStore.UserRole;
 import com.daimler.data.auth.client.DnaAuthClient;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.core.type.TypeReference;
 
@@ -140,7 +141,22 @@ public class JWTAuthenticationFilter implements Filter {
 		UserStore.UserInfo userInfo = objectMapper.readValue(userinfo, new TypeReference<UserStore.UserInfo>() {
 		});
 		this.userStore.setUserInfo(userInfo);
-
+		List<UserRole> userRoles = new ArrayList<>();
+		 try{
+			 JsonNode rootNode = objectMapper.readTree(userinfo);
+			 JsonNode digiRoleList = rootNode.get("digiRole");
+			 if (digiRoleList != null && digiRoleList.isArray()) {
+				 for (JsonNode role : digiRoleList) {
+					 UserRole userRole = new UserRole();
+					 userRole.setId(role.get("id").asText());
+					 userRole.setName(role.get("name").asText());
+					 userRoles.add(userRole);
+				 }
+			 }
+		 }catch(Exception e){
+			 log.debug("Exception occured during saving user role");
+		 }
+		 this.userStore.getUserInfo().setUserRole(userRoles);
 	}
 
 	/**
