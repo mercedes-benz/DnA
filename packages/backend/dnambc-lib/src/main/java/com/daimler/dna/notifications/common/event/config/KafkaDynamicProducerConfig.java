a13e9ced93792539f8028c0819b53c027588e324
PriyankaParamesh <161298475+PriyankaParamesh@users.noreply.github.com>
2024-03-15 13:28:07 +0530

    Update KafkaDynamicProducerConfig.java for dna backend

--- packages/backend/dnambc-lib/src/main/java/com/daimler/dna/notifications/common/event/config/KafkaDynamicProducerConfig.java
+++ packages/backend/dnambc-lib/src/main/java/com/daimler/dna/notifications/common/event/config/KafkaDynamicProducerConfig.java
@@ -11,6 +11,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
+import org.apache.kafka.clients.CommonClientConfigs;
+import org.apache.kafka.common.config.SslConfigs;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -21,6 +23,22 @@ public class KafkaDynamicProducerConfig {
 	@Value(value = "${spring.cloud.stream.kafka.binder.brokers}")
 	private String bootstrapAddress;
 
+	@Value(value = "${spring.kafka.properties.ssl.enabled}")
+	private Boolean sslEnabled;
+
+	@Value(value = "${spring.kafka.properties.ssl.keystore.location}")
+	 private String sslKeyStoreLocation;
+ 
+	 @Value(value = "${spring.kafka.properties.ssl.keystore.password}")
+	 private String sslKeyStorePassword;
+ 
+	 @Value(value = "${spring.kafka.properties.ssl.truststore.location}")
+	 private String sslTrustStoreLocation;
+ 
+	 @Value(value = "${spring.kafka.properties.ssl.truststore.password}")
+	 private String sslTrustStorePassword;
+
+
 	@Bean
 	public ProducerFactory<String, GenericEventRecord> producerFactory() {
 		Map<String, Object> configProps = new HashMap<>();
@@ -29,6 +47,16 @@ public class KafkaDynamicProducerConfig {
 		configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, GenericEventRecordSerializer.class);
 		log.info("New ProducerFactory created with bootstrap_server{} and String Key serializer "
 				+ "and GenericEventRecordSerializer for value", bootstrapAddress);
+		if (sslEnabled) {
+        		configProps.put(SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG,sslKeyStoreLocation);
+			configProps.put(SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG, sslKeyStorePassword);
+			configProps.put(SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG,sslTrustStoreLocation);
+			configProps.put(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG, sslTrustStorePassword);
+			configProps.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SSL");
+			configProps.put(SslConfigs.SSL_KEY_PASSWORD_CONFIG,sslKeyStorePassword);
+			configProps.put(SslConfigs.DEFAULT_SSL_KEYSTORE_TYPE, "PKCS12");
+			configProps.put(SslConfigs.DEFAULT_SSL_TRUSTSTORE_TYPE,"PKCS12");
+        }
 		return new DefaultKafkaProducerFactory<>(configProps);
 	}
 
