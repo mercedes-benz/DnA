e3e7a85ea526a1a842d0a6bd6f8ab5f92238d5f9
SHPEEKA <SHPEEKA>
2024-03-26 11:27:29 +0000

    tools detailed page

--- packages/frontend/src/components/mbc/createNewSolution/description/logoManager/LogoImage/LogoImage.tsx
+++ packages/frontend/src/components/mbc/createNewSolution/description/logoManager/LogoImage/LogoImage.tsx
@@ -4,14 +4,14 @@ import { ILogoDetails } from 'globals/types';
 
 export interface ILogoImageProps {
   displayType: string;
-  logoDetails?: ILogoDetails;
+  logoDetails: ILogoDetails;
   width?: string;
   height?: string;
   className?: string;
 }
 
 const LogoImage = (props: ILogoImageProps) => {
-    const logoId = props.logoDetails
+  const logoId = props.logoDetails
     ? PredefinedSolutionLogoImagesInfo.images.some((image) => image.id === props.logoDetails.id)
       ? props.logoDetails.id
       : 'default'
