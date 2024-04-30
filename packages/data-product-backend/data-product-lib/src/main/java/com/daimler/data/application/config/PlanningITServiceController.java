6e5fdf7c53d93c109c6c10a0a0dc0ddfe5f22a99
aravindh45 <63506505+aravindh45@users.noreply.github.com>
2024-04-30 13:58:26 +0530

    added planningIt auth header (#2771)
    Co-authored-by: ARAARUM <ARAARUM>

--- packages/data-product-backend/data-product-lib/src/main/java/com/daimler/data/application/config/PlanningITServiceController.java
+++ packages/data-product-backend/data-product-lib/src/main/java/com/daimler/data/application/config/PlanningITServiceController.java
@@ -3,9 +3,11 @@ package com.daimler.data.application.config;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,6 +19,7 @@ import com.daimler.data.controller.exceptions.GenericMessage;
 import com.daimler.data.controller.exceptions.MessageDescription;
 import com.daimler.data.dto.planningit.PlanningITVO;
 import com.daimler.data.dto.planningit.PlanningITVOCollection;
+import com.daimler.data.service.forecast.BaseFabricWorkspaceService;
 import com.daimler.data.service.planningit.PlanningITService;
 
 import io.swagger.annotations.Api;
@@ -24,14 +27,22 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @Api(value = "Planningit", tags = { "planningit" })
 @RequestMapping("/service")
+@Slf4j
 public class PlanningITServiceController {
 
 	@Autowired
 	private PlanningITService service;
+	
+	@Autowired
+	private  HttpServletRequest request;
+	
+	@Value("${planningIT.headers.authorization.token}")
+	private String planningITAuthToken;
 	
 	@ApiOperation(value = "Bulk add planning IT systems", nickname = "insertAll", notes = "Bulk add planning IT systems", response = GenericMessage.class, tags={ "planningit", })
     @ApiResponses(value = { 
@@ -47,8 +58,18 @@ public class PlanningITServiceController {
         method = RequestMethod.POST)
     public ResponseEntity<GenericMessage> insertAll(@ApiParam(value = "Request Body that contains data required for creating a new dataproduct provider form" ,
     	required=true )  @Valid @RequestBody PlanningITVOCollection planningITCollectionRequestVO){
+		
 		HttpStatus responseCode = HttpStatus.CREATED;
 		GenericMessage responseMessage = new GenericMessage();
+		String requestedKey = request.getHeader("planningIT-x-API");
+		if(!planningITAuthToken.equals(requestedKey)) {
+			List<MessageDescription> errors = new ArrayList<>();
+			errors.add(new MessageDescription("Invalid authorization token, Forbidden."));
+			log.error("Invalid authorization token, Forbidden.");
+			responseMessage.setSuccess("FAILED");
+			responseMessage.setErrors(errors);
+			return new ResponseEntity<>(responseMessage,HttpStatus.FORBIDDEN);
+		}
 		if(planningITCollectionRequestVO!=null) {
 			List<PlanningITVO> volist = planningITCollectionRequestVO.getData();
 			try {
