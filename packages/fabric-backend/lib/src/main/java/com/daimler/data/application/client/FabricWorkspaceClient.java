a3b1977084983c80077501000c7929f2fd89f425
boonbaluri <boonbaluri@gmail.com>
2024-05-06 14:34:16 +0530

    Update FabricWorkspaceClient.java (#2792)
    * Update FabricWorkspaceClient.java
    
    * Update BaseFabricWorkspaceService.java
    
    * Update FabricWorkspaceClient.java
    
    * Update BaseFabricWorkspaceService.java

--- packages/fabric-backend/lib/src/main/java/com/daimler/data/application/client/FabricWorkspaceClient.java
+++ packages/fabric-backend/lib/src/main/java/com/daimler/data/application/client/FabricWorkspaceClient.java
@@ -162,8 +162,17 @@ public class FabricWorkspaceClient {
 				workspaceDetailDto = response.getBody();
 			}
 		}catch(Exception e) {
-			log.error("Failed to get workspace details for id {} with {} exception ", workspaceId, e.getMessage());
-			return null;
+			if(e.getMessage()!=null) 
+				if(e.getMessage().contains("InsufficientPrivileges")) {
+					workspaceDetailDto.setErrorCode("403");
+					workspaceDetailDto.setMessage("Failed to fetch details, InsufficientPrivileges. Record might not exist.");
+					log.error("Failed to get workspace details for id {} with {} exception . Which could mean that workspace doesnt exist anymore.", workspaceId, e.getMessage());
+				}else if (e.getMessage().contains("WorkspaceNotFound")) {
+					workspaceDetailDto.setErrorCode("404");
+					workspaceDetailDto.setMessage("Failed to fetch details, WorkspaceNotFound. Record might not exist.");
+					log.error("Failed to get workspace details for id {} with {} exception . Which could mean that workspace doesnt exist anymore.", workspaceId, e.getMessage());
+				}
+			return workspaceDetailDto;
 		}
 		return workspaceDetailDto;
 	}
@@ -280,7 +289,7 @@ public class FabricWorkspaceClient {
 			headers.setContentType(MediaType.APPLICATION_JSON);
 			HttpEntity<WorkspaceUpdateDto> requestEntity = new HttpEntity<>(updateRequest,headers);
 			String workspaceUrl = workspacesBaseUrl + "/" + workspaceId;
-			ResponseEntity<WorkspaceDetailDto> response = proxyRestTemplate.exchange(workspaceUrl, HttpMethod.POST,
+			ResponseEntity<WorkspaceDetailDto> response = proxyRestTemplate.exchange(workspaceUrl, HttpMethod.PATCH,
 					requestEntity, WorkspaceDetailDto.class);
 			if (response!=null && response.hasBody()) {
 				workspaceDetailDto = response.getBody();
