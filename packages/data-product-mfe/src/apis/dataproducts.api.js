490b6fc6182e2d26dd444a9589fe352c15f99b31
SHPEEKA <SHPEEKA>
2024-05-07 08:20:45 +0000

    Added DataProduct Filter

--- packages/data-product-mfe/src/apis/dataproducts.api.js
+++ packages/data-product-mfe/src/apis/dataproducts.api.js
@@ -24,6 +24,177 @@ const createDataProduct = (data) => {
   });
 };
 
+const getDataProductsByGraphQL = (data) =>{ 
+  let reqQuery = `art:"${data.arts}",platform:"${data.platforms}",frontendTool:"${data.frontendTools}",offset:${data.offset},limit:${data.limit},division:"${data.divisions}",department:"${data.departments}",productOwner:"${data.productOwners}",informationOwner:"${data.informationOwners}",dataSteward:"${data.dataStewards}"`;
+  const resQuery = `totalCount
+  records {
+      id,
+      dataProductId,
+      dataProductName,
+      description,
+      carLaFunction {
+          id,
+          name
+      }
+      ddx,
+      productOwner {
+          shortId,
+          userType,
+          firstName,
+          lastName,
+          department,
+          email,
+          mobileNumber,
+          company,
+          teamMemberPosition,
+          addedByProvider,
+      }
+      tags,
+      platform {
+          id,
+          name
+      },
+      frontEndTools {
+          id,
+          name,
+      }
+      access {
+          accessType,
+          kafka,
+          oneApi,
+          confidentiality,
+          personalRelatedData,
+          deletionRequirement,
+          restrictDataAccess,
+          minimumInformationCheck,
+      },
+      howToAccessTemplate {
+          useTemplate,
+          accessDetailsCollectionVO {
+              accessType,
+              stepCollectionVO {
+                  stepNumber,
+                  stepIconType,
+                  stepText,
+              }
+          }
+      },
+      agileReleaseTrain {
+          id,
+          name
+      },
+      corporateDataCatalog {
+          id,
+          name
+      },
+      recordStatus,
+      isPublish,
+      notifyUsers,
+      createdDate,
+      lastModifiedDate,
+      additionalInformation,
+      createdBy {
+          id,
+          name,
+          description,
+          firstName,
+          lastName,
+          department,
+          email,
+          mobileNumber,
+      },
+      modifiedBy {
+          id,
+          name,
+          description,
+          firstName,
+          lastName,
+          department,
+          email,
+          mobileNumber,
+      },
+      contactInformation {
+          department
+          localComplianceOfficer
+          appId
+          informationOwner {
+              shortId,
+              userType,
+              firstName,
+              lastName,
+              department,
+              email,
+              mobileNumber,
+              company,
+              teamMemberPosition,
+              addedByProvider,
+          },
+          name {
+              shortId,
+              userType,
+              firstName,
+              lastName,
+              department,
+              email,
+              mobileNumber,
+              company,
+              teamMemberPosition,
+              addedByProvider,
+          },
+          division {
+              id,
+              name,
+              subdivision {
+                  id,
+                  name
+              }
+          }
+      }
+      classificationConfidentiality {
+          description,
+          confidentiality
+      },
+      personalRelatedData {
+          personalRelatedData,
+          description,
+          purpose,
+          legalBasis,
+          contactAwareTransfer,
+          objectionsToTransfer,
+          transferringNonetheless,
+          objections
+      },
+      transnationalDataTransfer {
+          dataTransferred,
+          notWithinEU,
+          contactAwareTransfer,
+          objectionsToTransfer,
+          transferringNonetheless,
+          objections
+      },
+      deletionRequirement {
+          deletionRequirements,
+          description,
+          otherRelevantInformation,
+          insiderInformation
+      },
+      datatransfersAssociated {
+          id,
+          datatransferId,
+          datatrandferName
+      },
+      openSegments
+  } `;
+  const apiQuery = {
+    query: `query Dataproducts{
+      dataproducts(${reqQuery}){
+        ${resQuery}
+      }
+    }`,
+  };
+  return server.post('minified', apiQuery);
+
+}
 const updateDataProduct = (data) => {
   return server.put('/dataproducts', {
     data,
@@ -103,5 +274,6 @@ export const dataProductApi = {
   getAllPlatforms,
   getAllFrontEndTools,
   getAllTags,
-  getAllAccessTypes
+  getAllAccessTypes,
+  getDataProductsByGraphQL
 };
