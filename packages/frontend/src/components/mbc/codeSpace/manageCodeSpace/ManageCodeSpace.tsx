7d4b2b18930ae6ee4d1e7a8502944ae67d34d79e
EBHANDA <eshaanibhandary@gmail.com>
2024-05-09 04:12:26 +0000

    security config tile disabled under manage codespaces

--- packages/frontend/src/components/mbc/codeSpace/manageCodeSpace/ManageCodeSpace.tsx
+++ packages/frontend/src/components/mbc/codeSpace/manageCodeSpace/ManageCodeSpace.tsx
@@ -59,7 +59,7 @@ export interface InewRecipeField {
 const ManageCodeSpace = () => {
   const classNames = cn.bind(Styles);
   const [loading, setLoading] = useState<boolean>(true);
-  const [workSpaceConfigs, setWorkSpaceConfigs] = useState<IWorkSpaceConfigs[]>([]);
+  //const [workSpaceConfigs, setWorkSpaceConfigs] = useState<IWorkSpaceConfigs[]>([]);
   const [newRecipes, setNewRecipes] = useState<InewRecipeField[]>([]);
   const [totalNumberOfPages, setTotalNumberOfPages] = useState(1);
   const [currentPageNumber, setCurrentPageNumber] = useState(1);
@@ -68,8 +68,9 @@ const ManageCodeSpace = () => {
     parseInt(sessionStorage.getItem(SESSION_STORAGE_KEYS.PAGINATION_MAX_ITEMS_PER_PAGE), 10) || 15,
   );
   const [sortBy, setSortBy] = useState({ name: 'requestedDate', currentSortType: 'desc', nextSortType: 'asc' });
-  const [currentTab, setCurrentTab] = useState('securityConfig');
-  const isConfigTab = currentTab === 'securityConfig' ? true : false;
+  // const [currentTab, setCurrentTab] = useState('securityConfig');
+  // const isConfigTab = currentTab === 'securityConfig' ? true : false;
+
 
   const onPaginationPreviousClick = () => {
     const currentPageNum = currentPageNumber - 1;
@@ -101,34 +102,35 @@ const ManageCodeSpace = () => {
     setCurrentPageOffset(currentPageOffsetInit);
   }, []);
   useEffect(() => {
-    if (currentTab === 'securityConfig') {
-      getRequestedSecurityConfig();
-    } else {
-      getRequestedNewCodeSpaces();
-    }
-  }, [currentPageOffset, maxItemsPerPage, currentTab]);
+    // if (currentTab === 'securityConfig') {
+    //   getRequestedSecurityConfig();
+    // } else {
+    //   getRequestedNewCodeSpaces();
+    // }
+    getRequestedNewCodeSpaces();
+  }, [currentPageOffset, maxItemsPerPage]);
 
   const showErrorNotification = (message: string) => {
     setLoading(false);
     Notification.show(message, 'alert');
   };
 
-  const getRequestedSecurityConfig = () => {
-    setLoading(true);
-    CodeSpaceApiClient.getWorkspaceConfigs()
-      .then((res: any) => {
-        setLoading(false);
-        ProgressIndicator.hide();
-        const totalNumberOfPagesInner = Math.ceil(res.totalCount / maxItemsPerPage);
-        setCurrentPageNumber(currentPageNumber > totalNumberOfPagesInner ? 1 : currentPageNumber);
-        setTotalNumberOfPages(totalNumberOfPagesInner);
-        setWorkSpaceConfigs(Array.isArray(res) ? res : (res.data as IWorkSpaceConfigs[]));
-      })
-      .catch((error: any) => {
-        setLoading(false);
-        showErrorNotification(error.message ? error.message : 'Some Error Occured');
-      });
-  };
+  // const getRequestedSecurityConfig = () => {
+  //   setLoading(true);
+  //   CodeSpaceApiClient.getWorkspaceConfigs()
+  //     .then((res: any) => {
+  //       setLoading(false);
+  //       ProgressIndicator.hide();
+  //       const totalNumberOfPagesInner = Math.ceil(res.totalCount / maxItemsPerPage);
+  //       setCurrentPageNumber(currentPageNumber > totalNumberOfPagesInner ? 1 : currentPageNumber);
+  //       setTotalNumberOfPages(totalNumberOfPagesInner);
+  //       setWorkSpaceConfigs(Array.isArray(res) ? res : (res.data as IWorkSpaceConfigs[]));
+  //     })
+  //     .catch((error: any) => {
+  //       setLoading(false);
+  //       showErrorNotification(error.message ? error.message : 'Some Error Occured');
+  //     });
+  // };
 
   const getRequestedNewCodeSpaces = () => {
     setLoading(true);
@@ -148,11 +150,12 @@ const ManageCodeSpace = () => {
   };
 
   const handleDataChange = () => {
-    if (isConfigTab) {
-      getRequestedSecurityConfig();
-    } else {
-      getRequestedNewCodeSpaces();
-    }
+    // if (isConfigTab) {
+    //   getRequestedSecurityConfig();
+    // } else {
+    //   getRequestedNewCodeSpaces();
+    // }
+    getRequestedNewCodeSpaces();
   };
 
   const sortByColumn = (propName: string, sortOrder: string) => {
@@ -173,49 +176,49 @@ const ManageCodeSpace = () => {
       const timeParts = parts[1].split(':');
       return new Date(dateParts[2], dateParts[1] - 1, dateParts[0], timeParts[0], timeParts[1], timeParts[2]);
     };
-    if (isConfigTab) {
-      let data = workSpaceConfigs;
-      if (propName === 'projectName') {
-        data = data.sort((a, b) => {
-          if (sortOrder === 'asc') {
-            return a.projectName.toLowerCase().localeCompare(b.projectName.toLowerCase());
-          } else {
-            return b.projectName.toLowerCase().localeCompare(a.projectName.toLowerCase());
-          }
-        });
-      } else if (propName === 'projectOwner') {
-        data = data.sort((a, b) => {
-          if (sortOrder === 'asc') {
-            return a.projectOwner.firstName.toLowerCase().localeCompare(b.projectOwner.firstName.toLowerCase());
-          } else {
-            return b.projectOwner.firstName.toLowerCase().localeCompare(a.projectOwner.firstName.toLowerCase());
-          }
-        });
-      } else if (propName === 'projectStatus') {
-        data = data.sort((a, b) => {
-          if (sortOrder === 'asc') {
-            return a.securityConfig.status.toLowerCase().localeCompare(b.securityConfig.status.toLowerCase());
-          } else {
-            return b.securityConfig.status.toLowerCase().localeCompare(a.securityConfig.status.toLowerCase());
-          }
-        });
-      } else if (propName === 'requestedDate') {
-        data = data.sort((a, b) => {
-          const dateA = convertToDateObj(
-            regionalDateAndTimeConversionSolution(a.securityConfig.requestedDate),
-          ).getTime();
-          const dateB = convertToDateObj(
-            regionalDateAndTimeConversionSolution(b.securityConfig.requestedDate),
-          ).getTime();
-          if (sortOrder === 'asc') {
-            return dateA - dateB;
-          } else {
-            return dateB - dateA;
-          }
-        });
-        setWorkSpaceConfigs(data);
-      }
-    } else {
+    // if (isConfigTab) {
+    //   let data = workSpaceConfigs;
+    //   if (propName === 'projectName') {
+    //     data = data.sort((a, b) => {
+    //       if (sortOrder === 'asc') {
+    //         return a.projectName.toLowerCase().localeCompare(b.projectName.toLowerCase());
+    //       } else {
+    //         return b.projectName.toLowerCase().localeCompare(a.projectName.toLowerCase());
+    //       }
+    //     });
+    //   } else if (propName === 'projectOwner') {
+    //     data = data.sort((a, b) => {
+    //       if (sortOrder === 'asc') {
+    //         return a.projectOwner.firstName.toLowerCase().localeCompare(b.projectOwner.firstName.toLowerCase());
+    //       } else {
+    //         return b.projectOwner.firstName.toLowerCase().localeCompare(a.projectOwner.firstName.toLowerCase());
+    //       }
+    //     });
+    //   } else if (propName === 'projectStatus') {
+    //     data = data.sort((a, b) => {
+    //       if (sortOrder === 'asc') {
+    //         return a.securityConfig.status.toLowerCase().localeCompare(b.securityConfig.status.toLowerCase());
+    //       } else {
+    //         return b.securityConfig.status.toLowerCase().localeCompare(a.securityConfig.status.toLowerCase());
+    //       }
+    //     });
+    //   } else if (propName === 'requestedDate') {
+    //     data = data.sort((a, b) => {
+    //       const dateA = convertToDateObj(
+    //         regionalDateAndTimeConversionSolution(a.securityConfig.requestedDate),
+    //       ).getTime();
+    //       const dateB = convertToDateObj(
+    //         regionalDateAndTimeConversionSolution(b.securityConfig.requestedDate),
+    //       ).getTime();
+    //       if (sortOrder === 'asc') {
+    //         return dateA - dateB;
+    //       } else {
+    //         return dateB - dateA;
+    //       }
+    //     });
+    //     setWorkSpaceConfigs(data);
+    //   }
+    // } else {
       let data = newRecipes;
       if (propName === 'recipeName') {
         data = data.sort((a, b) => {
@@ -254,25 +257,25 @@ const ManageCodeSpace = () => {
         });
       }
       setNewRecipes(data);
-    }
+    // }
 
     setSortBy(newSortField);
   };
 
-  const configData = workSpaceConfigs?.map((config) => {
-    return (
-      <CodeSpaceList
-        key={config.id}
-        id={config.id}
-        projectName={config.projectName}
-        projectOwner={config.projectOwner}
-        projectStatus={config.securityConfig.status}
-        requestedDate={config.securityConfig?.requestedDate}
-        onDataChanged={handleDataChange}
-        isConfigList={isConfigTab}
-      />
-    );
-  });
+  // const configData = workSpaceConfigs?.map((config) => {
+  //   return (
+  //     <CodeSpaceList
+  //       key={config.id}
+  //       id={config.id}
+  //       projectName={config.projectName}
+  //       projectOwner={config.projectOwner}
+  //       projectStatus={config.securityConfig.status}
+  //       requestedDate={config.securityConfig?.requestedDate}
+  //       onDataChanged={handleDataChange}
+  //       isConfigList={isConfigTab}
+  //     />
+  //   );
+  // });
 
   const recipeData = newRecipes?.map((recipe) => {
     return (
@@ -284,7 +287,7 @@ const ManageCodeSpace = () => {
         requestedDate={recipe.createdOn}
         projectStatus={recipe.status}
         onDataChanged={handleDataChange}
-        isConfigList={isConfigTab}
+        isConfigList={false}
       />
     );
   });
@@ -292,9 +295,9 @@ const ManageCodeSpace = () => {
   return (
     <div className={Styles.mainPanel}>
       <div className={Styles.wrapper}>
-        <Caption title="Code Space Administration" />
-        <div id="manage-codeSpace-tabs" className="tabs-panel">
-          <div className="tabs-wrapper">
+        <Caption title="Recipe Management" />
+          {/* <div id="manage-codeSpace-tabs" className="tabs-panel"> */}
+          {/* <div className="tabs-wrapper">
             <nav>
               <React.Fragment>
                 <ul className="tabs">
@@ -311,9 +314,9 @@ const ManageCodeSpace = () => {
                 </ul>
               </React.Fragment>
             </nav>
-          </div>
+          </div> */}
           <div className="tabs-content-wrapper">
-            <div id="tab-content-1" className="tab-content">
+            {/* <div id="tab-content-1" className="tab-content">
               {currentTab === 'securityConfig' ? (
                 <div className={Styles.content}>
                   {loading ? (
@@ -394,9 +397,9 @@ const ManageCodeSpace = () => {
               ) : (
                 ''
               )}
-            </div>
+            </div> */}
             <div id="tab-content-2" className="tab-content">
-              {currentTab === 'newRecipe' ? (
+              {/* {currentTab === 'newRecipe' ? ( */}
                 <div className={Styles.content}>
                   {loading ? (
                     <div className={'progress-block-wrapper ' + Styles.preloaderCutomnize}>
@@ -473,12 +476,12 @@ const ManageCodeSpace = () => {
                     </div>
                   )}
                 </div>
-              ) : (
+              {/* ) : (
                 ''
-              )}
+              )} */}
             </div>
           </div>
-        </div>
+        {/* </div> */}
       </div>
     </div>
   );
