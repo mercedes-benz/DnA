37999bcbc28c459a59668b3a807fb3b7e3ebcda1
GAISUSH <GAISUSH>
2024-05-15 04:44:03 +0000

    changes to onboard dash recipe..

--- packages/code-server/code-server-lib/src/main/java/com/daimler/data/util/ConstantsUtility.java
+++ packages/code-server/code-server-lib/src/main/java/com/daimler/data/util/ConstantsUtility.java
@@ -39,6 +39,7 @@ public class ConstantsUtility {
 	public static final String GRADLE = "gradle";
 	public static final String QUARKUS = "quarkus";
 	public static final String MICRONAUT = "micronaut";
+	public static final String DASHPYTHON = "dash";
 	public static final String DEFAULT = "default";
 	public static final String COLLABREQUESTEDSTATE = "COLLABORATION_REQUESTED";
 	public static final String CREATEREQUESTEDSTATE = "CREATE_REQUESTED";
