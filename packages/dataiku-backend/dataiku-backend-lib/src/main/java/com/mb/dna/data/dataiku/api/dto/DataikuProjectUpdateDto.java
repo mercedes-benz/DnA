d84f8d3d5cf91cb4269314054a61941c02aaedd3
GAISUSH <GAISUSH>
2024-05-15 04:07:51 +0000

    changes for adding flag

--- packages/dataiku-backend/dataiku-backend-lib/src/main/java/com/mb/dna/data/dataiku/api/dto/DataikuProjectUpdateDto.java
+++ packages/dataiku-backend/dataiku-backend-lib/src/main/java/com/mb/dna/data/dataiku/api/dto/DataikuProjectUpdateDto.java
@@ -24,7 +24,7 @@ public class DataikuProjectUpdateDto implements Serializable{
 	private String status;
 	private String classificationType;
 	private Boolean hasPii;
-	private Boolean has_termsOfUse;
+	private Boolean hasTermsOfUse;
 	private String divisionId;
 	private String divisionName;
 	private String subdivisionId;
