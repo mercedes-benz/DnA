ba23f7fdc12d2c44d752c57ca9267dbb41b9cf4e
Khartade <aniruddhakhartade@gmail.com>
2024-05-23 17:07:37 +0530

    data entry ui improvements

--- packages/dataentry-mfe/src/pages/dataEntryProject/DataEntryProject.js
+++ packages/dataentry-mfe/src/pages/dataEntryProject/DataEntryProject.js
@@ -2,22 +2,30 @@ import classNames from 'classnames';
 import React, { useState, useEffect, useRef } from 'react';
 import { useParams } from 'react-router-dom';
 import Styles from './data-entry-project.scss';
+// Container Components
+import Modal from 'dna-container/Modal';
 import Caption from 'dna-container/Caption';
-// utils
-import { regionalDateAndTimeConversionSolution } from '../../utilities/utils';
 import ProgressIndicator from '../../common/modules/uilab/js/src/progress-indicator';
 import Notification from '../../common/modules/uilab/js/src/notification';
 import DataEntrySheet from '../../components/dataEntrySheet/DataEntrySheet';
 import { dataEntryApi } from '../../apis/dataentry.api';
 import { DEFAULT_WORKBOOK_DATA } from '../../utilities/template';
+import ProjectDetails from '../../components/projectDetails/ProjectDetails';
+import DeUsersInformation from '../../components/deUsersInformation/DeUsersInformation';
+import DataEntryUsers from '../../components/dataEntryUsers/DataEntryUsers';
+import { regionalDateAndTimeConversionSolution } from '../../utilities/utils';
 
-const DataEntryProject = () => {
+const DataEntryProject = ({ user }) => {
   const { id: projectId } = useParams();
   const univerRef = useRef();
   const [data] = useState(DEFAULT_WORKBOOK_DATA);
 
   const [loading, setLoading] = useState(true);
   const [project, setProject] = useState();
+
+  const [showDeUsersModal, setShowDeUsersModal] = useState(false);
+  const [showDeUsersInformationModal, setShowDeUsersInformationModal] = useState(false);
+  const [showProjectDetailsModal, setShowProjectDetailsModal] = useState(false);
 
   useEffect(() => {
     getProject();
@@ -48,136 +56,92 @@ const DataEntryProject = () => {
   return (
     <React.Fragment>
       <div className={classNames(Styles.mainPanel)}>
-        <div className={classNames(Styles.wrapper)}>
-          {!loading ? 
-            <Caption title={project?.name} /> : null
-          }
-          {/* <div onClick={toggleFullScreenMode}>
-            <FullScreenModeIcon fsNeed={fullScreenMode} />
-          </div> */}
-          <div>
-            <div>
-              <DataEntrySheet style={{ flex: 1 }} ref={univerRef} data={data} />
-            </div>
-            <div className={Styles.btnContainer}>
-              <button
-                className={'btn btn-primary'}
-                onClick={() => {
-                  console.log(univerRef.current?.getData());
-                }}
-              >
-                Save as Draft
-              </button>
-              <button
-                className={'btn btn-tertiary'}
-                onClick={() => {
-                  console.log(univerRef.current?.getData());
-                }}
-              >
-                Send to Fillers
-              </button>
-            </div>
+        <div className={classNames(Styles.flex)}>
+          <div className={Styles.col2}>
+            { !loading && 
+              <Caption title={project?.name}>
+                <span className={Styles.dueDate}>(Survey due date: {project?.dueDate ? regionalDateAndTimeConversionSolution(project?.dueDate) : ''})</span>
+              </Caption> 
+            }
           </div>
-          <div className={Styles.content}>
-            <h3 id="productName">Project Details</h3>
-            <div className={Styles.firstPanel}>
-              <div className={Styles.formWrapper}>
-                {!loading &&
-                  <>
-                    <div className={classNames(Styles.flexLayout, Styles.threeColumn)}>
-                      <div id="productDescription">
-                        <label className="input-label summary">Project Name</label>
-                        <br />
-                        {project?.name}
-                      </div>
-                      <div id="tags">
-                        <label className="input-label summary">Created on</label>
-                        <br />
-                        {project?.createdOn !== undefined && regionalDateAndTimeConversionSolution(project?.createdOn)}
-                      </div>
-                      <div id="isExistingSolution">
-                        <label className="input-label summary">Created by</label>
-                        <br />
-                        {project?.createdBy?.firstName} {project?.createdBy?.lastName}
-                      </div>
-                    </div>
-
-                    <div className={classNames(Styles.flexLayout, Styles.threeColumn)}>
-                      <div id="typeOfProjectOption">
-                        <label className="input-label summary">Type of Project</label>
-                        <br />
-                        {project?.typeOfProject ? project?.typeOfProject : 'N/A'}
-                      </div>
-                      <div id="description">
-                        <label className="input-label summary">Description</label>
-                        <br />
-                        {project?.decription ? project?.decription : 'N/A'}
-                      </div>
-                      <div id="divisionField">
-                      </div>
-                    </div>
-
-                    <div className={classNames(Styles.flexLayout, Styles.threeColumn)}>
-                      <div id="divisionField">
-                        <label className="input-label summary">Division</label>
-                        <br />
-                        {project?.division === '0' || !project?.division ? 'N/A' : project?.division}
-                      </div>
-                      <div id="subDivisonField">
-                        <label className="input-label summary">Sub Division</label>
-                        <br />
-                        {project?.subDivision === '0' || !project?.subDivision ? 'N/A' : project?.subDivision}
-                      </div>
-                    </div>
-
-                    <div className={classNames(Styles.flexLayout, Styles.threeColumn)}>
-                      <div id="department">
-                        <label className="input-label summary">Department</label>
-                        <br />
-                        {project?.department ? project?.department : 'N/A'}
-                      </div>
-                      <div id="tags">
-                        <label className="input-label summary">Tags</label>
-                        <br />
-                        {project?.tags ? project.tags?.map((chip) => {
-                          return (
-                            <>
-                              <label className="chips">{chip}</label>&nbsp;&nbsp;
-                            </>
-                          );
-                        }) : 'N/A'}
-                      </div>
-                      <div id="dataClassificationField">
-                        <label className="input-label summary">Data Classification</label>
-                        <br />
-                        {project?.dataClassification === '0' || !project?.dataClassification ? 'N/A' : project?.dataClassification}
-                      </div>
-                    </div>
-
-                    <div className={classNames(Styles.flexLayout, Styles.threeColumn)}>
-                      <div id="PiiData">
-                        <label className="input-label summary">PII</label>
-                        <br />
-                        {project?.piiData === true ? 'Yes' : 'No'}
-                      </div>
-                      <div id="archerId">
-                        <label className="input-label summary">Archer ID</label>
-                        <br />
-                        {project?.archerId ? project?.archerId : 'N/A'}
-                      </div>
-                      <div id="procedureId">
-                        <label className="input-label summary">Procedure ID</label>
-                        <br />
-                        {project?.procedureId ? project?.procedureId : 'N/A'}
-                      </div>
-                    </div>
-                  </>
-                }
-              </div>
+          <div className={Styles.col2}>
+            <div className={Styles.actionsGroup}>
+              <button className={classNames('btn', Styles.btnLess)} onClick={() => setShowProjectDetailsModal(true)}>
+                <i className={'icon mbc-icon info'}></i>
+                <span>Project Details</span>
+              </button>
+              <button className={classNames('btn', Styles.btnLess)} onClick={() => setShowDeUsersInformationModal(true)}>
+                <i className={'icon mbc-icon profile'}></i>
+                <span>Data Entry Users</span>
+              </button>
             </div>
           </div>
         </div>
+        <div>
+          <p className={Styles.fillingInstructions}>Filling Instructions</p>
+          <div>
+            <DataEntrySheet style={{ flex: 1 }} ref={univerRef} data={data} />
+          </div>
+          <div className={Styles.footer}>
+            <button
+              className={'btn btn-primary'}
+              onClick={() => {
+                console.log(univerRef.current?.getData());
+              }}
+            >
+              Save as Draft
+            </button>
+            <button
+              className={'btn btn-tertiary'}
+              onClick={() => {
+                console.log(univerRef.current?.getData());
+                setShowDeUsersModal(true);
+              }}
+            >
+              Send to Data Entry Users
+            </button>
+          </div>
+        </div>
       </div>
+      { showProjectDetailsModal &&
+        <Modal
+          title={'Project Details'}
+          hiddenTitle={true}
+          showAcceptButton={false}
+          showCancelButton={false}
+          modalWidth={'800px'}
+          show={showProjectDetailsModal}
+          content={<ProjectDetails project={project} />}
+          scrollableContent={true}
+          onCancel={() => setShowProjectDetailsModal(false)}
+        />
+      }
+      { showDeUsersModal &&
+        <Modal
+          title={'Add Data Entry Users'}
+          hiddenTitle={true}
+          showAcceptButton={false}
+          showCancelButton={false}
+          modalWidth={'800px'}
+          show={showDeUsersModal}
+          content={<DataEntryUsers user={user} />}
+          scrollableContent={true}
+          onCancel={() => setShowDeUsersModal(false)}
+        />
+      }
+      { showDeUsersInformationModal &&
+        <Modal
+          title={'Data Entry Users Information'}
+          hiddenTitle={true}
+          showAcceptButton={false}
+          showCancelButton={false}
+          modalWidth={'800px'}
+          show={showDeUsersInformationModal}
+          content={<DeUsersInformation project={project} />}
+          scrollableContent={true}
+          onCancel={() => setShowDeUsersInformationModal(false)}
+        />
+      }
     </React.Fragment>
   );
 }
