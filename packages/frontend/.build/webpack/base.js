f1c1933e237c5d0793a8c6727833fcb4216bd810
Khartade <aniruddhakhartade@gmail.com>
2024-05-28 19:22:46 +0530

    frontend changes for power-platform-mfe

--- packages/frontend/.build/webpack/base.js
+++ packages/frontend/.build/webpack/base.js
@@ -198,6 +198,7 @@ const base = {
         'datalake-mfe': `datalake_mfe@[(window.INJECTED_ENVIRONMENT && window.INJECTED_ENVIRONMENT.DATALAKE_MFE_APP_URL)]/remoteEntry.js?[(new Date()).getTime()]`,
         'fabric-mfe': `fabric_mfe@[(window.INJECTED_ENVIRONMENT && window.INJECTED_ENVIRONMENT.FABRIC_MFE_APP_URL)]/remoteEntry.js?[(new Date()).getTime()]`,
         'data-entry-mfe': `data_entry_mfe@[(window.INJECTED_ENVIRONMENT && window.INJECTED_ENVIRONMENT.DATA_ENTRY_MFE_APP_URL)]/remoteEntry.js?[(new Date()).getTime()]`,
+        'power-platform-mfe': `power_platform_mfe@[(window.INJECTED_ENVIRONMENT && window.INJECTED_ENVIRONMENT.POWER_PLATFORM_MFE_APP_URL)]/remoteEntry.js?[(new Date()).getTime()]`,
       },
       shared: {
         ...packageJson.dependencies,
