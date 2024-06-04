b93ff2067530a830600ebd79f29bf1c9ddd72565
AJAYKKM <AJAYKKM>
2024-06-04 11:13:18 +0000

    recipe-front-end-changes

--- packages/frontend/src/components/mbc/codeSpace/manageCodeSpace/CodeSpaceList.tsx
+++ packages/frontend/src/components/mbc/codeSpace/manageCodeSpace/CodeSpaceList.tsx
@@ -1,26 +1,27 @@
 import cn from 'classnames';
 import React, { useState } from 'react';
 import Styles from './CodeSpaceList.scss';
-import { ICodeCollaborator } from 'globals/types';
 import { history } from '../../../../router/History';
 import { CodeSpaceApiClient } from '../../../../services/CodeSpaceApiClient';
 import { Notification, ProgressIndicator } from '../../../../assets/modules/uilab/bundle/js/uilab.bundle';
-import { regionalDateAndTimeConversionSolution } from '../../../../services/utils';
 import ViewRecipe from '../codeSpaceRecipe/viewRecipe';
 import Modal from 'components/formElements/modal/Modal';
 
-export interface ICodeSpaceList {
+export interface IRecipeList {
   id: any;
   projectName: string;
-  projectOwner: ICodeCollaborator;
-  projectStatus?: string;
-  requestedDate: string;
+  maxRam?: string;
+  maxCpu?: string;
+  diskSpace?: string;
   onDataChanged: () => void;
+  software?: string[];
   isConfigList: boolean;
+  isPublic: boolean;
 }
 
-const codeSpaceList = (props: ICodeSpaceList) => {
+const codeSpaceList = (props: IRecipeList) => {
   const [viewInfoModel, setviewInfoModel] = useState(false);
+  const [notificationMsg, setNotificationMsg] = useState(false);
   const classNames = cn.bind(Styles);
   const onSecrityConfigClick = () => {
     history.push(`/codespace/adminSecurityconfig/${props.id}?name=${props.projectName}`);
@@ -96,48 +97,85 @@ const codeSpaceList = (props: ICodeSpaceList) => {
     ProgressIndicator.hide();
   };
 
+  const chips =
+  props?.software && props?.software?.length
+      ? props?.software?.map((chip) => {
+          return (
+            <>
+              <label className="chips">{chip}</label>&nbsp;&nbsp;
+            </>
+          );
+        })
+      : 'N/A';
 
-  // const formatDate = (date: string) => {
-  //   const [datePart] = date.split('T');
-  //   const [year, month, day] = datePart.split('-');
-  //   const formattedDate = `${day}-${month}-${year}`;
-  //   return formattedDate;
-  // };
+  const onNotificationMsgAccept = () => {
+    setNotificationMsg(false);
+  };
+  const onNotificationMsgCancel = () => {
+    setNotificationMsg(false);
+  }
+
+  const deleteModal = ()=> {
+    setNotificationMsg(true);
+    return( <Modal
+      title="Public Visibility"
+      show={notificationMsg}
+      showAcceptButton={false}
+      showCancelButton={false}
+      acceptButtonTitle="Confirm"
+      cancelButtonTitle="Cancel"
+      buttonAlignment='right'
+      scrollableContent={false}
+      hideCloseButton={true}
+      content={
+        <div>
+          <header>
+            <button className="modal-close-button" onClick={onNotificationMsgCancel}><i className="icon mbc-icon close thin"></i></button>
+          </header>
+          <div>
+            <p>The Recipe will be visible to all users. Are you sure to make it Public?</p>
+          </div>
+          <div className="btn-set footerRight">
+            <button className="btn btn-primary" type="button" onClick={onNotificationMsgCancel}>Cancel</button>
+            <button className="btn btn-tertiary" type="button" onClick={onNotificationMsgAccept}>Confirm</button>
+          </div>
+        </div>
+      } />);
+  };
   return (
     <React.Fragment>
       <tr
         id={props.id}
         key={props.id}
         className={classNames('data-row', Styles.securityConfigRow)}
-        onClick={props.isConfigList ? onSecrityConfigClick : onNewRecipeClick}
       >
         <td className={'wrap-text ' + classNames(Styles.securityConfigName)}>
-          <div className={Styles.securityConfigNameDivide}>{props.projectName}</div>
+          <div className={Styles.securityConfigNameDivide} onClick={props.isConfigList ? onSecrityConfigClick : onNewRecipeClick}>{props.projectName} </div>
         </td>
         <td className={'wrap-text' + Styles.securityConfigCol}>
           <span className={Styles.securityConfig}>
-            {props.projectOwner?.firstName + ' ' + props.projectOwner.lastName}
+            {"Diskspace- "+props.diskSpace +" CPU- "+props.maxCpu+" RAM- "+props.maxRam}
           </span>
         </td>
 
         <td className={'wrap-text' + Styles.securityConfigCol}>
-          <span>{props.projectStatus}</span>
+          {chips}
         </td>
-
-        <td className={'wrap-text' + Styles.securityConfigCol}>
-          <span>{props.requestedDate !== null ? regionalDateAndTimeConversionSolution(props.requestedDate) : ''}</span>
+         <td className={'wrap-text' + Styles.securityConfigCol}>
+          <span>{props.isPublic ? "Yes" : 'No'}</span>
         </td>
-        <td className={'wrap-text' + Styles.securityConfigCol}>
+       <td className={'wrap-text' + Styles.securityConfigCol}>
           <button
             className={
-              props.projectStatus === 'REQUESTED' ? 'btn btn-primary ' : 'btn btn-tertiary ' + Styles.actionBtn
+              'btn btn-primary '+ Styles.actionBtn
             }
             type="button"
-            onClick={props.projectStatus === 'REQUESTED' ? onAccept : onPublish}
+            onClick={deleteModal}
           >
-            {props.projectStatus === 'REQUESTED' ? 'Accept' : props.projectStatus === 'ACCEPTED' ? 'Publish' : ''}
+             <i className='icon delete'></i>
           </button>
-        </td>
+          
+        </td> 
       </tr>
       {viewInfoModel&&(<Modal
         title={''}
@@ -152,6 +190,7 @@ const codeSpaceList = (props: ICodeSpaceList) => {
           setviewInfoModel(false);
         }}
       />)}
+                         
     </React.Fragment>
   );
 };
