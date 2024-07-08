46632137ca83eb8657d4fc00d781627c85032066
sushma <98083702+6-Sushma@users.noreply.github.com>
2024-07-08 14:32:45 +0530

    Update V10__insert_data.sql (#3023)

--- packages/code-server/code-server-lib/src/main/resources/db/migration/V10__insert_data.sql
+++ packages/code-server/code-server-lib/src/main/resources/db/migration/V10__insert_data.sql
@@ -7,7 +7,7 @@ VALUES
     "plugins": [], "isPublic": true, "gitPath": "", "gitRepoLoc": "", "deployPath": "", "toDeployType": "xxx"}'::jsonb
 ),
 (
-    'DEFAULT',
+    'DEFAULT1',
     '{"osname": "XXX", "recipeName": "xxx", "recipeId": "XXX", "recipeType": "private", "repodetails": "", "diskSpace": "2", "minCpu": 0.5, "maxCpu": 1, "maxRam": "2000", "minRam": "1000", "software": ["JAVA-11.0.4","MAVEN-3.9.4"],
     "createdOn": "2024-06-02T12:02:21.946+00:00", "createdBy": {"id": "xxx", "firstName": "xxx", "lastName": "xxx", "department": "", "email": "", "mobileNumber": "", "gitUserName": "xxx"},
     "plugins": [], "isPublic": true, "gitPath": "", "gitRepoLoc": "", "deployPath": "", "toDeployType": "xxx"}'::jsonb
