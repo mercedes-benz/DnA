73547fccbcaaa6c9e88e9eb340b8e76276db1b40
AJAYKKM <AJAYKKM>
2024-07-12 07:17:36 +0000

    software updates change

---
+++ packages/code-server/code-server-lib/src/main/resources/db/migration/V10_create_tables.sql
@@ -0,0 +1,14 @@
+INSERT INTO recipe_nsql (id, data)
+VALUES
+(
+    'DEFAULT',
+    '{"osname": "XXX", "recipeName": "xxx", "recipeId": "XXX", "recipeType": "private", "repodetails": "", "diskSpace": "2", "minCpu": 0.5, "maxCpu": 1, "maxRam": "2000", "minRam": "1000", "software": ["JAVA-11.0.4","GRADLE-7.6.3"],
+    "createdOn": "2024-06-02T12:02:21.946+00:00", "createdBy": {"id": "XXX", "firstName": "XXX", "lastName": "XXX", "department": "", "email": "", "mobileNumber": "", "gitUserName": "XXX"},
+    "plugins": [], "isPublic": true, "gitPath": "", "gitRepoLoc": "", "deployPath": "", "toDeployType": "xxx"}'::jsonb
+),
+(
+    'DEFAULT1',
+    '{"osname": "XXX", "recipeName": "xxx", "recipeId": "XXX", "recipeType": "private", "repodetails": "", "diskSpace": "2", "minCpu": 0.5, "maxCpu": 1, "maxRam": "2000", "minRam": "1000", "software": ["JAVA-11.0.4","MAVEN-3.9.4"],
+    "createdOn": "2024-06-02T12:02:21.946+00:00", "createdBy": {"id": "xxx", "firstName": "xxx", "lastName": "xxx", "department": "", "email": "", "mobileNumber": "", "gitUserName": "xxx"},
+    "plugins": [], "isPublic": true, "gitPath": "", "gitRepoLoc": "", "deployPath": "", "toDeployType": "xxx"}'::jsonb
+)
