c838f05b3096523c6736f2d14488ec499cd3fad7
Aluri <bealuri@apac.corpdir.net>
2024-07-17 13:20:09 +0530

    fabric identity management changes

--- packages/fabric-backend/lib/src/main/java/com/daimler/data/dto/fabric/CreateRoleRequestDto.java
+++ packages/fabric-backend/lib/src/main/java/com/daimler/data/dto/fabric/CreateRoleRequestDto.java
@@ -19,26 +19,28 @@ public class CreateRoleRequestDto implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 
-    private String roleType;
     private String id;
-    private boolean isGlobalCentralAvailable;
-    private boolean isWorkflowBased;
     private String name;
-    private boolean needsAdditionalSelfRequestApproval;
-    private boolean needsOrgScopes;
     private String description;
-    private String defaultValidityType;
-    private boolean isSelfRequestable;
-    private boolean needsCustomScopes;
-    private boolean isDynamic;
     private boolean isJobTitle;
     private boolean notificationsActive;
+//    private boolean isDynamic;
+    private boolean isSelfRequestable;
+    private boolean needsAdditionalSelfRequestApproval;
+    private boolean needsOrgScopes;
+    private boolean needsCustomScopes;
+    
+    private String defaultValidityType;
+    private boolean deprovisioning;
+    private boolean isGlobalCentralAvailable;
+    private boolean isWorkflowBased;
+    private String roleType;
     private String dataClassification;
     private List<String> communityAvailability;
     private WorkflowDefinitionDto workflowDefinition;
     private List<String> marketAvailabilities;
     private List<String> organizationAvailabilities;
-    private boolean deprovisioning;
+    
     private AccessReviewDto accessReview;
     
 }
