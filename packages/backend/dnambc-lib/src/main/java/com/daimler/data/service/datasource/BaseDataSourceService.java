f247ec21b5f06022861dd89ff3fc1962c6843a67
araarum <araarum>
2024-09-11 09:03:06 +0000

    imported date

--- packages/backend/dnambc-lib/src/main/java/com/daimler/data/service/datasource/BaseDataSourceService.java
+++ packages/backend/dnambc-lib/src/main/java/com/daimler/data/service/datasource/BaseDataSourceService.java
@@ -31,6 +31,7 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Date;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
