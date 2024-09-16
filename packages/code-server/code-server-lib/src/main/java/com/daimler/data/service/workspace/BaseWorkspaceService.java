aedaf63dd8f8d6c1b62494b2aa4e745ff0e85b83
ARAARUM <ARAARUM>
2024-09-16 09:03:20 +0000

    bugfix username logs

--- packages/code-server/code-server-lib/src/main/java/com/daimler/data/service/workspace/BaseWorkspaceService.java
+++ packages/code-server/code-server-lib/src/main/java/com/daimler/data/service/workspace/BaseWorkspaceService.java
@@ -702,7 +702,7 @@ import com.daimler.data.util.ConstantsUtility;
 						 	for (Map.Entry<String, Boolean> gitUser : gitUsers.entrySet()) {
 								HttpStatus addGitUser = gitClient.addUserToRepo(gitUser.getKey(), repoName);
 								if (addGitUser == HttpStatus.UNPROCESSABLE_ENTITY) {
-									log.info("Failed while adding {} as collaborator with status {}",gitUser, addGitUser.name());
+									log.info("Failed while adding {} as collaborator with status {}",gitUser.getKey(), addGitUser.name());
 									MessageDescription errMsg = new MessageDescription(
 											"Failed while adding " + gitUser.getKey()
 													+ " as collaborator, Because"
