dfdb029d20ca83ba347870dc47a9c1dd934f2b4b
aravindh45 <63506505+aravindh45@users.noreply.github.com>
2024-09-24 15:15:28 +0530

    Update FabricWorkspaceClient.java

--- packages/fabric-backend/lib/src/main/java/com/daimler/data/application/client/FabricWorkspaceClient.java
+++ packages/fabric-backend/lib/src/main/java/com/daimler/data/application/client/FabricWorkspaceClient.java
@@ -370,7 +370,7 @@ public class FabricWorkspaceClient {
 				response = getUserResponse.getBody();
 				return response;
 			}
-		}}catch(HttpClientErrorException e){
+		}catch(HttpClientErrorException e){
 			log.error("Http Error occured: {}",e);
 		}catch(Exception e) {
 			log.error("Failed to get users and groups of workspace {} with exception {} ",workspaceGroupId, e);
