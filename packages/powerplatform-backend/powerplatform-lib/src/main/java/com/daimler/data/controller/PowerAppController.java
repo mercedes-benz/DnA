235320d5a883fa2cef8916d04aa62ad17cbc6af3
Aluri <bealuri@apac.corpdir.net>
2024-10-09 14:00:32 +0530

    Notifications enablement

--- packages/powerplatform-backend/powerplatform-lib/src/main/java/com/daimler/data/controller/PowerAppController.java
+++ packages/powerplatform-backend/powerplatform-lib/src/main/java/com/daimler/data/controller/PowerAppController.java
@@ -1,8 +1,14 @@
 package com.daimler.data.controller;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,12 +20,16 @@ import org.springframework.web.bind.annotation.RestController;
 import com.daimler.data.api.powerapps.PowerappsApi;
 import com.daimler.data.application.auth.UserStore;
 import com.daimler.data.controller.exceptions.GenericMessage;
+import com.daimler.data.controller.exceptions.MessageDescription;
+import com.daimler.data.dto.powerapps.CreatedByVO;
+import com.daimler.data.dto.powerapps.DeveloperVO;
 import com.daimler.data.dto.powerapps.PowerAppCollectionVO;
 import com.daimler.data.dto.powerapps.PowerAppCreateRequestVO;
+import com.daimler.data.dto.powerapps.PowerAppCreateRequestWrapperVO;
 import com.daimler.data.dto.powerapps.PowerAppResponseVO;
 import com.daimler.data.dto.powerapps.PowerAppUpdateRequestVO;
 import com.daimler.data.dto.powerapps.PowerAppVO;
-import com.daimler.data.service.fabric.PowerAppService;
+import com.daimler.data.service.powerapp.PowerAppService;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -40,6 +50,14 @@ public class PowerAppController implements PowerappsApi
 	@Autowired
 	private UserStore userStore;
 	
+	@Value("${powerapps.defaults.environment}")
+	private String defaultEnvironment;
+	
+	@Value("${powerapps.defaults.productionAvailability}")
+	private String prodAvailabilityDefault;
+	
+	private SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");  
+	
 	@Override
 	@ApiOperation(value = "Creates a new power app subscription request.", nickname = "create", notes = "Creates a new power app subscription request", response = PowerAppResponseVO.class, tags={ "powerapps", })
     @ApiResponses(value = { 
@@ -53,9 +71,54 @@ public class PowerAppController implements PowerappsApi
         produces = { "application/json" }, 
         consumes = { "application/json" },
         method = RequestMethod.POST)
-    public ResponseEntity<PowerAppResponseVO> create(@ApiParam(value = "Request Body that contains data required for creating a new workspace" ,required=true )  @Valid @RequestBody PowerAppCreateRequestVO powerAppCreateVO){
+    public ResponseEntity<PowerAppResponseVO> create(@ApiParam(value = "Request Body that contains data required for creating a new workspace" ,required=true )  @Valid @RequestBody PowerAppCreateRequestWrapperVO powerAppCreateVO){
+		if(powerAppCreateVO!= null) {
+			PowerAppResponseVO responseVO = new PowerAppResponseVO();
+			PowerAppCreateRequestVO projectCreateVO = powerAppCreateVO.getData();
+			String name = projectCreateVO.getName();
+			PowerAppVO existingApp = service.findbyUniqueLiteral(name);
+			if(existingApp!=null && existingApp.getId()!=null) {
+				log.error("Power App request with this name {} already exists , failed to create new request", name);
+				MessageDescription invalidMsg = new MessageDescription("Power app request already exists with given name. Please retry with unique name");
+				GenericMessage errorMessage = new GenericMessage();
+				errorMessage.setSuccess(HttpStatus.CONFLICT.name());
+				errorMessage.addWarnings(invalidMsg);
+				responseVO.setData(null);
+				responseVO.setResponse(errorMessage);
+				return new ResponseEntity<>(responseVO, HttpStatus.CONFLICT);
+			}
+			if(projectCreateVO.getName() == null || "".equalsIgnoreCase(projectCreateVO.getName())
+					|| projectCreateVO.getEnvOwnerId() == null || "".equalsIgnoreCase(projectCreateVO.getEnvOwnerId())
+					|| projectCreateVO.getDepartment()  == null || "".equalsIgnoreCase(projectCreateVO.getDepartment())
+					|| projectCreateVO.getBillingPlant()  == null || "".equalsIgnoreCase(projectCreateVO.getBillingPlant())
+					|| projectCreateVO.getBillingCostCentre() == null || "".equalsIgnoreCase(projectCreateVO.getBillingCostCentre())) {
+				MessageDescription mandatoryFieldsError = new MessageDescription("Bad Request, Please fill all mandatory fields.");
+				GenericMessage errorMessage = new GenericMessage();
+				errorMessage.setSuccess(HttpStatus.BAD_REQUEST.name());
+				errorMessage.addErrors(mandatoryFieldsError);
+				responseVO.setData(null);
+				responseVO.setResponse(errorMessage);
+				return new ResponseEntity<>(responseVO, HttpStatus.BAD_REQUEST);
+			}
+			if(projectCreateVO.getEnvironment() == null || "".equalsIgnoreCase(projectCreateVO.getEnvironment().name())){
+				projectCreateVO.setEnvironment(null);
+			}
+			CreatedByVO requestUser = this.userStore.getVO();
+			List<MessageDescription> errors = new ArrayList<>();
+			//proceed
+		}else {
+			PowerAppResponseVO responseVO = new PowerAppResponseVO();
+			MessageDescription invalidMsg = new MessageDescription("Bad request, please fill all required fields and retry.");
+			GenericMessage errorMessage = new GenericMessage();
+			errorMessage.setSuccess(HttpStatus.BAD_REQUEST.name());
+			errorMessage.addWarnings(invalidMsg);
+			responseVO.setData(null);
+			responseVO.setResponse(errorMessage);
+			return new ResponseEntity<>(responseVO, HttpStatus.BAD_REQUEST);
+		}
 		return null;
 	}
+	
 
 	
 	@Override
@@ -74,9 +137,27 @@ public class PowerAppController implements PowerappsApi
         method = RequestMethod.GET)
     public ResponseEntity<PowerAppCollectionVO> getAll(@ApiParam(value = "page number from which listing of workspaces should start. Offset. Example 2") @Valid @RequestParam(value = "offset", required = false) Integer offset,
     		@ApiParam(value = "page size to limit the number of workspaces, Example 15") @Valid @RequestParam(value = "limit", required = false) Integer limit,
-    		@ApiParam(value = "Sort workspaces by a given variable like name, createdOn", allowableValues = "name, createdOn") @Valid @RequestParam(value = "sortBy", required = false) String sortBy,
+    		@ApiParam(value = "Sort workspaces by a given variable like name, requestedOn, state", allowableValues = "name, requestedOn, state") @Valid @RequestParam(value = "sortBy", required = false) String sortBy,
     		@ApiParam(value = "Sort solutions based on the given order, example asc,desc", allowableValues = "asc, desc") @Valid @RequestParam(value = "sortOrder", required = false) String sortOrder){
-    	return null;
+		PowerAppCollectionVO collection = new PowerAppCollectionVO();
+		int defaultLimit = 10;
+		if (offset == null || offset < 0)
+			offset = 0;
+		if (limit == null || limit < 0) {
+			limit = defaultLimit;
+		}
+		List<PowerAppVO> records =  new ArrayList<>();
+		CreatedByVO requestUser = this.userStore.getVO();
+		String user = requestUser.getId();
+		records = service.getAll(limit, offset, user);
+		Long count = service.getCount(user);
+		HttpStatus responseCode = HttpStatus.NO_CONTENT;
+		if(records!=null && !records.isEmpty()) {
+			collection.setRecords(records);
+			collection.setTotalCount(count.intValue());
+			responseCode = HttpStatus.OK;
+		}
+	return new ResponseEntity<>(collection, responseCode);
     }
 
 	@Override
@@ -94,7 +175,27 @@ public class PowerAppController implements PowerappsApi
         consumes = { "application/json" },
         method = RequestMethod.GET)
     public ResponseEntity<PowerAppVO> getById(@ApiParam(value = "Power platform subscription ID to be fetched",required=true) @PathVariable("id") String id){
-		return null;
+		PowerAppVO existingApp = service.getById(id);
+		if(existingApp==null || !id.equalsIgnoreCase(existingApp.getId())) {
+			log.warn("No app found with id {}, failed to fetch saved inputs for given power app request id", id);
+			return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+		}
+		CreatedByVO requestUser = this.userStore.getVO();
+		List<String> appUsers = new ArrayList<>();
+		appUsers.add(existingApp.getRequestedBy().getId());
+		List<DeveloperVO> developers = existingApp.getDevelopers();
+		if(developers!=null && !developers.isEmpty()) {
+			developers.forEach(n-> appUsers.add(n.getUserDetails().getId()));
+		}
+		if(appUsers!=null && !appUsers.isEmpty()) {
+			if(!appUsers.contains(requestUser.getId())) {
+				log.warn("User not part of requested power platform application with id {} and name {}, Not authorized to use other projects",id,existingApp.getName());
+				return new ResponseEntity<>(null, HttpStatus.FORBIDDEN);
+			}else {
+				return new ResponseEntity<>(existingApp, HttpStatus.OK);
+			}
+		}
+		return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
 	}
 
 	@Override
