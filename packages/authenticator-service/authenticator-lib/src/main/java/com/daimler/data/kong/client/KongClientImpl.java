3eb8f2672b20e8553c7526221a40b4feb11be5a2
araarum <araarum>
2024-10-22 06:11:01 +0000

    controller logic change

--- packages/authenticator-service/authenticator-lib/src/main/java/com/daimler/data/kong/client/KongClientImpl.java
+++ packages/authenticator-service/authenticator-lib/src/main/java/com/daimler/data/kong/client/KongClientImpl.java
@@ -844,7 +844,7 @@ public class KongClientImpl implements KongClient {
 				}
 			}
 			else{
-				LOGGER.error("plugin {} does not exist", pluginName);
+				LOGGER.info("plugin {} does not exist", pluginName);
 				messageDescription.setMessage("plugin does not exist");
 				message.setSuccess("NOT_FOUND");
 				errors.add(messageDescription);
@@ -860,15 +860,6 @@ public class KongClientImpl implements KongClient {
 			message.setErrors(errors);
 			return message;
 			}
-			if (ex.getRawStatusCode() == HttpStatus.NOT_FOUND.value()) {	
-				LOGGER.error("plugin {} does not exist", pluginName);
-					messageDescription.setMessage("plugin does not exist");
-					message.setSuccess("NOT_FOUND");
-					errors.add(messageDescription);
-					message.setErrors(errors);
-					return message;
-			}
-
 			LOGGER.error("Exception: {} occured while updating plugin: {} details", ex.getMessage(), pluginName);			
 			messageDescription.setMessage(ex.getMessage());
 			errors.add(messageDescription);
@@ -883,6 +874,63 @@ public class KongClientImpl implements KongClient {
 			message.setErrors(errors);
 		}
 
+		return message;
+	}
+
+	@Override
+	public  GenericMessage attachRequestTransformerPluginToService(AttachFunctionPluginVO attachFunctionPluginVO, String serviceName){
+		GenericMessage message = new GenericMessage();
+		MessageDescription messageDescription = new MessageDescription();
+		List<MessageDescription> errors = new ArrayList<>();
+		List<MessageDescription> warnings = new ArrayList<>();
+		try {			
+			String kongUri = kongBaseUri + "/services/" + serviceName + "/plugins";
+			HttpHeaders headers = new HttpHeaders();
+			headers.set("Accept", "application/json");
+			headers.set("Content-Type", "application/json");
+			AttachFunctionPluginWrapperDto requestWrapper = new AttachFunctionPluginWrapperDto();
+			AttachFunctionPluginConfigVO functionPluginConfigVO = attachFunctionPluginVO.getConfig();
+			AttachFunctionPluginConfigRequestDto functionPluginConfigRequestDto = new AttachFunctionPluginConfigRequestDto();
+
+			functionPluginConfigRequestDto.setAccess(functionPluginConfigVO.getAccess());
+			requestWrapper.setName(attachFunctionPluginVO.getName());
+			requestWrapper.setConfig(functionPluginConfigRequestDto);
+
+			HttpEntity<AttachFunctionPluginWrapperDto> functionPluginRequest = new HttpEntity<AttachFunctionPluginWrapperDto>(requestWrapper, headers);
+			ResponseEntity<String> response = restTemplate.exchange(kongUri, HttpMethod.POST, functionPluginRequest, String.class);
+			if (response != null && response.hasBody()) {
+				HttpStatus statusCode = response.getStatusCode();
+				if (statusCode == HttpStatus.CREATED) {
+					LOGGER.info("Function plugin attached successfully to service: {}", serviceName);					
+					message.setSuccess("Success");
+					message.setErrors(errors);
+					message.setWarnings(warnings);
+					return message;
+				}
+			}
+		} catch (HttpClientErrorException ex) {
+			if (ex.getRawStatusCode() == HttpStatus.CONFLICT.value()) {
+				LOGGER.info("Function plugin already attached to service: {}", serviceName);
+				message.setSuccess("Failure");
+				messageDescription.setMessage("Api Authoriser Plugin already attached to service");
+				errors.add(messageDescription);
+				message.setErrors(errors);
+				return message;
+			}	
+			LOGGER.error("Error occured while attaching Function plugin to service: {}", ex.getMessage());
+			message.setSuccess("Failure");
+			messageDescription.setMessage(ex.getMessage());
+			errors.add(messageDescription);
+			message.setErrors(errors);
+			return message;
+		} catch (Exception e) {
+			LOGGER.error("Error while attaching Function plugin to service: {}", e.getMessage());
+			message.setSuccess("Failure");
+			messageDescription.setMessage(e.getMessage());
+			errors.add(messageDescription);
+			message.setErrors(errors);
+			return message;
+		}
 		return message;
 	}
 //	@Override
