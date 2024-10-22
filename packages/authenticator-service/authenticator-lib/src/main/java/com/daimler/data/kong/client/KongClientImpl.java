96f03d96fd5b8820d68242dfa50751ae9cf5fe3d
araarum <araarum>
2024-10-22 06:15:54 +0000

    removed transformer plugin

--- packages/authenticator-service/authenticator-lib/src/main/java/com/daimler/data/kong/client/KongClientImpl.java
+++ packages/authenticator-service/authenticator-lib/src/main/java/com/daimler/data/kong/client/KongClientImpl.java
@@ -877,62 +877,62 @@ public class KongClientImpl implements KongClient {
 		return message;
 	}
 
-	@Override
-	public  GenericMessage attachRequestTransformerPluginToService(AttachFunctionPluginVO attachFunctionPluginVO, String serviceName){
-		GenericMessage message = new GenericMessage();
-		MessageDescription messageDescription = new MessageDescription();
-		List<MessageDescription> errors = new ArrayList<>();
-		List<MessageDescription> warnings = new ArrayList<>();
-		try {			
-			String kongUri = kongBaseUri + "/services/" + serviceName + "/plugins";
-			HttpHeaders headers = new HttpHeaders();
-			headers.set("Accept", "application/json");
-			headers.set("Content-Type", "application/json");
-			AttachFunctionPluginWrapperDto requestWrapper = new AttachFunctionPluginWrapperDto();
-			AttachFunctionPluginConfigVO functionPluginConfigVO = attachFunctionPluginVO.getConfig();
-			AttachFunctionPluginConfigRequestDto functionPluginConfigRequestDto = new AttachFunctionPluginConfigRequestDto();
+	// @Override
+	// public  GenericMessage attachRequestTransformerPluginToService(AttachFunctionPluginVO attachFunctionPluginVO, String serviceName){
+	// 	GenericMessage message = new GenericMessage();
+	// 	MessageDescription messageDescription = new MessageDescription();
+	// 	List<MessageDescription> errors = new ArrayList<>();
+	// 	List<MessageDescription> warnings = new ArrayList<>();
+	// 	try {			
+	// 		String kongUri = kongBaseUri + "/services/" + serviceName + "/plugins";
+	// 		HttpHeaders headers = new HttpHeaders();
+	// 		headers.set("Accept", "application/json");
+	// 		headers.set("Content-Type", "application/json");
+	// 		AttachFunctionPluginWrapperDto requestWrapper = new AttachFunctionPluginWrapperDto();
+	// 		AttachFunctionPluginConfigVO functionPluginConfigVO = attachFunctionPluginVO.getConfig();
+	// 		AttachFunctionPluginConfigRequestDto functionPluginConfigRequestDto = new AttachFunctionPluginConfigRequestDto();
 
-			functionPluginConfigRequestDto.setAccess(functionPluginConfigVO.getAccess());
-			requestWrapper.setName(attachFunctionPluginVO.getName());
-			requestWrapper.setConfig(functionPluginConfigRequestDto);
+	// 		functionPluginConfigRequestDto.setAccess(functionPluginConfigVO.getAccess());
+	// 		requestWrapper.setName(attachFunctionPluginVO.getName());
+	// 		requestWrapper.setConfig(functionPluginConfigRequestDto);
 
-			HttpEntity<AttachFunctionPluginWrapperDto> functionPluginRequest = new HttpEntity<AttachFunctionPluginWrapperDto>(requestWrapper, headers);
-			ResponseEntity<String> response = restTemplate.exchange(kongUri, HttpMethod.POST, functionPluginRequest, String.class);
-			if (response != null && response.hasBody()) {
-				HttpStatus statusCode = response.getStatusCode();
-				if (statusCode == HttpStatus.CREATED) {
-					LOGGER.info("Function plugin attached successfully to service: {}", serviceName);					
-					message.setSuccess("Success");
-					message.setErrors(errors);
-					message.setWarnings(warnings);
-					return message;
-				}
-			}
-		} catch (HttpClientErrorException ex) {
-			if (ex.getRawStatusCode() == HttpStatus.CONFLICT.value()) {
-				LOGGER.info("Function plugin already attached to service: {}", serviceName);
-				message.setSuccess("Failure");
-				messageDescription.setMessage("Api Authoriser Plugin already attached to service");
-				errors.add(messageDescription);
-				message.setErrors(errors);
-				return message;
-			}	
-			LOGGER.error("Error occured while attaching Function plugin to service: {}", ex.getMessage());
-			message.setSuccess("Failure");
-			messageDescription.setMessage(ex.getMessage());
-			errors.add(messageDescription);
-			message.setErrors(errors);
-			return message;
-		} catch (Exception e) {
-			LOGGER.error("Error while attaching Function plugin to service: {}", e.getMessage());
-			message.setSuccess("Failure");
-			messageDescription.setMessage(e.getMessage());
-			errors.add(messageDescription);
-			message.setErrors(errors);
-			return message;
-		}
-		return message;
-	}
+	// 		HttpEntity<AttachFunctionPluginWrapperDto> functionPluginRequest = new HttpEntity<AttachFunctionPluginWrapperDto>(requestWrapper, headers);
+	// 		ResponseEntity<String> response = restTemplate.exchange(kongUri, HttpMethod.POST, functionPluginRequest, String.class);
+	// 		if (response != null && response.hasBody()) {
+	// 			HttpStatus statusCode = response.getStatusCode();
+	// 			if (statusCode == HttpStatus.CREATED) {
+	// 				LOGGER.info("Function plugin attached successfully to service: {}", serviceName);					
+	// 				message.setSuccess("Success");
+	// 				message.setErrors(errors);
+	// 				message.setWarnings(warnings);
+	// 				return message;
+	// 			}
+	// 		}
+	// 	} catch (HttpClientErrorException ex) {
+	// 		if (ex.getRawStatusCode() == HttpStatus.CONFLICT.value()) {
+	// 			LOGGER.info("Function plugin already attached to service: {}", serviceName);
+	// 			message.setSuccess("Failure");
+	// 			messageDescription.setMessage("Api Authoriser Plugin already attached to service");
+	// 			errors.add(messageDescription);
+	// 			message.setErrors(errors);
+	// 			return message;
+	// 		}	
+	// 		LOGGER.error("Error occured while attaching Function plugin to service: {}", ex.getMessage());
+	// 		message.setSuccess("Failure");
+	// 		messageDescription.setMessage(ex.getMessage());
+	// 		errors.add(messageDescription);
+	// 		message.setErrors(errors);
+	// 		return message;
+	// 	} catch (Exception e) {
+	// 		LOGGER.error("Error while attaching Function plugin to service: {}", e.getMessage());
+	// 		message.setSuccess("Failure");
+	// 		messageDescription.setMessage(e.getMessage());
+	// 		errors.add(messageDescription);
+	// 		message.setErrors(errors);
+	// 		return message;
+	// 	}
+	// 	return message;
+	// }
 //	@Override
 //	public CreateServiceResponseVO getServiceByName(String serviceName) {
 //		
