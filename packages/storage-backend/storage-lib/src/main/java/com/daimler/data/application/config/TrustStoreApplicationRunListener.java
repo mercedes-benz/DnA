2544cd116a056275da5b92705ebca4af06c14817
araarum <araarum>
2024-11-25 12:04:00 +0000

    added logs

--- packages/storage-backend/storage-lib/src/main/java/com/daimler/data/application/config/TrustStoreApplicationRunListener.java
+++ packages/storage-backend/storage-lib/src/main/java/com/daimler/data/application/config/TrustStoreApplicationRunListener.java
@@ -1,5 +1,8 @@
 package com.daimler.data.application.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringApplicationRunListener;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -7,19 +10,24 @@ import org.springframework.core.env.ConfigurableEnvironment;
 
 public class TrustStoreApplicationRunListener implements SpringApplicationRunListener {
 
+    private static Logger LOGGER = LoggerFactory.getLogger(CacheUpdateEventListener.class);
+
     public TrustStoreApplicationRunListener(SpringApplication application, String[] args) {
         // Constructor required by SpringApplicationRunListener
     }
 
     @Override
     public void environmentPrepared(ConfigurableEnvironment environment) {
-        String trustStorePath = environment.getProperty("trust.store.path");
-        String trustStorePassword = environment.getProperty("trust.store.password");
 
+        LOGGER.info("Adding CA Trust Store....");
+        String trustStorePath = environment.getProperty("spring.cloud.vault.ssl.trust.store.path");
+        String trustStorePassword = environment.getProperty("spring.cloud.vault.ssl.trust.store.password");
+        
         if (trustStorePath != null && trustStorePassword != null) {
             System.setProperty("javax.net.ssl.trustStore", trustStorePath);
             System.setProperty("javax.net.ssl.trustStorePassword", trustStorePassword);
         }
+        LOGGER.info("Added CA Trust Store value {}....",System.getProperty("javax.net.ssl.trustStore"));
     }
 
 } 
