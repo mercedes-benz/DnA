ae1c84483ac9a1551e87cdaa2e2f5541ac077afa
aravindh45 <arjunraj034@gmail.com>
2024-11-26 14:27:38 +0530

    added context for chronos

--- packages/chronos/chronos-lib/src/main/java/com/daimler/data/application/config/TrustStoreApplicationRunListener.java
+++ packages/chronos/chronos-lib/src/main/java/com/daimler/data/application/config/TrustStoreApplicationRunListener.java
@@ -5,35 +5,29 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.ConfigurableBootstrapContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringApplicationRunListener;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.env.ConfigurableEnvironment;
 
 public class TrustStoreApplicationRunListener implements SpringApplicationRunListener {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(TrustStoreApplicationRunListener.class);
+    private static Logger LOGGER = LoggerFactory.getLogger(TrustStoreApplicationRunListener.class);
 
     public TrustStoreApplicationRunListener(SpringApplication application, String[] args) {
-        // Required constructor
+        // Constructor required by SpringApplicationRunListener
     }
 
     @Override
-    public void environmentPrepared(ConfigurableBootstrapContext bootstrapContext, ConfigurableEnvironment environment) {
-        LOGGER.info("Adding CA Trust Store....");
+    public void environmentPrepared(ConfigurableBootstrapContext bootstrapContext,ConfigurableEnvironment environment) {
 
+        LOGGER.info("Adding CA Trust Store....");
         String trustStorePath = environment.getProperty("spring.cloud.vault.ssl.trust.store.path");
         String trustStorePassword = environment.getProperty("spring.cloud.vault.ssl.trust.store.password");
 
         if (trustStorePath != null && trustStorePassword != null) {
-            // Set the trust store properties
             System.setProperty("javax.net.ssl.trustStore", trustStorePath);
             System.setProperty("javax.net.ssl.trustStorePassword", trustStorePassword);
-            LOGGER.info("Trust store configured: {}", trustStorePath);
-        } else {
-            LOGGER.warn("Trust store path or password is not configured!");
         }
+        LOGGER.info("Added CA Trust Store value {}....",System.getProperty("javax.net.ssl.trustStore"));
     }
 
-    @Override
-    public void starting(ConfigurableBootstrapContext bootstrapContext) {
-        // This method is intentionally left empty or for additional early setup
-    }
-}
+} 
