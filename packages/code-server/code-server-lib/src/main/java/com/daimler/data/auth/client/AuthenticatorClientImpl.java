9676888037f27a6c697a7b8d6dd4ba92119ee62c
aravindh45 <63506505+aravindh45@users.noreply.github.com>
2024-12-03 15:27:03 +0530

    Update AuthenticatorClientImpl.java

--- packages/code-server/code-server-lib/src/main/java/com/daimler/data/auth/client/AuthenticatorClientImpl.java
+++ packages/code-server/code-server-lib/src/main/java/com/daimler/data/auth/client/AuthenticatorClientImpl.java
@@ -475,7 +475,7 @@ public class AuthenticatorClientImpl  implements AuthenticatorClient{
 			}
 			LOGGER.info("calling kong to get route details for projectname : {}",env!=null ? serviceName.toLowerCase()+"-"+env:serviceName);
 			RouteResponseVO routeResponse = getRouteByName( env!=null ? serviceName.toLowerCase()+"-"+env:serviceName,  env!=null ? serviceName.toLowerCase()+"-"+env:serviceName);
-			if(routeResponse.getId()==null){
+			if(routeResponse == null || routeResponse.getId()==null){
 				if("success".equalsIgnoreCase(createServiceResponse.getSuccess()) || isServiceAlreadyCreated ) {
 					createRouteResponse = createRoute(createRouteRequestVO, env!=null ? serviceName.toLowerCase()+"-"+env:serviceName);
 					if(Objects.nonNull(createRouteResponse) && Objects.nonNull(createRouteResponse.getErrors())) {
