7d6f60152ca5eeae89babcfa30c9e4ce6fb88298
BENSON BOON ALURI <benson_boon.aluri@mercedes-benz.com>
2024-12-09 15:47:12 +0530

    Delete packages/fabric-backend/lib/src/main directory

--- packages/fabric-backend/lib/src/main/resources/db/migration/V1__create_tables.sql
+++
@@ -1,6 +0,0 @@
-
-CREATE TABLE IF NOT EXISTS fabric_workspace_nsql (
-    id text NOT NULL PRIMARY KEY,
-    data jsonb NOT NULL
-);
-
--- packages/fabric-backend/lib/src/main/resources/db/migration/V2__update_data.sql
+++
@@ -1,8 +0,0 @@
---
--- UPDATE Script start
---
-UPDATE fabric_workspace_nsql SET data = data where id ='1';
-
---
--- UPDATE Script end
---
