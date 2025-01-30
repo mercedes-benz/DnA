3a275519db5a977709a8c7e388533d76cd88e9ab
sudarsv <sudarsv>
2025-01-30 05:33:41 +0000

    Edit button in Consumer Summary

--- packages/data-product-mfe/src/components/dataTransfer/Summary/index.js
+++ packages/data-product-mfe/src/components/dataTransfer/Summary/index.js
@@ -25,9 +25,9 @@ const Summary = ({ history }) => {
   });
   const consumerDivision = data.consumer
     ? serializeDivisionSubDivision(divisionList, {
-        division: data.consumer?.division,
-        subDivision: data.consumer?.subDivision,
-      })
+      division: data.consumer?.division,
+      subDivision: data.consumer?.subDivision,
+    })
     : {};
 
   const dispatch = useDispatch();
@@ -209,7 +209,7 @@ const Summary = ({ history }) => {
                 ) : null}
                 {showPersonalData ? (
                   <div className={classNames(Styles.sectionWrapper, data.personalRelatedData === 'Yes' && Styles.yellowBorder)}>
-                    { data.personalRelatedData === 'Yes' && <i className={classNames('icon mbc-icon alert circle', Styles.warningIcon)} /> }
+                    {data.personalRelatedData === 'Yes' && <i className={classNames('icon mbc-icon alert circle', Styles.warningIcon)} />}
                     <div className={Styles.firstPanel}>
                       <div className={Styles.flexLayout}>
                         <div>
@@ -392,148 +392,160 @@ const Summary = ({ history }) => {
                           <div>
                             <h5>Contact Information</h5>
                           </div>
+                          <span
+                            className={Styles.editicon}
+                            onClick={() => {
+                              const url = `/data/datasharing/consume/${dataTransferId}`;
+                              console.log("url", url)
+                              window.location.href = url;
+                            }}
+                            style={{ cursor: "pointer" }}>
+                            <div>
+                            <i className="icon mbc-icon edit small" />
+                          </div>
+                        </span>
+                      </div>
+                      <div className={classNames(Styles.flexLayout, Styles.fourColumn)}>
+                        <div>
+                          <label className="input-label summary">Responsible Manager (E3 +) </label>
+                          <br />
+                          {data?.consumer?.businessOwnerName?.firstName} {data?.consumer?.businessOwnerName?.lastName}
                         </div>
+                        <div>
+                          <label className="input-label summary">Date of Data Transfer</label>
+                          <br />
+                          {regionalDateFormat(data?.consumer?.dateOfDataTransfer)}
+                        </div>
+                        <div>
+                          <label className="input-label summary">Division</label>
+                          <br />
+                          {consumerDivision?.name}
+                        </div>
+                        <div>
+                          <label className="input-label summary">Sub Division</label>
+                          <br />
+                          {consumerDivision?.subdivision?.name || '-'}
+                        </div>
+                      </div>
+                      <div className={classNames(Styles.flexLayout, Styles.fourColumn)}>
+                        <div>
+                          <label className="input-label summary">Department</label>
+                          <br />
+                          {data?.consumer?.department}
+                        </div>
+                        <div>
+                          <label className="input-label summary">
+                            LCO/LCR needed to be involved / has to check legal basis of usage of personal data?{' '}
+                          </label>
+                          <br />
+                          {data?.consumer?.lcoNeeded}
+                        </div>
+                        <div>
+                          <label className="input-label summary">LeanIX App-ID</label>
+                          <br />
+                          {data?.consumer?.leanIX?.leanIXDetails.appReferenceStr !== null ? <a href={`${Envs.LEANIX_BASEURL}/${data.consumer?.leanIX?.leanIXDetails?.appReferenceStr}`} target="_blank" rel="noopener noreferrer">{data?.consumer?.leanIX?.appId}</a> : data?.consumer?.leanIX?.appId || '-'}
+                        </div>
+                        <div>
+                          <label className="input-label summary">Corresponding Compliance Contact, i.e. Local Compliance Officer/ Responsible or Multiplier </label>
+                          <br />
+                          {data?.consumer?.complianceOfficer || '-'}
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                {showConsumerPersonalData ? (
+                  <div className={classNames(Styles.sectionWrapper, data?.consumer?.personalRelatedData === 'Yes' && Styles.yellowBorder)}>
+                    {data?.consumer?.personalRelatedData === 'Yes' && <i className={classNames('icon mbc-icon alert circle', Styles.warningIcon)} />}
+                    <div className={Styles.firstPanel}>
+                      <div className={Styles.flexLayout}>
+                        <div>
+                          <h5>Personal Related Data</h5>
+                        </div>
+                      </div>
+                      <div className={Styles.flexLayout}>
+                        <div>
+                          <label className="input-label summary">
+                            Is personal related data transferred and actually processed at application level?
+                          </label>
+                          <br />
+                          {data?.consumer?.personalRelatedData}
+                        </div>
+                      </div>
+                      {data?.consumer?.personalRelatedData === 'Yes' ? (
                         <div className={classNames(Styles.flexLayout, Styles.fourColumn)}>
                           <div>
-                            <label className="input-label summary">Responsible Manager (E3 +) </label>
+                            <label className="input-label summary">
+                              (Business) purpose of processing this personal related data
+                            </label>
                             <br />
-                            {data?.consumer?.businessOwnerName?.firstName} {data?.consumer?.businessOwnerName?.lastName}
-                          </div>
-                          <div>
-                            <label className="input-label summary">Date of Data Transfer</label>
-                            <br />
-                            {regionalDateFormat(data?.consumer?.dateOfDataTransfer)}
-                          </div>
-                          <div>
-                            <label className="input-label summary">Division</label>
-                            <br />
-                            {consumerDivision?.name}
-                          </div>
-                          <div>
-                            <label className="input-label summary">Sub Division</label>
-                            <br />
-                            {consumerDivision?.subdivision?.name || '-'}
-                          </div>
-                        </div>
-                        <div className={classNames(Styles.flexLayout, Styles.fourColumn)}>
-                          <div>
-                            <label className="input-label summary">Department</label>
-                            <br />
-                            {data?.consumer?.department}
+                            <pre className={classNames(Styles.formattedText)}>{data?.consumer?.personalRelatedDataPurpose}</pre>
                           </div>
                           <div>
                             <label className="input-label summary">
-                              LCO/LCR needed to be involved / has to check legal basis of usage of personal data?{' '}
+                              Legal basis for processing this personal related data?
                             </label>
                             <br />
-                            {data?.consumer?.lcoNeeded}
+                            {data?.consumer?.personalRelatedDataLegalBasis}
                           </div>
                           <div>
-                            <label className="input-label summary">LeanIX App-ID</label>
+                            <label className="input-label summary">
+                              Has corresponding compliance contact checked overall personal data processing
+                            </label>
                             <br />
-                            {data?.consumer?.leanIX?.leanIXDetails.appReferenceStr !== null ?<a href={`${Envs.LEANIX_BASEURL}/${data.consumer?.leanIX?.leanIXDetails?.appReferenceStr}`} target="_blank" rel="noopener noreferrer">{data?.consumer?.leanIX?.appId}</a>: data?.consumer?.leanIX?.appId || '-'}
+                            {data?.consumer?.LCOCheckedLegalBasis}
                           </div>
                           <div>
-                            <label className="input-label summary">Corresponding Compliance Contact, i.e. Local Compliance Officer/ Responsible or Multiplier </label>
+                            <label className="input-label summary">
+                              LCO/LCR comments to data usage of personal data
+                            </label>
                             <br />
-                            {data?.consumer?.complianceOfficer || '-'}
+                            <pre className={classNames(Styles.formattedText)}>{data?.consumer?.LCOComments}</pre>
                           </div>
                         </div>
-                      </div>
-                    </div>
-                    {showConsumerPersonalData ? (
-                      <div className={classNames(Styles.sectionWrapper, data?.consumer?.personalRelatedData === 'Yes' && Styles.yellowBorder)}>
-                        { data?.consumer?.personalRelatedData === 'Yes' && <i className={classNames('icon mbc-icon alert circle', Styles.warningIcon)} /> }
-                        <div className={Styles.firstPanel}>
-                          <div className={Styles.flexLayout}>
-                            <div>
-                              <h5>Personal Related Data</h5>
-                            </div>
-                          </div>
-                          <div className={Styles.flexLayout}>
-                            <div>
-                              <label className="input-label summary">
-                                Is personal related data transferred and actually processed at application level?
-                              </label>
-                              <br />
-                              {data?.consumer?.personalRelatedData}
-                            </div>
-                          </div>
-                          {data?.consumer?.personalRelatedData === 'Yes' ? (
-                            <div className={classNames(Styles.flexLayout, Styles.fourColumn)}>
-                              <div>
-                                <label className="input-label summary">
-                                  (Business) purpose of processing this personal related data
-                                </label>
-                                <br />
-                                <pre className={classNames(Styles.formattedText)}>{data?.consumer?.personalRelatedDataPurpose}</pre>
-                              </div>
-                              <div>
-                                <label className="input-label summary">
-                                  Legal basis for processing this personal related data?
-                                </label>
-                                <br />
-                                {data?.consumer?.personalRelatedDataLegalBasis}
-                              </div>
-                              <div>
-                                <label className="input-label summary">
-                                  Has corresponding compliance contact checked overall personal data processing
-                                </label>
-                                <br />
-                                {data?.consumer?.LCOCheckedLegalBasis}
-                              </div>
-                              <div>
-                                <label className="input-label summary">
-                                  LCO/LCR comments to data usage of personal data
-                                </label>
-                                <br />
-                                <pre className={classNames(Styles.formattedText)}>{data?.consumer?.LCOComments}</pre>
-                              </div>
-                            </div>
-                          ) : null}
-                          {data?.consumer?.personalRelatedData === 'Yes' ? (<div className={Styles.flexLayout}>
-                            <div>
-                              <label className="input-label summary">Is corresponding Compliance contact aware of this transfer?</label>
-                              <br />
-                              {data?.consumer?.personalRelatedDataContactAwareTransfer}
-                            </div>
-                          </div>) : null}
-                          {data?.consumer?.personalRelatedData === 'Yes' && data?.consumer?.personalRelatedDataContactAwareTransfer === 'Yes'
-                            ? (<div className={classNames(Styles.flexLayout, Styles.threeColumn)}>
-                              <div>
-                                <label className="input-label summary">Has s/he any objections to this transfer?</label>
-                                <br />
-                                {data?.consumer?.personalRelatedDataObjectionsTransfer}
-                              </div>
-                              {data?.consumer?.personalRelatedDataObjectionsTransfer === 'Yes' && <>
-                                <div>
-                                  <label className="input-label summary">Please state your reasoning for transfering nonetheless</label>
-                                  <br />
-                                  <pre className={classNames(Styles.formattedText)}>{data?.consumer?.personalRelatedDataTransferingNonetheless}</pre>
-                                </div>
-                                <div>
-                                  <label className="input-label summary">Please state your objections</label>
-                                  <br />
-                                  <pre className={classNames(Styles.formattedText)}>{data?.consumer?.personalRelatedDataTransferingObjections}</pre>
-                                </div>
-                              </>}
-                            </div>) : null}
+                      ) : null}
+                      {data?.consumer?.personalRelatedData === 'Yes' ? (<div className={Styles.flexLayout}>
+                        <div>
+                          <label className="input-label summary">Is corresponding Compliance contact aware of this transfer?</label>
+                          <br />
+                          {data?.consumer?.personalRelatedDataContactAwareTransfer}
                         </div>
-                      </div>
-                    ) : null}
-                  </>
-                ) : (
-                  <div className={Styles.sectionWrapper}>
-                    <div className={Styles.flexLayout} style={{ color: '#c0c8d0', justifyContent: 'center' }}>
-                      Receiving side not yet started to finalise the data transfer.
+                      </div>) : null}
+                      {data?.consumer?.personalRelatedData === 'Yes' && data?.consumer?.personalRelatedDataContactAwareTransfer === 'Yes'
+                        ? (<div className={classNames(Styles.flexLayout, Styles.threeColumn)}>
+                          <div>
+                            <label className="input-label summary">Has s/he any objections to this transfer?</label>
+                            <br />
+                            {data?.consumer?.personalRelatedDataObjectionsTransfer}
+                          </div>
+                          {data?.consumer?.personalRelatedDataObjectionsTransfer === 'Yes' && <>
+                            <div>
+                              <label className="input-label summary">Please state your reasoning for transfering nonetheless</label>
+                              <br />
+                              <pre className={classNames(Styles.formattedText)}>{data?.consumer?.personalRelatedDataTransferingNonetheless}</pre>
+                            </div>
+                            <div>
+                              <label className="input-label summary">Please state your objections</label>
+                              <br />
+                              <pre className={classNames(Styles.formattedText)}>{data?.consumer?.personalRelatedDataTransferingObjections}</pre>
+                            </div>
+                          </>}
+                        </div>) : null}
                     </div>
                   </div>
-                )}
+                ) : null}
+              </>
+              ) : (
+              <div className={Styles.sectionWrapper}>
+                <div className={Styles.flexLayout} style={{ color: '#c0c8d0', justifyContent: 'center' }}>
+                  Receiving side not yet started to finalise the data transfer.
+                </div>
               </div>
+                )}
             </div>
           </div>
         </div>
       </div>
+    </div >
     </>
   );
 };
