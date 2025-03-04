12b58f54307d67b60137e1ed93cc720614380ea7
snagala <snagala>
2025-03-04 08:17:16 +0000

    remove file

--- packages/backend/dnambc-lib/src/main/resources/db/migration/V19_update_data.sql
+++
@@ -1,9 +0,0 @@
---
--- UPDATE Script start
---
-UPDATE userinfo_nsql_backup
-SET data = jsonb_set(data, '{isDeleted}', 'false'::jsonb, true)
-WHERE jsonb_typeof(data) = 'object';
---
--- UPDATE Script end
---
