8701af5f670c0f967bf5278b12ac06749237b9f4
aniruddha <aniruddhakhartade@gmail.com>
2025-04-18 11:22:26 +0530

    blueprint bug fixes

--- packages/code-space-mfe/src/components/codeSpaceBlueprint/CodeSpaceBlueprint.js
+++ packages/code-space-mfe/src/components/codeSpaceBlueprint/CodeSpaceBlueprint.js
@@ -5,7 +5,6 @@ import Styles from './CodeSpaceBlueprint.scss';
 import { blueprintTemplate } from './blueprintTemplate';
 
 const CodeSpaceBlueprint = ({ codespace }) => {
-  console.log('blueprint template', blueprintTemplate(codespace));
   return (
     <div className={classNames(Styles.csbWrapper)}>
       <DrawIoEmbed
