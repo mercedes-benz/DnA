51186bd0f8dee4fe5277d786d6790d64d88fabfc
SNEHA KS <ibm.ks@mercedes-benz.com>
2025-05-27 11:33:37 +0530

    Update BaseWorkspaceService.java

--- packages/code-server/code-server-lib/src/main/java/com/daimler/data/service/workspace/BaseWorkspaceService.java
+++ packages/code-server/code-server-lib/src/main/java/com/daimler/data/service/workspace/BaseWorkspaceService.java
@@ -155,9 +155,7 @@ import com.daimler.data.dto.workspace.UserInfoVO;
  
 	 @Value("${codeServer.codespace.filename}")
 	 private String codespaceFileName;
-   
-	 @Value("${codeServer.collab.pid}")
-	 private String collabPid;
+
 
 	 @Value("${codeServer.technical.id}")
 	 private String technicalId;
