ffe74e7f7eb7b2d2fca66e8fe714656e3f7e6007
ebhanda <ebhanda>
2025-06-04 05:09:25 +0000

    project update debug

--- packages/code-server/code-server-lib/src/main/java/com/daimler/data/db/repo/workspace/WorkspaceCustomRepositoryImpl.java
+++ packages/code-server/code-server-lib/src/main/java/com/daimler/data/db/repo/workspace/WorkspaceCustomRepositoryImpl.java
@@ -378,7 +378,7 @@ public class WorkspaceCustomRepositoryImpl extends CommonDataRepositoryImpl<Code
 		Date deployedOn = deploymentDetails.getLastDeployedOn();
 		String longdate = null;
 		if(deployedOn!=null)
-			log.info("deployed on is not null for project {}",projectName);
+			log.info("{} - deployed on is not null for project.",projectName);
 			longdate = String.valueOf(deployedOn.getTime()) ;
 			String updateQuery = "update workspace_nsql " +
 				"set data = jsonb_set(data,'{projectDetails," + environment + "}', " +
@@ -405,7 +405,7 @@ public class WorkspaceCustomRepositoryImpl extends CommonDataRepositoryImpl<Code
 				" \"lastDeploymentStatus\": " + addQuotes(deploymentDetails.getLastDeploymentStatus()) ;
 
 			List<DeploymentAudit> deploymentAuditLogs = deploymentDetails.getDeploymentAuditLogs();
-			log.info("deployment audit log is {}",deploymentAuditLogs);
+			log.info("{} - deployment audit log is {}",projectName, deploymentAuditLogs);
 			updateQuery += ", \"deploymentAuditLogs\" : ";
 			if (deploymentAuditLogs != null && !deploymentAuditLogs.isEmpty()) {
 				// Iterate over each DeploymentAudit object and add it to the JSON array
@@ -426,24 +426,24 @@ public class WorkspaceCustomRepositoryImpl extends CommonDataRepositoryImpl<Code
 				updateQuery += "]";
 			}else {
 				updateQuery +=  " []";
-				log.info("project {} deployed on is null.",projectName);
+				log.info("{} - project deployed on is null.",projectName);
 			}
 			updateQuery += "}')\r\n";
 			updateQuery += "where data->'projectDetails'->>'projectName' = '" + projectName + "'";
-			log.info("update query for project {} is {}",projectName, updateQuery);
+			log.info("{} - update query for project is {}",projectName, updateQuery);
 
 		try {
 			Date now = new Date();
-			log.info("creating native query at time {} ",now);
+			log.info("{} - creating native query at time {} ",projectName, now);
 			Query q = em.createNativeQuery(updateQuery);
-			log.info("execute update");
+			log.info("{} - execute update",projectName);
 			q.executeUpdate();
 			now = new Date();
-			log.info("execute update completed at time {}",now);
+			log.info("{} - execute update completed at time {}",projectName, now);
 			updateResponse.setSuccess("SUCCESS");
 			updateResponse.setErrors(new ArrayList<>());
 			updateResponse.setWarnings(new ArrayList<>());
-			log.info("deployment details updated successfully for project {} ", projectName);
+			log.info("{} - deployment details updated successfully for project ", projectName);
 		}catch(Exception e) {
 			MessageDescription errMsg = new MessageDescription("Failed while updating deployment details.");
 			errors.add(errMsg);
