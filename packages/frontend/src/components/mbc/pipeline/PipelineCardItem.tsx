0f86fd5ffeab01b5a5ee2da9ef86cc11c5570a9b
manjuha <manjuha>
2025-06-27 04:43:36 +0000

    CardViewUI

--- packages/frontend/src/components/mbc/pipeline/PipelineCardItem.tsx
+++ packages/frontend/src/components/mbc/pipeline/PipelineCardItem.tsx
@@ -16,92 +16,66 @@ const PipelineCardItem = ({ project, getRefreshedDagPermission }: Props) => {
   const goToDag = (dagId: string) => {
     history.push('/editcode/' + dagId);
   };
+
   const goToEditProject = () => {
     history.push(`/createnewpipeline/${project.projectId}/true`);
   };
 
-
-
-
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
-      if (
-        popupRef.current &&
-        !popupRef.current.contains(event.target as Node)
-      ) {
+      if (popupRef.current && !popupRef.current.contains(event.target as Node)) {
         setIsDagPopupVisible(false);
       }
     };
-
     document.addEventListener('mousedown', handleClickOutside);
-    return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
-    };
+    return () => document.removeEventListener('mousedown', handleClickOutside);
   }, []);
 
   return (
     <div className={Styles.pipelineCard}>
       <div className={Styles.cardHead}>
-        <span className={Styles.projectName} title={project.projectName}>
-          {project.projectName}
-        </span>
+        <div className={Styles.cardHeadInfo} onClick={goToEditProject}>
+          <div className={`btn btn-text forward arrow ${Styles.cardHeadTitle}`} title={project.projectName}>
+            {project.projectName}
+          </div>
+        </div>
       </div>
       <hr />
-
-      <div className={Styles.dagList}>
-        <div className={Styles.cardBodySection}>
-
-          <div className={Styles.metaItem}>
-            <span className={Styles.metaLabel}>ProjectId:</span>
-            <span className={Styles.metaValue}>{project.projectId}</span>
+      <div className={Styles.cardBodySection}>
+        <div>
+          <div>
+            <div>Project ID</div>
+            <div>{project.projectId}</div>
           </div>
-          <div className={Styles.metaItem}>
-            <span className={Styles.metaLabel}>ProjectName:</span>
-            <span className={Styles.metaValue} title={project.projectName}>
-              {project.projectName}
-            </span>
+          <div>
+            <div>Project Name</div>
+            <div>{project.projectName}</div>
           </div>
-          <div className={Styles.metaItem}>
-            <span className={Styles.metaLabel}>Permission:</span>
-            <span className={Styles.metaValue}>
-              {project.permission ? 'Read/Edit' : 'Read'}
-            </span>
+          <div>
+            <div>Permission</div>
+            <div>{project.permission ? 'Read/Edit' : 'Read'}</div>
           </div>
-
-          <div className={Styles.cardDagSection} >
+          <div className={Styles.cardCollabSection}>
             <div>DAGs</div>
-            <div className={Styles.dagSpacing} />
             {project.dags?.length > 0 ? (
-              <div
-                onMouseEnter={() => setIsDagPopupVisible(true)}
-                onMouseLeave={() => setIsDagPopupVisible(false)}
-              >
+              <div onMouseEnter={() => setIsDagPopupVisible(true)} onMouseLeave={() => setIsDagPopupVisible(false)}>
                 <i className="icon mbc-icon profile" />
-                <span
-                  className={Styles.cardDagIcon}
-                >
-                  {project.dags.length}
-                </span>
-                <div
-                  ref={popupRef}
-                  className={`${Styles.dagListPopup} ${isDagPopupVisible ? 'show' : 'hide'
-                    }`}
-                >
+                <span className={Styles.cardCollabIcon}>{project.dags.length}</span>
+                <div ref={popupRef} className={`${Styles.collabsList} ${isDagPopupVisible ? 'show' : 'hide'}`}>
                   <ul>
                     {project.dags.map((dag, idx) => (
                       <li key={idx} className={Styles.dagItem}>
-                        <div className={Styles.dagLeft}>
-                          <span className={Styles.dagName}>{dag.dagName}</span>
-                          <span className={Styles.permission}>
-                            [{dag.permissions?.includes('can_edit') ? 'Read/Edit' : 'Read'}]
-                          </span>
-                        </div>
+                        <span className={Styles.dagName}>{dag.dagName}</span>
+
+                        <span className={Styles.permission}>
+                          {dag.permissions?.includes('can_edit') ? 'Read/Edit' : 'Read'}
+                        </span>
+
                         <div className={Styles.cardDagActions}>
                           <button
-                            type="button"
                             className={Styles.actionBtn}
                             onClick={() => goToDag(dag.dagName)}
-                            title="Edit Code"
+                            title="Open in Editor"
                           >
                             <i className="icon mbc-icon edit" />
                           </button>
@@ -110,16 +84,13 @@ const PipelineCardItem = ({ project, getRefreshedDagPermission }: Props) => {
                             href={`${Envs.DATA_PIPELINES_APP_BASEURL}/graph?dag_id=${dag.dagName}`}
                             target="_blank"
                             rel="noreferrer"
-                            className={`${Styles.actionBtn} ${Styles.externalBtn}`}
-                            title="Open in Airflow"
+                            className={Styles.actionBtn}
+                            title="Open in New Tab"
                           >
-                            <i className={`${Styles.airflowNewTab} icon mbc-icon new-tab`} />
+                            <i className="icon mbc-icon new-tab" />
                           </a>
-
-
                         </div>
                       </li>
-
                     ))}
                   </ul>
                 </div>
@@ -129,10 +100,13 @@ const PipelineCardItem = ({ project, getRefreshedDagPermission }: Props) => {
             )}
           </div>
         </div>
-        <div className={Styles.cardFooter}>
-          <button className={Styles.actionBtn} onClick={goToEditProject}>
-            <i className="icon mbc-icon edit" />
-            <span>Edit Project</span>
+      </div>
+      <div className={Styles.cardFooter}>
+        <div></div>
+        <div className={Styles.btnGrp}>
+          <button className="btn btn-primary" onClick={goToEditProject}>
+            <i className="icon mbc-icon edit"></i>
+            <span>Edit</span>
           </button>
         </div>
       </div>
