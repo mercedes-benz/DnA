b2ddea8176343c8b89692d0d400bfc3d52baa8f9
snehaks <snehaks>
2025-07-21 18:10:14 +0000

    changes on delete api

--- packages/dbservice-backend/db-service-lib/src/main/java/com/daimler/data/db/repo/DbService/DbServiceCustomRepositoryImpl.java
+++ packages/dbservice-backend/db-service-lib/src/main/java/com/daimler/data/db/repo/DbService/DbServiceCustomRepositoryImpl.java
@@ -1,12 +1,16 @@
 package com.daimler.data.db.repo.DbService;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.stereotype.Repository;
 
+import com.daimler.data.controller.exceptions.GenericMessage;
 import com.daimler.data.db.entities.DbServiceNsql;
 import com.daimler.data.db.repo.common.CommonDataRepositoryImpl;
 
+import jakarta.persistence.Query;
 import jakarta.persistence.TypedQuery;
 import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.CriteriaQuery;
@@ -39,5 +43,52 @@ public class DbServiceCustomRepositoryImpl extends CommonDataRepositoryImpl<DbSe
         return getAllQuery.getResultList();
 
     }
+
+	private String addQuotes(String value) {
+		if(value!=null && !"null".equalsIgnoreCase(value)){
+			String escaped = value
+            .replace("\\", "\\\\")
+            .replace("\"", "\\\"")  
+            .replace("'", "''"); 
+			return "\"" + escaped + "\"";
+		}
+		else
+			return null;
+	}
+
+
+    @Override
+	public String updateDeleteStatus(DbServiceNsql entity) {
+		String response = "FAILED";
+		Date deployedOn = entity.getData().getModifiedOn();
+		String	longdate = String.valueOf(deployedOn.getTime()) ;
+		String updateQuery = "update db_service_nsql\r\n"
+				+ "set data = jsonb_set(jsonb_set(jsonb_set(data, '{status}', '\"DELETED\"'), '{modifiedBy}', '{"+
+				" \"id\": " + addQuotes(entity.getData().getModifiedBy().getId()) + "," +
+				" \"email\": " + addQuotes(entity.getData().getModifiedBy().getEmail()) + "," +
+				" \"lastName\": " + addQuotes(entity.getData().getModifiedBy().getLastName()) + "," +
+				" \"firstName\": " + addQuotes(entity.getData().getModifiedBy().getFirstName()) + "," +
+				" \"department\": " + addQuotes(entity.getData().getModifiedBy().getDepartment()) + "," +
+				" \"gitUserName\": " + addQuotes(entity.getData().getModifiedBy().getGitUserName()) + "," +
+				" \"mobileNumber\": " + addQuotes(entity.getData().getModifiedBy().getMobileNumber()) + "," +				
+				" \"isRead\": " + entity.getData().getModifiedBy().getIsRead() + "," +
+				" \"isAdmin\": " + entity.getData().getModifiedBy().getIsAdmin() + "," +
+				" \"isWrite\": " + entity.getData().getModifiedBy().getIsWrite() + "}'"+
+				"), '{modifiedOn}', '"+longdate+"') "+
+				"WHERE id = '"+entity.getId()+"'";
+
+				log.info("updateQuery {}",updateQuery);
+				
+
+		try {
+			Query q = em.createNativeQuery(updateQuery);
+			q.executeUpdate();
+			log.info("delete status updated successfully for dbService {} ", entity.getData().getServiceName());
+			response = "SUCCESS";
+		} catch (Exception e) {
+			log.error("Failed while updating the delete status with Exception {} ", e.getMessage());
+		}
+		return response;
+	}
 
 }
