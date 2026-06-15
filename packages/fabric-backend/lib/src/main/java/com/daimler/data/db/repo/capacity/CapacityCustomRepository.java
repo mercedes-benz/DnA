c888e41c939b9cd4d8f02d2f47546ab6745953f5
tsatend <tsatend>
2026-06-15 08:11:53 +0000

    FCF-4 FCF-365 Auto-assign Fabric workspaces to assigned capacity per project

---
+++ packages/fabric-backend/lib/src/main/java/com/daimler/data/db/repo/capacity/CapacityCustomRepository.java
@@ -0,0 +1,7 @@
+package com.daimler.data.db.repo.capacity;
+
+import com.daimler.data.db.entities.CapacityNsql;
+import com.daimler.data.db.repo.common.CommonDataRepository;
+
+public interface CapacityCustomRepository extends CommonDataRepository<CapacityNsql, String> {
+}
